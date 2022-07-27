0735ae86a0dfaaca4683009f56d93edb620c82ee
Andrei Chenchik <andrei@chenchik.me>
2022-07-27 16:27:56 +0200

    Move color resources to Helpers group

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,4 +1,11 @@
 import Foundation
 import UIKit
 
-extension UIColor { }
+extension UIColor {
+    static var ypBlack: UIColor { UIColor(named: "YP Black") ?? .black }
+    static var ypWhite: UIColor { UIColor(named: "YP White") ?? .white }
+    static var ypGreen: UIColor { UIColor(named: "YP Green") ?? .green }
+    static var ypRed: UIColor { UIColor(named: "YP Red") ?? .red }
+    static var ypGray: UIColor { UIColor(named: "YP Gray") ?? .gray }
+    static var ypBackground: UIColor { UIColor(named: "YP Background") ?? .systemBackground }
+}
