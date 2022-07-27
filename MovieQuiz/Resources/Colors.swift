a760ff86c041d261ea4342efce7e4a9f6a8621da
Andrei Chenchik <andrei@chenchik.me>
2022-07-27 15:34:56 +0200

    Make Resource Colors safer by removing force unwrap

--- MovieQuiz/Resources/Colors.swift
+++ MovieQuiz/Resources/Colors.swift
@@ -8,10 +8,10 @@
 import UIKit
 
 extension UIColor {
-    static var ypBlack: UIColor { UIColor(named: "YP Black")! }
-    static var ypWhite: UIColor { UIColor(named: "YP White")! }
-    static var ypGreen: UIColor { UIColor(named: "YP Green")! }
-    static var ypRed: UIColor { UIColor(named: "YP Red")! }
-    static var ypGray: UIColor { UIColor(named: "YP Gray")! }
-    static var ypBackground: UIColor { UIColor(named: "YP Background")! }
+    static var ypBlack: UIColor { UIColor(named: "YP Black") ?? .black }
+    static var ypWhite: UIColor { UIColor(named: "YP White") ?? .white }
+    static var ypGreen: UIColor { UIColor(named: "YP Green") ?? .green }
+    static var ypRed: UIColor { UIColor(named: "YP Red") ?? .red }
+    static var ypGray: UIColor { UIColor(named: "YP Gray") ?? .gray }
+    static var ypBackground: UIColor { UIColor(named: "YP Background") ?? .systemBackground }
 }
