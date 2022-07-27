78120578959a6bc3edba59ffbf81385d563aaec5
Andrei Chenchik <andrei@chenchik.me>
2022-07-27 14:06:44 +0200

    Add colors declaration file

---
+++ MovieQuiz/Resources/Colors.swift
@@ -0,0 +1,17 @@
+//
+//  Colors.swift
+//  MovieQuiz
+//
+//  Created by Andrei Chenchik on 27/7/22.
+//
+
+import UIKit
+
+extension UIColor {
+    static var ypBlack: UIColor { UIColor(named: "YP Black")! }
+    static var ypWhite: UIColor { UIColor(named: "YP White")! }
+    static var ypGreen: UIColor { UIColor(named: "YP Green")! }
+    static var ypRed: UIColor { UIColor(named: "YP Red")! }
+    static var ypGray: UIColor { UIColor(named: "YP Gray")! }
+    static var ypBackground: UIColor { UIColor(named: "YP Background")! }
+}
