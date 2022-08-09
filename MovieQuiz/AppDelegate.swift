b70438fd8913614cde726ad647a2f1e29ddd9aa3
ArthurM2 <arthur.komarov@gmail.com>
2022-08-09 12:31:46 +0500

    added LaunchScreen

--- MovieQuiz/AppDelegate.swift
+++ MovieQuiz/AppDelegate.swift
@@ -11,6 +11,7 @@ import UIKit
 final class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        sleep(3)
         return true
     }
 
