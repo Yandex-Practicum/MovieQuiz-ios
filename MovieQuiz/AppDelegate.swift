4c3edb7465dac68a7dad3c2e22f3512d97c0f33b
Gospodi <gospodijazzet@gmail.com>
2022-08-09 19:54:50 +0300

    добавлено базовое оформление кнопок (шрифт, цвет, подложка)

--- MovieQuiz/AppDelegate.swift
+++ MovieQuiz/AppDelegate.swift
@@ -11,7 +11,7 @@ import UIKit
 final class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        sleep(3) // задержка срабатывания заставки
+        // sleep(3) // задержка срабатывания заставки
         return true
     }
 
