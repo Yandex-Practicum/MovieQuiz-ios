953e286c5b0445595326e8946a9b2b594f2af968
Gospodi <gospodijazzet@gmail.com>
2022-08-11 16:23:21 +0300

    создали связи кода и элементов сториборда

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,65 +1,40 @@
 import UIKit
 
 final class MovieQuizViewController: UIViewController {
+    @IBOutlet private var imageView: UIImageView!
+    @IBOutlet private var textLabel: UILabel!
+    @IBOutlet private var counterLabel: UILabel!
 
-
-
-
-
-    // MARK: - Lifecycle
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        struct QuizQuestion {
-            let imageName: String
-            let questionText: String
-            let correctAnswer: Bool
-        }
-
-
-        let questions1 = QuizQuestion (
-            imageName: "The Godfather", questionText: "Рейтинг этого фильма больше чем 6?", correctAnswer: true
-        )
-
-        let questions2 = QuizQuestion (
-            imageName: "The Dark Knight", questionText: "Рейтинг этого фильма больше чем 6?", correctAnswer: true
-        )
-
-        let questions3 = QuizQuestion (
-            imageName: "Kill Bill", questionText: "Рейтинг этого фильма больше чем 6?", correctAnswer: true
-        )
-
-        let questions4 = QuizQuestion (
-            imageName: "The Avengers", questionText: "Рейтинг этого фильма больше чем 6?", correctAnswer: true
-        )
-
-        let questions5 = QuizQuestion (
-            imageName: "Deadpool", questionText: "Рейтинг этого фильма больше чем 6?", correctAnswer: true
-        )
-
-        let questions6 = QuizQuestion (
-            imageName: "The Green Knight", questionText: "Рейтинг этого фильма больше чем 6?", correctAnswer: true
-        )
-
-        let questions7 = QuizQuestion (
-            imageName: "Old", questionText: "Рейтинг этого фильма больше чем 6?", correctAnswer: false
-        )
-
-        let questions8 = QuizQuestion (
-            imageName: "The Ice Age Adventures of Buck Wild", questionText: "Рейтинг этого фильма больше чем 6?", correctAnswer: false
-        )
-
-        let questions9 = QuizQuestion (
-            imageName: "Tesla", questionText: "Рейтинг этого фильма больше чем 6?", correctAnswer: false
-        )
-
-        let questions10 = QuizQuestion (
-            imageName: "Vivarium", questionText: "Рейтинг этого фильма больше чем 6?", correctAnswer: false
-        )
+    @IBAction private func yesButtonClicked(_ sender: UIButton) {
 
     }
 
+    @IBAction private func noButtonClicked(_ sender: UIButton) {
 
+    }
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    struct QuizQuestion {
+        let imageName: String
+        let questionText: String
+        let correctAnswer: Bool
+        var questionNumber = 0
+
+        private let questions = [
+            QuizQuestion(imageName: "The Godfather", questionText: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+            QuizQuestion(imageName: "The Dark Knight", questionText: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+            QuizQuestion(imageName: "Kill Bill", questionText: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+            QuizQuestion(imageName: "The Avengers", questionText: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+            QuizQuestion(imageName: "The Avengers", questionText: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+            QuizQuestion(imageName: "The Green Knight", questionText: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+            QuizQuestion(imageName: "Old", questionText: "Рейтинг этого фильма больше чем 6?", correctAnswer: false),
+            QuizQuestion(imageName: "The Ice Age Adventures of Buck Wild", questionText: "Рейтинг этого фильма больше чем 6?", correctAnswer: false),
+            QuizQuestion(imageName: "Tesla", questionText: "Рейтинг этого фильма больше чем 6?", correctAnswer: false),
+            QuizQuestion(imageName: "Vivarium", questionText: "Рейтинг этого фильма больше чем 6?", correctAnswer: false)
+        ]
+    }
     //
     //
     //    Картинка: Old
@@ -123,7 +98,5 @@ final class MovieQuizViewController: UIViewController {
     //    Вопрос: Рейтинг этого фильма больше чем 6?
     //    Ответ: ДА
 
+
 }
-
-
-
