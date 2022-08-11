3b861104ee51838dfbb067528ab020dbb404e843
Sergey Makhlenko <mahlenko-weblive@yandex.ru>
2022-08-11 16:44:03 +0300

    Верстка приложения

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,4 +1,13 @@
 import Foundation
 import UIKit
 
-extension UIColor { }
+extension UIColor {
+    // swiftlint:disable force_unwrapping
+    static var appBackdrop: UIColor { UIColor(named: "backdrop")! }
+    static var appBackground: UIColor { UIColor(named: "background")! }
+    static var appDefault: UIColor { UIColor(named: "default")! }
+    static var appFail: UIColor { UIColor(named: "fail")! }
+    static var appSuccess: UIColor { UIColor(named: "success")! }
+    static var appSecondary: UIColor { UIColor(named: "secondary")! }
+    // swiftlint:enable force_unwrapping
+}
--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,72 +1,81 @@
 import UIKit
 
+enum FontDefault {
+    static let bold = "YSDisplay-Bold"
+    static let medium = "YSDisplay-Medium"
+    static let size = 20.0
+}
+
 final class MovieQuizViewController: UIViewController {
+    // MARK: - IBOutlet
+    @IBOutlet var viewContainer: UIView!
+    @IBOutlet weak var headerTitleLabel: UILabel!
+    @IBOutlet weak var headerCounterLabel: UILabel!
+    @IBOutlet weak var questionImageView: UIImageView!
+    @IBOutlet weak var questionTextLabel: UILabel!
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var trueButton: UIButton!
+
+    var allQuestions: Int = 10
+    var currentPosition: Int = 0
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        configuration()
+    }
+
+    // MARK: - Private methods
+
+    /// Default configuration storyboard elements
+    private func configuration() {
+        viewContainer.backgroundColor = UIColor.appBackground
+
+        headerTitleLabel.font = UIFont(name: FontDefault.medium, size: FontDefault.size)
+        headerCounterLabel.font = UIFont(name: FontDefault.medium, size: FontDefault.size)
+        headerCounterLabel.text = "\(currentPosition) / \(allQuestions)"
+
+        questionTextLabel.text = "..."
+        questionTextLabel.font = UIFont(name: FontDefault.bold, size: 23.0)
+
+        imageQuestionStyle(image: questionImageView)
+
+        // TODO: на сколько я понимаю, должен быть вариант создания дизайн-системы с наследованием класса UIButton
+        buttonStyle(button: falseButton)
+        buttonStyle(button: trueButton)
+    }
+
+    /// Default buttons style
+    private func buttonStyle(button: UIButton) {
+        button.layer.cornerRadius = 15
+        button.backgroundColor = UIColor.appDefault
+        button.tintColor = UIColor.appBackground
+
+        button.titleLabel?.adjustsFontForContentSizeCategory = true
+        button.titleLabel?.font = UIFont(name: FontDefault.bold, size: FontDefault.size)
+        button.titleEdgeInsets = UIEdgeInsets(top: 18.0, left: 16.0, bottom: 18.0, right: 16.0)
+    }
+
+    private func imageQuestionStyle(image: UIImageView) {
+        image.layer.borderWidth = 8
+        image.layer.cornerRadius = 20
+
+        imageResetStyle(image: image)
+    }
+
+    private func imageResetStyle(image: UIImageView) {
+        image.layer.borderColor = .none
+    }
+
+    private func answerSuccess(image: UIImageView) {
+        image.layer.borderColor = UIColor.appSuccess.cgColor
+    }
+
+    private func answerFail(image: UIImageView) {
+        image.layer.borderColor = UIColor.appFail.cgColor
     }
 }
-
-/*
- Mock-данные
- 
- 
- Картинка: The Godfather
- Настоящий рейтинг: 9,2
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: The Dark Knight
- Настоящий рейтинг: 9
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: Kill Bill
- Настоящий рейтинг: 8,1
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: The Avengers
- Настоящий рейтинг: 8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: Deadpool
- Настоящий рейтинг: 8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: The Green Knight
- Настоящий рейтинг: 6,6
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: Old
- Настоящий рейтинг: 5,8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
-
-
- Картинка: The Ice Age Adventures of Buck Wild
- Настоящий рейтинг: 4,3
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
-
-
- Картинка: Tesla
- Настоящий рейтинг: 5,1
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
-
-
- Картинка: Vivarium
- Настоящий рейтинг: 5,8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
- */
