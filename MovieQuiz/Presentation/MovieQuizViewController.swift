07020c55041141e6e8cb3b025a5920a3bed46510
ArthurM2 <arthur.komarov@gmail.com>
2022-08-17 12:32:54 +0500

    fix alert statistics & warning UILabel

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,137 +1,24 @@
 import UIKit
 
 final class MovieQuizViewController: UIViewController {
-    @IBOutlet private var imageView: UIImageView!
-    @IBOutlet private var textLabel: UILabel!
-    @IBOutlet private var counterLabel: UILabel!
-    @IBOutlet weak var noAnswerButton: UIButton!
-    @IBOutlet weak var yesAnswerButton: UIButton!
-    
-    var counterAnswers: Int = 0 // счетчик на каком вопросе мы находимся
-    var currentQuestionIndex: Int = 0 // индекс нашего вопроса
-    var counterCorrectAnswers: Int = 0
-    var numberOfQuizGames: Int = 0
-    var recordCorrectAnswers: Int = 0
-    var recordDate = Date()
-    var averageAccuracy = 0.0
-    var numberOfQuizQuestion: Int = 10
-    var allCorrectAnswers: Int = 0
-    
-    // MARK: - Lifecycle
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        viewModel()
-    }
-    
-    @IBAction private func noButtonClicked(_ sender: UIButton) {
-        showAnswerResult(isCorrect: questions[currentQuestionIndex].correctAnswer == false)
-    }
-    
-    @IBAction private func yesButtonClicked(_ sender: UIButton) {
-        showAnswerResult(isCorrect: questions[currentQuestionIndex].correctAnswer == true)
-    }
-    
-    private func show(quiz step: QuizStepViewModel) {
-        UIView.animate(withDuration: 0.1) {
-            self.textLabel.text = step.question
-            self.counterLabel.text = step.questionNumber
-            self.imageView.image = step.image
-        }
-    }
-
-    private func show(quiz result: QuizResultsViewModel) {
-        let alert = UIAlertController(title: result.title,
-                                      message: result.text,
-                                      preferredStyle: .alert)
-        let action = UIAlertAction(title: result.buttonText,
-                                   style: .default,
-                                   handler: {
-                                    _ in self.restart()
-        })
-        alert.addAction(action)
-        self.present(alert, animated: true, completion: nil)
-    }
-    
-    private func convert(model: QuizQuestion) -> QuizStepViewModel {
-        QuizStepViewModel(
-            image: UIImage(named: model.image) ?? .remove,
-            question: model.text,
-            questionNumber: "\(currentQuestionIndex + 1)/\(numberOfQuizQuestion)")
-    }
-    
-    private func restart() {
-        currentQuestionIndex = 0
-        viewModel()
-    }
-    
-    private func viewModel() {
-        imageView.layer.borderWidth = 8
-        imageView.layer.borderColor = UIColor.clear.cgColor
-        let viewModel = convert(model: questions[currentQuestionIndex])
-        show(quiz: viewModel)
-    }
-    
-    private func showAnswerResult(isCorrect: Bool) {
-        imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-            self.showNextQuestionOrResults()
-        }
-    }
-    
-    private func showNextQuestionOrResults() {
-        if currentQuestionIndex == questions.count - 1 {
-            numberOfQuizGames += 1
-            allCorrectAnswers += counterCorrectAnswers
-            let resultQuiz = getResultQuiz()
-            show(quiz: resultQuiz)
-        } else {
-            currentQuestionIndex += 1
-            viewModel()
-        }
-    }
-    
-    private func getResultQuiz() -> QuizResultsViewModel {
-        var title = "Игра окончена!"
-        var recordDateString = ""
-        if counterCorrectAnswers >= recordCorrectAnswers {
-            title = "новый рекорд"
-            recordCorrectAnswers = counterCorrectAnswers
-            recordDateString = Date().dateTimeString
-        }
-        
-        if counterCorrectAnswers == numberOfQuizGames {
-            title = "Поздравляем! Это лучший результат"
-        }
-        averageAccuracy = Double(allCorrectAnswers * 100) / Double(numberOfQuizQuestion * numberOfQuizGames)
-        let resultQuiz = QuizResultsViewModel(title: title,
-                                             text: """
-                                             Ваш результат: \(counterCorrectAnswers)/\(numberOfQuizQuestion)
-                                             Количество сыгранных квизов: \(numberOfQuizGames)
-                                             Рекорд: \(recordCorrectAnswers)/\(numberOfQuizQuestion) \(recordDateString)
-                                             Средняя точность: \(String(format: "%.02f", averageAccuracy))%
-                                             """,
-                                             buttonText: "Новая игра")
-        return resultQuiz
-    }
-    
     struct QuizQuestion {
         let image: String
         let text: String
         let correctAnswer: Bool
     }
-
+    
     struct QuizStepViewModel {
         let image: UIImage
         let question: String
         let questionNumber: String
     }
-
+    
     struct QuizResultsViewModel {
         let title: String
         let text: String
         let buttonText: String
     }
-
+    
     private let questions: [QuizQuestion] = [
         QuizQuestion(
             image: "The Godfather",
@@ -174,4 +61,124 @@ final class MovieQuizViewController: UIViewController {
             text: "Рейтинг этого фильма больше чем 6?",
             correctAnswer: false)  // Настоящий рейтинг 5,8
     ]
+    
+    @IBOutlet private var imageView: UIImageView!
+    @IBOutlet private var textLabel: UILabel!
+    @IBOutlet private var counterLabel: UILabel!
+    @IBOutlet weak var noAnswerButton: UIButton!
+    @IBOutlet weak var yesAnswerButton: UIButton!
+    
+    private var currentQuestionIndex: Int = 0 // индекс нашего вопроса
+    private var counterCorrectAnswers: Int = 0 // счетчик правильных ответов
+    private var numberOfQuizGames: Int = 0 // количетсво сыгранных квизов
+    private var recordCorrectAnswers: Int = 0 // рекорд правильных овтетов
+    private var recordDate = Date() // дата рекорда
+    private var averageAccuracy = 0.0 // среднее кол-во угаданных ответов в %
+    private var numberOfQuizQuestion: Int = 10 // максимальное количество вопросов ответов
+    private var allCorrectAnswers: Int = 0
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViewModel()
+    }
+    
+    @IBAction private func noButtonClicked(_ sender: UIButton) {
+        showAnswerResult(isCorrect: questions[currentQuestionIndex].correctAnswer == false)
+    }
+    
+    @IBAction private func yesButtonClicked(_ sender: UIButton) {
+        showAnswerResult(isCorrect: questions[currentQuestionIndex].correctAnswer == true)
+    }
+    
+    private func show(quiz step: QuizStepViewModel) {
+        UIView.animate(withDuration: 0.1) {
+            self.textLabel.text = step.question
+            self.counterLabel.text = step.questionNumber
+            self.imageView.image = step.image
+        }
+    }
+
+    // показываем алерт
+    private func show(quiz result: QuizResultsViewModel) {
+        let alert = UIAlertController(title: result.title,
+                                      message: result.text,
+                                      preferredStyle: .alert)
+        let action = UIAlertAction(title: result.buttonText,
+                                   style: .default,
+                                   handler: {
+                                    _ in self.restart()
+        })
+        alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    private func convert(model: QuizQuestion) -> QuizStepViewModel {
+        QuizStepViewModel(
+            image: UIImage(named: model.image) ?? .remove,
+            question: model.text,
+            questionNumber: "\(currentQuestionIndex + 1)/\(numberOfQuizQuestion)")
+    }
+    
+    // перезапуск игры
+    private func restart() {
+        counterCorrectAnswers = 0
+        currentQuestionIndex = 0
+        setupViewModel()
+    }
+    
+    // установка вьюшки
+    private func setupViewModel() {
+        imageView.layer.borderWidth = 8
+        imageView.layer.borderColor = UIColor.clear.cgColor
+        let viewModel = convert(model: questions[currentQuestionIndex])
+        show(quiz: viewModel)
+    }
+    
+    private func showAnswerResult(isCorrect: Bool) {
+        if isCorrect {
+            counterCorrectAnswers += 1
+        }
+        imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            self.showNextQuestionOrResults()
+        }
+    }
+    
+    private func showNextQuestionOrResults() {
+        if currentQuestionIndex == questions.count - 1 {
+            allCorrectAnswers += counterCorrectAnswers
+            numberOfQuizGames += 1
+            let resultQuiz = getResultQuiz()
+            show(quiz: resultQuiz)
+        } else {
+            currentQuestionIndex += 1
+            setupViewModel()
+        }
+    }
+    
+    private func getResultQuiz() -> QuizResultsViewModel {
+        var title = "Игра окончена!"
+        var recordDateString = ""
+        if counterCorrectAnswers >= recordCorrectAnswers {
+            title = "Поздравляем! Новый рекорд!"
+            recordCorrectAnswers = counterCorrectAnswers
+            recordDateString = Date().dateTimeString
+        }
+        
+        if counterCorrectAnswers == numberOfQuizGames {
+            title = "Поздравляем! Это лучший результат"
+        }
+        averageAccuracy = Double(allCorrectAnswers * 100) / Double(numberOfQuizQuestion * numberOfQuizGames)
+        let resultQuiz = QuizResultsViewModel(title: title,
+                                             text: """
+                                             Ваш результат: \(counterCorrectAnswers)/\(numberOfQuizQuestion)
+                                             Количество сыгранных квизов: \(numberOfQuizGames)
+                                             Рекорд: \(recordCorrectAnswers)/\(numberOfQuizQuestion) \(recordDateString)
+                                             Средняя точность: \(String(format: "%.02f", averageAccuracy))%
+                                             """,
+                                             buttonText: "Новая игра")
+        return resultQuiz
+    }
+    
 }
