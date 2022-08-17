90018400407ae5bb270058b65702ad986b71fb41
Vladislav Gorelov <vsg1993@yandex.ru>
2022-08-17 19:17:10 +0300

    Добавили констрейнты для экрана загрузки LaunchScreen, чтобы всё отображалось корректно на разных устройствах.

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -2,10 +2,12 @@ import Foundation
 import UIKit
 
 extension UIColor {
-       static var ypBlack: UIColor { UIColor(named: "black")! }
+    // swiftlint:disable force_unwrapping
+    static var ypBlack: UIColor { UIColor(named: "black")! }
     static var ypDarkGray: UIColor { UIColor(named: "darkGray")! }
     static var ypRed: UIColor { UIColor(named: "red")! }
     static var ypGray: UIColor { UIColor(named: "gray")! }
     static var ypGreen: UIColor { UIColor(named: "green")! }
     static var ypWhite: UIColor { UIColor(named: "white")! }
+// swiftlint: enable force_unwrapping
 }
