a1ca3adf996efa3dd5eeb5443ac7e777d5bbc9df
EduardFort <eduardseleznyov@gmail.com>
2022-09-01 14:39:01 +0300

    Реализована логика приложения

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,4 +1,12 @@
 import Foundation
 import UIKit
 
-extension UIColor { }
+extension UIColor {
+static var ypBlack: UIColor { UIColor(named: "Black")! }
+static var ypWhite: UIColor { UIColor(named: "White")! }
+static var ypGreen: UIColor { UIColor(named: "Green")! }
+static var ypRed: UIColor   { UIColor(named: "Red")! }
+static var ypGrey: UIColor  { UIColor(named: "Grey")! }
+static var ypSpaceGrey: UIColor { UIColor(named: "Space Grey")! }
+    }
+
