b6ed73d288e753fcf55ba4a3e6b3adc44195e10d
Andrey Sysoev <andrey@sysoev.com>
2022-09-16 15:36:14 +0300

    Added views to Main screen storyboard and defined its logic in MovieQuizViewController

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,9 +1,187 @@
 import UIKit
 
 final class MovieQuizViewController: UIViewController {
+    // MARK: - Outlets and Actions
+
+    @IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet private weak var textLabel: UILabel!
+    @IBOutlet private weak var counterLabel: UILabel!
+
+    @IBAction private func yesButtonClicked(_ sender: UIButton) {
+        showAnswerResult(isCorrect: checkAnswerCorrectness(for: true))
+    }
+
+    @IBAction private func noButtonClicked(_ sender: UIButton) {
+        showAnswerResult(isCorrect: checkAnswerCorrectness(for: false))
+    }
+
+    // MARK: - Vars
+
+    private let questions: [QuizQuestion] = [
+        QuizQuestion(
+            image: "The Godfather",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "The Dark Knight",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "Kill Bill",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "The Avengers",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "Deadpool",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "The Green Knight",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "Old",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false),
+        QuizQuestion(
+            image: "The Ice Age Adventures of Buck Wild",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false),
+        QuizQuestion(
+            image: "Tesla",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false),
+        QuizQuestion(
+            image: "Vivarium",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false),
+    ]
+
+    private var currentQuestionIndex: Int = 0
+    private var correctAnswersCount: Int = 0
+    private var currentQuestionCorrectAnswer: Bool? {
+        guard let question = questions[safe: currentQuestionIndex] else { return nil }
+        return question.correctAnswer
+    }
+
     // MARK: - Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        guard let firstQuestion = questions.first else { return }
+
+        show(quiz: convert(model: firstQuestion))
+    }
+
+    private func checkAnswerCorrectness(for chosenAnswer: Bool) -> Bool {
+        chosenAnswer == currentQuestionCorrectAnswer
+    }
+
+    private func convert(model: QuizQuestion) -> QuizStepViewModel {
+        QuizStepViewModel(
+            image: UIImage(named: model.image) ?? UIImage(),
+            question: model.text,
+            questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)")
+    }
+
+    private func show(quiz step: QuizStepViewModel) {
+        imageView.image = step.image
+        textLabel.text = step.question
+        counterLabel.text = step.questionNumber
+    }
+
+    private func show(quiz result: QuizResultsViewModel) {
+        let alert = UIAlertController(
+            title: result.title,
+            message: result.text,
+            preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: result.buttonText, style: .default) { _ in
+            self.currentQuestionIndex = 0
+
+            self.correctAnswersCount = 0
+
+            let firstQuestion = self.questions[self.currentQuestionIndex]
+            let viewModel = self.convert(model: firstQuestion)
+            self.show(quiz: viewModel)
+        }
+
+        alert.addAction(action)
+
+        self.present(alert, animated: true, completion: nil)
+    }
+
+    private func showAnswerResult(isCorrect: Bool) {
+        if isCorrect {
+            correctAnswersCount += 1
+        }
+
+        imageView.layer.masksToBounds = true
+        imageView.layer.borderWidth = 8
+        imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
+        imageView.layer.cornerRadius = 20
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            self.imageView.layer.borderWidth = 0
+            self.showNextQuestionOrResults()
+        }
+    }
+
+    private func showNextQuestionOrResults() {
+        if let nextQuestion = questions[safe: currentQuestionIndex + 1] {
+            currentQuestionIndex += 1
+            let viewModel = convert(model: nextQuestion)
+            show(quiz: viewModel)
+        } else {
+            let text = "Ваш результат: \(correctAnswersCount) из \(questions.count)"
+            let viewModel = QuizResultsViewModel(
+                title: "Это раунд окончен!",
+                text: text,
+                buttonText: "Cыграть еще раз")
+            show(quiz: viewModel)
+        }
+    }
+
+    // MARK: - ViewModel
+
+    private struct QuizStepViewModel {
+        let image: UIImage
+        let question: String
+        let questionNumber: String
+    }
+    
+    private struct QuizResultsViewModel {
+        let title: String
+        let text: String
+        let buttonText: String
+    }
+
+    private struct QuizQuestion {
+        let image: String
+        let text: String
+        let correctAnswer: Bool
+    }
+}
+
+// MARK: - Extensions
+
+extension UIColor {
+    static var ypGreen: UIColor {
+        UIColor(named: "green" ) ?? UIColor.green
+    }
+
+    static var ypRed: UIColor {
+        UIColor(named: "red" ) ?? UIColor.red
+    }
+}
+
+extension Collection {
+    subscript (safe index: Index) -> Element? {
+        return indices.contains(index) ? self[index] : nil
     }
 }
 
