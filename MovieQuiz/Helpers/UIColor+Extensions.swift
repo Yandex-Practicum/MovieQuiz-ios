b5ccf15234581e422eeca60a339f22d2e216d8fa
Yuriy <ura.yakovlev@yandex.ru>
2022-09-25 17:01:19 +0300

    Сверстал основной экран по макетам в Figma. Реализовал логику работы приложения, добавил вывод результата и возможность начать новый раунд

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,4 +1,7 @@
 import Foundation
 import UIKit
 
-extension UIColor { }
+extension UIColor {
+    static let ypGreen = UIColor { _ in UIColor(named: "YP Green")! }
+    static let ypRed = UIColor { _ in UIColor(named: "YP Red")! }
+}
