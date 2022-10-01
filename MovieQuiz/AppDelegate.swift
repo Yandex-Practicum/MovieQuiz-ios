91f911ec1741cc55fad46e3febabdab7a5f11b29
Ruslan Batalov <batalov@nord-steel.ru>
2022-10-01 16:49:47 +0300

    В AppDelegate добавлен вывод на консоль

--- MovieQuiz/AppDelegate.swift
+++ MovieQuiz/AppDelegate.swift
@@ -11,6 +11,8 @@ import UIKit
 final class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        print("Hello word  ")
         return true
     }
 
