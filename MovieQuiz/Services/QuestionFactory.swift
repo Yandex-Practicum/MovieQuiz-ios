bf882fbc63fa074235924a12a95a361b40f08d55
Maxim Papier <109724501+maxim-papier@users.noreply.github.com>
2022-10-03 15:00:38 +0600

    retain cycle for delegate fixed

--- MovieQuiz/Services/QuestionFactory.swift
+++ MovieQuiz/Services/QuestionFactory.swift
@@ -8,10 +8,10 @@ class QuestionFactory: QuestionFactoryProtocol {
         case noElements, noImages
     }
     
-    private let delegate: QuestionFactoryDelegate
+    private weak var delegate: QuestionFactoryDelegate?
     private let moviesLoader: MoviesLoading
     private var movies: [MostPopularMovie] = []
-  
+    
     init(delegate: QuestionFactoryDelegate, moviesLoader: MoviesLoading) {
         self.delegate = delegate
         self.moviesLoader = moviesLoader
@@ -23,16 +23,20 @@ class QuestionFactory: QuestionFactoryProtocol {
             guard let self = self else { return }
             let index = (0..<self.movies.count).randomElement() ?? 0
             guard let movie = self.movies[safe: index] else {
-                self.delegate.didFailToLoadData(with: QuesionsError.noElements)
+                if let safeDelegate = self.delegate {
+                    safeDelegate.didFailToLoadData(with: QuesionsError.noElements)
+                }
                 return
             }
             
             var imageData = Data()
-
+            
             do {
                 imageData = try Data(contentsOf: movie.imageURL)
             } catch {
-                self.delegate.didFailToLoadData(with: QuesionsError.noImages)
+                if let safeDelegate = self.delegate {
+                    safeDelegate.didFailToLoadData(with: QuesionsError.noImages)
+                }
                 print("Failed to load image")
                 return
             }
@@ -50,7 +54,9 @@ class QuestionFactory: QuestionFactoryProtocol {
             
             DispatchQueue.main.async { [weak self] in
                 guard let self = self else { return }
-                self.delegate.didReceiveNextQuestion(question: question)
+                if let safeDelegate = self.delegate {
+                    safeDelegate.didReceiveNextQuestion(question: question)
+                }
             }
         }
     }
@@ -61,57 +67,62 @@ class QuestionFactory: QuestionFactoryProtocol {
             switch result {
             case .success(let mostPopularMovies):
                 self.movies = mostPopularMovies.items // сохраняем фильм в нашу новую переменную
-                self.delegate.didLoadDataFromServer() // сообщаем, что данные загрузились
+                if let safeDelegate = self.delegate {
+                    safeDelegate.didLoadDataFromServer() // сообщаем, что данные загрузились
+                }
             case .failure(let error):
-                self.delegate.didFailToLoadData(with: error) // сообщаем об ошибке нашему MovieQuizViewController
+                if let safeDelegate = self.delegate {
+                    safeDelegate.didFailToLoadData(with: error) // сообщаем об ошибке нашему MovieQuizViewController
+                }
             }
+            
         }
     }
 }
 
-    
-    
-    /*
-    private let questions: [QuizQuestion] = [
-        QuizQuestion(
-            image: "The Godfather",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: true),
-        QuizQuestion(
-            image: "The Dark Knight",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: true),
-        QuizQuestion(
-            image: "Kill Bill",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: true),
-        QuizQuestion(
-            image: "The Avengers",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: true),
-        QuizQuestion(
-            image: "Deadpool",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: true),
-        QuizQuestion(
-            image: "The Green Knight",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: false),
-        QuizQuestion(
-            image: "Old",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: false),
-        QuizQuestion(
-            image: "The Ice Age Adventures of Buck Wild",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: false),
-        QuizQuestion(
-            image: "Tesla",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: false),
-        QuizQuestion(
-            image: "Vivarium",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: false)
-    ]
-*/
+
+
+/*
+ private let questions: [QuizQuestion] = [
+ QuizQuestion(
+ image: "The Godfather",
+ text: "Рейтинг этого фильма больше чем 6?",
+ correctAnswer: true),
+ QuizQuestion(
+ image: "The Dark Knight",
+ text: "Рейтинг этого фильма больше чем 6?",
+ correctAnswer: true),
+ QuizQuestion(
+ image: "Kill Bill",
+ text: "Рейтинг этого фильма больше чем 6?",
+ correctAnswer: true),
+ QuizQuestion(
+ image: "The Avengers",
+ text: "Рейтинг этого фильма больше чем 6?",
+ correctAnswer: true),
+ QuizQuestion(
+ image: "Deadpool",
+ text: "Рейтинг этого фильма больше чем 6?",
+ correctAnswer: true),
+ QuizQuestion(
+ image: "The Green Knight",
+ text: "Рейтинг этого фильма больше чем 6?",
+ correctAnswer: false),
+ QuizQuestion(
+ image: "Old",
+ text: "Рейтинг этого фильма больше чем 6?",
+ correctAnswer: false),
+ QuizQuestion(
+ image: "The Ice Age Adventures of Buck Wild",
+ text: "Рейтинг этого фильма больше чем 6?",
+ correctAnswer: false),
+ QuizQuestion(
+ image: "Tesla",
+ text: "Рейтинг этого фильма больше чем 6?",
+ correctAnswer: false),
+ QuizQuestion(
+ image: "Vivarium",
+ text: "Рейтинг этого фильма больше чем 6?",
+ correctAnswer: false)
+ ]
+ */
