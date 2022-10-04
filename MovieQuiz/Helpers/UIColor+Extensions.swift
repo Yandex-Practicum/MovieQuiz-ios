cb0884c2c64305e57ae1837ed91886083c3630fa
EduardFort <eduardseleznyov@gmail.com>
2022-10-04 22:25:25 +0300

    Исправлена проблема с UIColor, заменен Green на green, по тому же принципу  заменены все остальные цвета. После замены, они исправлены и в сториборде на всех элементах экрана.

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -2,11 +2,11 @@ import Foundation
 import UIKit
 
 extension UIColor {
-static var ypBlack: UIColor { UIColor(named: "black")! }
-static var ypWhite: UIColor { UIColor(named: "white")! }
-static var ypGreen: UIColor { UIColor(named: "green")! }
-static var ypRed: UIColor   { UIColor(named: "red")! }
-static var ypGrey: UIColor  { UIColor(named: "grey")! }
-static var ypSpaceGrey: UIColor { UIColor(named: "space grey")! }
-    }
+    static var ypBlack: UIColor { UIColor(named: "black")! }
+    static var ypWhite: UIColor { UIColor(named: "white")! }
+    static var ypGreen: UIColor { UIColor(named: "green")! }
+    static var ypRed: UIColor   { UIColor(named: "red")! }
+    static var ypGrey: UIColor  { UIColor(named: "grey")! }
+    static var ypSpaceGrey: UIColor { UIColor(named: "space grey")! }
+}
 
