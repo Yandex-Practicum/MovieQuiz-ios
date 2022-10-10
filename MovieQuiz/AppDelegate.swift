85a84f7ed2b8abf4243f2e5589489bd5b5cb6aac
stealcompass <venya.e@gmail.com>
2022-10-10 19:49:04 +0300

    New message output for AppDelegate

--- MovieQuiz/AppDelegate.swift
+++ MovieQuiz/AppDelegate.swift
@@ -12,6 +12,7 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         print("Hello world")
+        print("Hello world for branch to merge") 
         return true
     }
 
