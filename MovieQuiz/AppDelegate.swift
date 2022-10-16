8e78937480751142d149cdecf8181267ea87b192
Aleksandr <9120603@gmail.com>
2022-10-16 18:28:31 +0300

    Реализованы все задания спринта 4, создано приложение-квиз по рейтингу фильмов. Дополнительно в функцию отражения результата добавлено обнуление рамки правильного/неправильного ответа при переходе к следующему вопросу для удобства пользователя.

--- MovieQuiz/AppDelegate.swift
+++ MovieQuiz/AppDelegate.swift
@@ -22,7 +22,7 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
     
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) { print("Hello, world")
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) { print("Hello, world, for branch to merge")
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
