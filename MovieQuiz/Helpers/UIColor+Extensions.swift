5472a61521905f5441221ea366ceca328032e51f
xholy6 <blast6xgit@gmail.com>
2022-10-21 23:40:13 +0300

    Завершено приложение исходя из ТЗ по спринту 4

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,4 +1,12 @@
 import Foundation
 import UIKit
 
-extension UIColor { }
+extension UIColor {
+    static var ypRed: UIColor { UIColor (named: "red")! }
+    static var ypGreen: UIColor { UIColor (named: "green")! }
+    static var ypWhite: UIColor { UIColor (named: "white")! }
+    static var ypBlack: UIColor { UIColor (named: "black")! }
+    static var ypGray: UIColor { UIColor (named: "gray")! }
+
+}
+
