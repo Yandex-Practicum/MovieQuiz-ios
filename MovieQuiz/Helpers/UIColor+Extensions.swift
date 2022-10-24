866a1f2aab9d8ed0906066fd8ffcf385dba03be0
Pavel <paveltihtey@gmail.com>
2022-10-24 21:25:51 +0300

    sprint_4.added logic for 4th sprint

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,4 +1,7 @@
 import Foundation
 import UIKit
 
-extension UIColor { }
+extension UIColor {
+    static let ypGreen = UIColor(red: 96/256, green: 194/256, blue: 142/256, alpha: 1)
+    static let ypRed = UIColor(red: 245/256, green: 107/256, blue: 108/256, alpha: 1)
+}
