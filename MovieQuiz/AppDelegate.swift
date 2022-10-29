725eaba46c1eb81e6c19978a8cddd432ac63c70b
Dmitry Ivanov <f1-ks@mail.ru>
2022-10-29 15:24:07 +0300

    Добавили ещё немного текста в нашу ветку

--- MovieQuiz/AppDelegate.swift
+++ MovieQuiz/AppDelegate.swift
@@ -12,6 +12,7 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         print("Hello world for branch to merge")
+        print("Тут добавим ещё текста")
         return true
     }
 
