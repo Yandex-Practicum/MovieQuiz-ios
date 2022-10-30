50052b1728a47102b9766409b0b6aebd120c742c
ozik <72821761+ozik1959@users.noreply.github.com>
2022-10-30 20:55:25 +0300

    Добавил слабую ссылку в презенте на ViewController

--- MovieQuiz/Presentation/AlertPresenter.swift
+++ MovieQuiz/Presentation/AlertPresenter.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 class AlertPresenter: AlertPresenterDelegate {
-    var startOverDelegate: AlertPresentProtocol?
+    weak var startOverDelegate: AlertPresentProtocol?
     init(startOverDelegate: AlertPresentProtocol) {
         self.startOverDelegate = startOverDelegate
     }
@@ -10,7 +10,8 @@ class AlertPresenter: AlertPresenterDelegate {
                                       message: alertModel.masseg,
                                       preferredStyle: .alert)
         let action = UIAlertAction(title: "Ok", style: .cancel) { [weak self]_ in
-            self?.startOverDelegate?.startOver()
+            guard let self = self else { return }
+            self.startOverDelegate?.startOver()
         }
         alert.addAction(action)
         return alert
--- MovieQuiz/Servises/AlertPresentProtocol.swift
+++ MovieQuiz/Servises/AlertPresentProtocol.swift
@@ -1,5 +1,5 @@
 import Foundation
 
-protocol AlertPresentProtocol {
+protocol AlertPresentProtocol: AnyObject {
     func startOver()
 }
