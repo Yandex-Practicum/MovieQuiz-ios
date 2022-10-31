d88583e91bfa6445843251b367a3df0b9c3bb0af
Roman <31464179+grand-roman@users.noreply.github.com>
2022-10-31 11:10:32 +0700

    Update MovieQuizViewController.swift

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -70,6 +70,7 @@ final class MovieQuizViewController: UIViewController {
     ]
     
     private var currentQuestionIndex: Int = 0
+    private var correctAnswers: Int = 0
 
     @IBOutlet private var counterLabel: UILabel!
     @IBOutlet private var imageView: UIImageView!
@@ -115,6 +116,9 @@ final class MovieQuizViewController: UIViewController {
     }
     
     private func showAnswerResult(isCorrect: Bool) {
+        if isCorrect {
+              correctAnswers += 1
+          }
         
         imageView.layer.masksToBounds = true
         imageView.layer.borderWidth = 8
@@ -131,16 +135,16 @@ final class MovieQuizViewController: UIViewController {
     private func showNextQuestionOrResults() {
       if currentQuestionIndex == questions.count - 1 {
           // - 1 потому что индекс начинается с 0, а длинна массива — с 1
-          let text = "Ваш результат: \(1) из 10"
+          let text = "Ваш результат: \(self.correctAnswers) из 10"
           let viewModel = QuizResultsViewModel(title: "Этот раунд окончен",
                                                text: text,
                                                buttonText: "Сыграть еще раз")
-          show(quiz: viewModel)
+          self.show(quiz: viewModel)
       } else {
         currentQuestionIndex += 1 // увеличиваем индекс текущего урока на 1; таким образом мы сможем получить следующий урок
         let nextQuestion = questions[currentQuestionIndex]
         let viewModel = convert(model: nextQuestion)
-        show(quiz: viewModel)
+        self.show(quiz: viewModel)
       }
     }
     
@@ -154,104 +158,11 @@ final class MovieQuizViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        show(
+        self.show(
             quiz: self.convert(
                 model: questions[currentQuestionIndex]
             )
         )
     }
 
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        /*
-        Тут имеет смысл дополнительно настроить наши изображения, например,
-        задать цвет фона для экрана.
-        */
-    }
-
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-      /*
-      Тут имеет смысл оповестить систему аналитики о том, что экран показался.
-      */
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-       /*
-       Тут имеет смысл остановить все процессы, которые происходили на этом экране.
-       */
-     }
-    
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-        /*
-        Тут имеет смысл оповестить систему аналитики, что экран перестал показываться
-        и привести его в изначальное состояние.
-        */
-    }
 }
-
-/*
- Mock-данные
- 
- 
- Картинка: The Godfather
- Настоящий рейтинг: 9,2
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: The Dark Knight
- Настоящий рейтинг: 9
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: Kill Bill
- Настоящий рейтинг: 8,1
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: The Avengers
- Настоящий рейтинг: 8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: Deadpool
- Настоящий рейтинг: 8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: The Green Knight
- Настоящий рейтинг: 6,6
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: Old
- Настоящий рейтинг: 5,8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
-
-
- Картинка: The Ice Age Adventures of Buck Wild
- Настоящий рейтинг: 4,3
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
-
-
- Картинка: Tesla
- Настоящий рейтинг: 5,1
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
-
-
- Картинка: Vivarium
- Настоящий рейтинг: 5,8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
- */
