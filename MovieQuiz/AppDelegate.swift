765538c77b5ffcf5774e5eb1d278638f8dfc1ffe
Kolokolnikov Mikhail <kolokolnikovmb@yandex.ru>
2022-10-31 20:36:54 +0300

    new commit

--- MovieQuiz/AppDelegate.swift
+++ MovieQuiz/AppDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        print("Hello world for branch to merge") 
+        print("Hello world for branch to merge new one") 
         // Override point for customization after application launch.
         return true
     }
