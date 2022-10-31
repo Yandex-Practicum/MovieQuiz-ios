b746e0d1c5ca4cbdd07a9b574319892271ba6774
Roman <31464179+grand-roman@users.noreply.github.com>
2022-11-01 01:27:11 +0700

    fix

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,4 +1,8 @@
 import Foundation
 import UIKit
 
-extension UIColor { }
+extension UIColor {
+    static var ypBlack: UIColor { UIColor(named: "ypBlack")!}
+    static var ypGreen: UIColor { UIColor(named: "ypGreen")!}
+    static var ypRed: UIColor { UIColor(named: "ypRed")! }
+}
--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -80,19 +80,19 @@ final class MovieQuizViewController: UIViewController {
     @IBAction private func noButtonClicked(_ sender: UIButton) {
         let currentQuestion = questions[currentQuestionIndex]
         let givenAnswer = false
-        self.showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
+        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
     }
     
     @IBAction private func yesButtonClicked(_ sender: UIButton) {
         let currentQuestion = questions[currentQuestionIndex]
         let givenAnswer = true
-        self.showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
+        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
     }
     
     private func show(quiz step: QuizStepViewModel) {
-        self.imageView.image = step.image
-        self.textLabel.text = step.question
-        self.counterLabel.text = step.questionNumber
+        imageView.image = step.image
+        textLabel.text = step.question
+        counterLabel.text = step.questionNumber
     }
 
     private func show(quiz result: QuizResultsViewModel) {
@@ -112,7 +112,7 @@ final class MovieQuizViewController: UIViewController {
         
         alert.addAction(action)
         
-        self.present(alert, animated: true, completion: nil)
+        present(alert, animated: true, completion: nil)
     }
     
     private func showAnswerResult(isCorrect: Bool) {
@@ -122,7 +122,7 @@ final class MovieQuizViewController: UIViewController {
         
         imageView.layer.masksToBounds = true
         imageView.layer.borderWidth = 8
-        imageView.layer.borderColor = isCorrect ? UIColor.green.cgColor : UIColor.red.cgColor
+        imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
         
         imageView.layer.cornerRadius = 20
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
@@ -135,17 +135,17 @@ final class MovieQuizViewController: UIViewController {
     private func showNextQuestionOrResults() {
       if currentQuestionIndex == questions.count - 1 {
           // - 1 потому что индекс начинается с 0, а длинна массива — с 1
-          let text = "Ваш результат: \(self.correctAnswers) из 10"
+          let text = "Ваш результат: \(correctAnswers) из 10"
           let viewModel = QuizResultsViewModel(title: "Этот раунд окончен",
                                                text: text,
                                                buttonText: "Сыграть еще раз")
-          self.correctAnswers = 0
-          self.show(quiz: viewModel)
+          correctAnswers = 0
+          show(quiz: viewModel)
       } else {
         currentQuestionIndex += 1 // увеличиваем индекс текущего урока на 1; таким образом мы сможем получить следующий урок
         let nextQuestion = questions[currentQuestionIndex]
         let viewModel = convert(model: nextQuestion)
-        self.show(quiz: viewModel)
+        show(quiz: viewModel)
       }
     }
     
@@ -159,8 +159,9 @@ final class MovieQuizViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.show(
-            quiz: self.convert(
+        imageView.layer.cornerRadius = 20
+        show(
+            quiz: convert(
                 model: questions[currentQuestionIndex]
             )
         )
