d336b80337ebea4c3dc825703ca3d12a471b6bda
totoriole <t-punch@ya.ru>
2022-11-01 01:36:46 +0300

    Made main screen and user response processing

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,14 +1,131 @@
 import UIKit
 
+// MARK: - ViewModels
+// condition "Question asked"
+private struct QuizStepViewModel {
+    let image: UIImage
+    let question: String
+    let questionNumber: String
+}
+
+// condition "Quiz result"
+private struct QuizResultsViewModel {
+    let title: String
+    let text: String
+    let buttonText: String
+}
+
+// MARK: - MockData
+// question
+private struct QuizQuestion {
+    let image: String
+    let text: String
+    let correctAnswer: Bool
+}
+
+private let questions: [QuizQuestion] = [
+    QuizQuestion(image: "The Godfather",
+                 text: "Рейтинг этого фильма больше 7?",
+                 correctAnswer: true),
+    QuizQuestion(image: "The Dark Knight",
+                 text: "Рейтинг этого фильма больше 7?",
+                 correctAnswer: true),
+    QuizQuestion(image: "Kill Bill",
+                 text: "Рейтинг этого фильма больше 7?",
+                 correctAnswer: true),
+    QuizQuestion(image: "The Avengers",
+                 text: "Рейтинг этого фильма больше 7?",
+                 correctAnswer: true),
+    QuizQuestion(image: "Deadpool",
+                 text: "Рейтинг этого фильма больше 7?",
+                 correctAnswer: true),
+    QuizQuestion(image: "The Green Knight",
+                 text: "Рейтинг этого фильма больше 7?",
+                 correctAnswer: true),
+    QuizQuestion(image: "Old",
+                 text: "Рейтинг этого фильма больше 7?",
+                 correctAnswer: false),
+    QuizQuestion(image: "The Ice Age Adventures of Buck Wild",
+                 text: "Рейтинг этого фильма больше 7?",
+                 correctAnswer: false),
+    QuizQuestion(image: "Tesla",
+                 text: "Рейтинг этого фильма больше 7?",
+                 correctAnswer: false),
+    QuizQuestion(image: "Vivarium",
+                 text: "Рейтинг этого фильма больше 7?",
+                 correctAnswer: false)
+]
+
 final class MovieQuizViewController: UIViewController {
+    
     // MARK: - Lifecycle
     
-    @IBOutlet weak var noButton: UIButton!
-    @IBOutlet weak var yesButton: UIButton!
+    @IBOutlet private var imageView: UIImageView!
+    @IBOutlet private var textLabel: UILabel!
+    @IBOutlet private var counterLabel: UILabel!
+    
+    private var currentQuestionIndex = 0
+    private var currentQuestion: QuizQuestion { questions[currentQuestionIndex] }
+    
+    private func convert(model: QuizQuestion) -> QuizStepViewModel {
+        return QuizStepViewModel(
+            image: UIImage(named: model.image) ?? UIImage(),
+            question: model.text,
+            questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)")
+    }
+    
+    private func show(quiz step: QuizStepViewModel) {
+        // здесь мы заполняем нашу картинку, текст и счётчик данными
+        imageView.image = step.image
+        textLabel.text = step.question
+        counterLabel.text = step.questionNumber
+        
+    }
+    
+    private func show(quiz result: QuizResultsViewModel) {
+        // здесь мы показываем результат прохождения квиза
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        show(quiz: convert(model: currentQuestion))
     }
+    
+    private func showAnswerResult(isCorrect: Bool) {
+        imageView.layer.masksToBounds = true
+        imageView.layer.borderWidth = 8
+        if isCorrect {
+            imageView.layer.borderColor = UIColor.ypGreen?.cgColor
+        } else {
+            imageView.layer.borderColor = UIColor.ypRed?.cgColor
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            self.showQuestionOrResult()
+        }
+    }
+    
+    private func showQuestionOrResult() {
+        if currentQuestionIndex == questions.count - 1 { // - 1 потому что индекс начинается с 0, а длинна массива — с 1
+            // показать результат квиза
+        } else {
+            currentQuestionIndex += 1 // увеличиваем индекс текущего урока на 1; таким образом мы сможем получить следующий урок
+            // показать следующий вопрос
+        }
+    }
+    
+    
+    @IBAction private func noButtonClicked(_ sender: UIButton) {
+        let userAnswer = false
+        showAnswerResult(isCorrect: userAnswer == currentQuestion.correctAnswer)
+    }
+    @IBAction private func yesButtonClicked(_ sender: UIButton) {
+        let userAnswer = true
+        showAnswerResult(isCorrect: userAnswer == currentQuestion.correctAnswer)
+    }
+    
+    
 }
 
 /*
