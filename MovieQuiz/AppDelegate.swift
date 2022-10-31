e1bba3f5f83455e05264c8de95c485b1aa74aea2
Roman <31464179+grand-roman@users.noreply.github.com>
2022-10-31 11:13:04 +0700

    Update AppDelegate.swift

--- MovieQuiz/AppDelegate.swift
+++ MovieQuiz/AppDelegate.swift
@@ -23,7 +23,6 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        print("Hello world for branch to merge")
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
