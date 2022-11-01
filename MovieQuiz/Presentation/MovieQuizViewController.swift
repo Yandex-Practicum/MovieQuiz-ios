294f73ba8d65459abc2e2bd7918c5c55feec42c0
totoriole <t-punch@ya.ru>
2022-11-01 23:39:21 +0300

    Added switching questions and displaying quiz results

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,6 +1,8 @@
 import UIKit
 
 // MARK: - ViewModels
+// показываем View-модели на экране:
+
 // condition "Question asked"
 private struct QuizStepViewModel {
     let image: UIImage
@@ -23,6 +25,7 @@ private struct QuizQuestion {
     let correctAnswer: Bool
 }
 
+// заполняем массив mock-данными
 private let questions: [QuizQuestion] = [
     QuizQuestion(image: "The Godfather",
                  text: "Рейтинг этого фильма больше 7?",
@@ -66,8 +69,10 @@ final class MovieQuizViewController: UIViewController {
     
     private var currentQuestionIndex = 0
     private var currentQuestion: QuizQuestion { questions[currentQuestionIndex] }
+    private var correctAnswer = 0
     
     private func convert(model: QuizQuestion) -> QuizStepViewModel {
+        // преобразовываем данные модели вопроса в те, что нужно показать на экране
         return QuizStepViewModel(
             image: UIImage(named: model.image) ?? UIImage(),
             question: model.text,
@@ -79,48 +84,62 @@ final class MovieQuizViewController: UIViewController {
         imageView.image = step.image
         textLabel.text = step.question
         counterLabel.text = step.questionNumber
-        
     }
     
     private func show(quiz result: QuizResultsViewModel) {
         // здесь мы показываем результат прохождения квиза
+        let alert = UIAlertController(title: "Этот раунд окончен!", message: "Ваш результат: \(correctAnswer)/\(questions.count)", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Cыграть еще раз", style: .default, handler: { _ in
+            self.currentQuestionIndex = 0
+            self.correctAnswer = 0
+            let firstQuestionModel = self.convert(model: self.currentQuestion)
+            self.show(quiz: firstQuestionModel)
+        })
+        alert.addAction(action)
+        self.present(alert, animated: true)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         show(quiz: convert(model: currentQuestion))
     }
     
     private func showAnswerResult(isCorrect: Bool) {
+        // отображаем результат ответа (выделяем рамкой верный или неверный ответ)
+        if isCorrect {
+            correctAnswer += 1
+        }
         imageView.layer.masksToBounds = true
         imageView.layer.borderWidth = 8
-        if isCorrect {
-            imageView.layer.borderColor = UIColor.ypGreen?.cgColor
-        } else {
-            imageView.layer.borderColor = UIColor.ypRed?.cgColor
-        }
-        
+        imageView.layer.cornerRadius = 15
+        imageView.layer.borderColor = isCorrect ? UIColor.ypGreen?.cgColor : UIColor.ypRed?.cgColor
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
             self.showQuestionOrResult()
+            self.imageView.layer.borderWidth = 0
         }
     }
     
     private func showQuestionOrResult() {
         if currentQuestionIndex == questions.count - 1 { // - 1 потому что индекс начинается с 0, а длинна массива — с 1
             // показать результат квиза
+            let viewModel = QuizResultsViewModel(title: "Этот раунд окончен!", text: "Ваш результат: \(correctAnswer)/\(questions.count)", buttonText: "Cыграть еще раз")
+            show(quiz: viewModel)
         } else {
             currentQuestionIndex += 1 // увеличиваем индекс текущего урока на 1; таким образом мы сможем получить следующий урок
             // показать следующий вопрос
+            let quizQuestion = convert(model: currentQuestion)
+            show(quiz: quizQuestion)
         }
     }
     
     
     @IBAction private func noButtonClicked(_ sender: UIButton) {
+        // проверка ответа
         let userAnswer = false
         showAnswerResult(isCorrect: userAnswer == currentQuestion.correctAnswer)
     }
     @IBAction private func yesButtonClicked(_ sender: UIButton) {
+        // проверка ответа
         let userAnswer = true
         showAnswerResult(isCorrect: userAnswer == currentQuestion.correctAnswer)
     }
