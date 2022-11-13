6037c07ffef26080959efc05b5ec66802d3cb84c
Andrey Fedin <mondaine.ru@yandex.ru>
2022-11-13 18:53:26 +0300

    add buttons

---
+++ MovieQuiz/Resources/UIColor.swift
@@ -0,0 +1,18 @@
+//
+//  UIColor.swift
+//  MovieQuiz
+//
+//  Created by user on 11.11.2022.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static var ypBlack: UIColor { UIColor(named: "Black")! }
+    static var ypBackground: UIColor { UIColor(named: "Background")! }
+    static var ypGray: UIColor { UIColor(named: "Gray")! }
+    static var ypGreen: UIColor { UIColor(named: "Green")! }
+    static var ypRed: UIColor { UIColor(named: "Red")! }
+    static var ypWhite: UIColor { UIColor(named: "White")! }
+}
