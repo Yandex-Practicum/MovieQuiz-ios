aeeb43bd682491f8771ff59325b294739d3f9d1f
DmitriiMM <martinovit72@gmail.com>
2022-11-13 23:45:35 +0400

    changed names colors

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -2,11 +2,11 @@ import Foundation
 import UIKit
 
 extension UIColor {
-    static let ypBackground = UIColor(named: "YP Background")!
-    static let ypBlack = UIColor(named: "YP Black")!
-    static let ypGreen = UIColor(named: "YP Green")!
-    static let ypGrey = UIColor(named: "YP Grey")!
-    static let ypRed = UIColor(named: "YP Red")!
-    static let ypWhite = UIColor(named: "YP White")!
+    static let ypBackground = UIColor(named: "YP background")!
+    static let ypBlack = UIColor(named: "YP black")!
+    static let ypGreen = UIColor(named: "YP green")!
+    static let ypGrey = UIColor(named: "YP grey")!
+    static let ypRed = UIColor(named: "YP red")!
+    static let ypWhite = UIColor(named: "YP white")!
 }
 
