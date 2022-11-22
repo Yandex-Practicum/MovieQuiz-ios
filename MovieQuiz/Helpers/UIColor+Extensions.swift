fc5c9c2d877ddfddfa739c0d28a9016638bf3d41
marinapisareva <m.pisareva@esforce.com>
2022-11-22 22:29:11 +0300

    sprint_04: created layout with basic functionality

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,4 +1,11 @@
 import Foundation
 import UIKit
 
-extension UIColor { }
+extension UIColor {
+    static var ypBlack: UIColor { UIColor(named: "yp.Black")! }
+    static var ypWhite: UIColor { UIColor(named: "yp.White")! }
+    static var ypGreen: UIColor { UIColor(named: "yp.Green")! }
+    static var ypRed: UIColor { UIColor(named: "yp.Red")! }
+    static var ypGray: UIColor { UIColor(named: "yp.Gray")! }
+    static var ypDarkGray: UIColor { UIColor(named: "yp.DarkGray")! }
+}
