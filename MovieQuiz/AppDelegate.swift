465651f72d8eedd34139bb677b6e15437af276c8
olzrchik <olgalazarchik997@gmail.com>
2022-11-23 17:06:16 +0100

    первая версия проекта создана

--- MovieQuiz/AppDelegate.swift
+++ MovieQuiz/AppDelegate.swift
@@ -7,10 +7,11 @@
 
 import UIKit
 
-@main
+    @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        sleep(3)
         return true
     }
 
