8550be6023c324ec617999a65c9b7f5a94c68e09
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2022-11-23 16:12:24 +0500

    Adds UIColor extension

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,4 +1,10 @@
 import Foundation
 import UIKit
 
-extension UIColor { }
+extension UIColor {
+    static let ypBlack = UIColor(named: "black")
+    static let ypRed = UIColor(named: "red")
+    static let ypGreen = UIColor(named: "green")
+    static let ypGray = UIColor(named: "gray")
+    static let ypBackground = UIColor(named: "background")
+}
