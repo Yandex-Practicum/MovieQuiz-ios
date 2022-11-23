8c7c0eb0e472c5a59534473d05efc63dfce38ca7
Nikita Chagochkin <nick.chagochkin@yandex.ru>
2022-11-23 18:12:27 +0300

    sprint_04 task

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,4 +1,23 @@
 import Foundation
 import UIKit
 
-extension UIColor { }
+extension UIColor {
+  static var ypBlack: UIColor {
+    UIColor(named: "YP Black")!
+  }
+  static var ypWhite: UIColor {
+    UIColor(named: "YP White")!
+  }
+  static var ypGray: UIColor {
+    UIColor(named: "YP Gray")!
+  }
+  static var ypGreen: UIColor {
+    UIColor(named: "YP Green")!
+  }
+  static var ypRed: UIColor {
+    UIColor(named: "YP Red")!
+  }
+  static var ypDarkGray: UIColor {
+    UIColor(named: "YP DarkGray")!
+  }
+}
---
+++ MovieQuiz/Helpers/UIFont+Extensions.swift
@@ -0,0 +1,11 @@
+
+import UIKit
+
+extension UIFont {
+  static var YSDisplay_Medium: UIFont {
+    UIFont(name: "YSDisplay-Medium", size: 20)!
+  }
+  static var YSDisplay_Bold: UIFont {
+    UIFont(name: "YSDisplay-Bold", size: 20)!
+  }
+}
