182ba0ece9b1ae3cc35a76edadaa0f1107bc9cfa
YagudinDD <ddyagudin@yandex.ru>
2022-11-23 14:17:28 +0300

    осталась ошибка загрузки экрана на стадии viewDidLoad

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -7,25 +7,71 @@ final class MovieQuizViewController: UIViewController {
     @IBOutlet private var counterLabel: UILabel!
     @IBOutlet private var imageView: UIImageView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
-    @IBAction private func yesButtonClicked(_ sender: Any) {
-    }
-    
-    @IBAction private func noButtonClicked(_ sender: Any) {
-    }
+    private let questions : [QuizQuestion] = [
+       QuizQuestion (
+           image: "The Godfather",
+           text: "Рейтинг этого фильма больше чем 6?",
+           correctAnswer: true),
+       QuizQuestion (
+           image: "The Dark Knight",
+           text: "Рейтинг этого фильма больше чем 6?",
+           correctAnswer: true),
+       QuizQuestion (
+           image: "Kill Bill",
+           text: "Рейтинг этого фильма больше чем 6?",
+           correctAnswer: true),
+       QuizQuestion (
+           image: "The Avengers",
+           text: "Рейтинг этого фильма больше чем 6?",
+           correctAnswer: true),
+       QuizQuestion (
+           image: "Deadpool",
+           text: "Рейтинг этого фильма больше чем 6?",
+           correctAnswer: true),
+       QuizQuestion (
+           image: "The Green Knight",
+           text: "Рейтинг этого фильма больше чем 6?",
+           correctAnswer: true),
+       QuizQuestion (
+           image: "Old",
+           text: "Рейтинг этого фильма больше чем 6?",
+           correctAnswer: false),
+       QuizQuestion (
+           image: "The Ice Age Adventures of Buck Wild",
+           text: "Рейтинг этого фильма больше чем 6?",
+           correctAnswer: false),
+       QuizQuestion (
+           image: "Tesla",
+           text: "Рейтинг этого фильма больше чем 6?",
+           correctAnswer: false),
+       QuizQuestion (
+           image: "Vivarium",
+           text: "Рейтинг этого фильма больше чем 6?",
+           correctAnswer: false),
+   ]
+    private var currentQuestionIndex: Int = 0
+    var currentQuestion: QuizQuestion { questions[currentQuestionIndex] }
     
     private func show(quiz step: QuizStepViewModel) {
+        imageView.image = step.image
+        textLabel.text = step.question
+        counterLabel.text = step.question
       // здесь мы заполняем нашу картинку, текст и счётчик данными
-        
+
     }
 
     private func show(quiz result: QuizResultsViewModel) {
       // здесь мы показываем результат прохождения квиза
     }
-    // для состояния "Вопрос задан"
+    
+    private func convert (model:QuizQuestion) -> QuizStepViewModel{
+        return QuizStepViewModel (
+            image: UIImage (named: model.image) ?? UIImage(),
+            question: model.text,
+            questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)")
+    }
+    
+     //для состояния "Вопрос задан"
 struct QuizStepViewModel {
   let image: UIImage
   let question: String
@@ -43,51 +89,20 @@ struct QuizQuestion {
     let image: String
     let text: String
     let correctAnswer: Bool
-
- private let question : [QuizQuestion] = [
-    QuizQuestion (
-        image: "The Godfather",
-        text: "Рейтинг этого фильма больше чем 6?",
-        correctAnswer: true),
-    QuizQuestion (
-        image: "The Dark Knight",
-        text: "Рейтинг этого фильма больше чем 6?",
-        correctAnswer: true),
-    QuizQuestion (
-        image: "Kill Bill",
-        text: "Рейтинг этого фильма больше чем 6?",
-        correctAnswer: true),
-    QuizQuestion (
-        image: "The Avengers",
-        text: "Рейтинг этого фильма больше чем 6?",
-        correctAnswer: true),
-    QuizQuestion (
-        image: "Deadpool",
-        text: "Рейтинг этого фильма больше чем 6?",
-        correctAnswer: true),
-    QuizQuestion (
-        image: "The Green Knight",
-        text: "Рейтинг этого фильма больше чем 6?",
-        correctAnswer: true),
-    QuizQuestion (
-        image: "Old",
-        text: "Рейтинг этого фильма больше чем 6?",
-        correctAnswer: false),
-    QuizQuestion (
-        image: "The Ice Age Adventures of Buck Wild",
-        text: "Рейтинг этого фильма больше чем 6?",
-        correctAnswer: false),
-    QuizQuestion (
-        image: "Tesla",
-        text: "Рейтинг этого фильма больше чем 6?",
-        correctAnswer: false),
-    QuizQuestion (
-        image: "Vivarium",
-        text: "Рейтинг этого фильма больше чем 6?",
-        correctAnswer: false),
-]
 }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.show(quiz step: QuizStepViewModel)
+    }
+
+    @IBAction private func yesButtonClicked(_ sender: Any) {
+    }
+    
+    @IBAction private func noButtonClicked(_ sender: Any) {
+    }
+    
+   
 
 /*
  Картинка: The Godfather
