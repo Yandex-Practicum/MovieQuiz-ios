43b3376a5afdf384f8d65bf68d0098aba7813dec
Stanny-Starczewski <spaceboyua@yahoo.com>
2022-11-28 16:32:52 +0200

    home work 4 sprint

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,9 +1,186 @@
 import UIKit
 
 final class MovieQuizViewController: UIViewController {
-    // MARK: - Lifecycle
+    
+    @IBOutlet weak private var counterLabel: UILabel!
+    @IBOutlet weak private var imageView: UIImageView!
+    @IBOutlet weak private var textLabel: UILabel!
+    
+    private var currentQuestionIndex: Int = 0
+    var currentQuestion: QuizQuestion {questions[currentQuestionIndex]}
+    
+    private var correctAnswers: Int = 0
+    
+    struct QuizQuestion {
+      let image: String
+      let text: String
+      let correctAnswer: Bool
+    }
+
+    private let questions: [QuizQuestion] = [
+        QuizQuestion(
+            image: "The Godfather",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "The Dark Knight",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "Kill Bill",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "The Avengers",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "Deadpool",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "The Green Knight",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "Old",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false),
+        QuizQuestion(
+            image: "The Ice Age Adventures of Buck Wild",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false),
+        QuizQuestion(
+            image: "Tesla",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false),
+        QuizQuestion(
+            image: "Vivarium",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false)
+    ]
+        
+    // для состояния "Вопрос задан"
+    struct QuizStepViewModel {
+      let image: UIImage
+      let question: String
+      let questionNumber: String
+    }
+
+    // для состояния "Результат квиза"
+    struct QuizResultsViewModel {
+      let title: String
+      let text: String
+      let buttonText: String
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        guard let
+            firstQuestion = questions.first
+        else {
+            return
+        }
+        let firstQuestionViewModel = convert(model: firstQuestion)
+        self.show(quiz: firstQuestionViewModel)
+    }
+    
+    @IBAction private func noButtonClicked(_ sender: Any) {
+        var currentQuestion: QuizQuestion {questions[currentQuestionIndex]}
+        let givenAnswer = false
+        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
+    }
+    
+    @IBAction private func yesButtonClicked(_ sender: Any) {
+        var currentQuestion: QuizQuestion {questions[currentQuestionIndex]}
+        let givenAnswer = true
+        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
+    }
+    
+    private func show(quiz step: QuizStepViewModel) {
+      // здесь мы заполняем нашу картинку, текст и счётчик данными
+        imageView.image = step.image
+        textLabel.text = step.question
+        counterLabel.text = step.questionNumber
+    }
+
+    private func show(quiz result: QuizResultsViewModel) {
+      // здесь мы показываем результат прохождения квиза
+        
+        // создаём объекты всплывающего окна
+        let alert = UIAlertController(
+            title: result.title, // заголовок всплывающего окна
+            message: result.text, // текст во всплывающем окне
+            preferredStyle: .alert) // preferredStyle может быть .alert или .actionSheet
+
+        // создаём для него кнопки с действиями ("Сыграть ещё раз")
+        let action = UIAlertAction(
+            title: result.buttonText,
+            style: .default,
+            handler: { _ in
+            // вот этот код с переключением индекса и показом первого вопроса надо будет написать тут
+            self.currentQuestionIndex = 0
+                
+            // скидываем счётчик правильных ответов
+            self.correctAnswers = 0
+                
+            // заново показываем первый вопрос
+            let firstQuestion = self.questions[self.currentQuestionIndex]
+            let viewModel = self.convert(model: firstQuestion)
+            self.show(quiz: viewModel)
+        })
+
+        // добавляем в алерт кнопки
+        alert.addAction(action)
+
+        // показываем всплывающее окно
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    private func convert(model: QuizQuestion) -> QuizStepViewModel {
+      // Попробуйте написать код конвертации сами
+        return QuizStepViewModel(
+            image: UIImage(named: model.image) ?? UIImage(),
+            question: model.text,
+            questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)")
+      }
+    
+    private func showAnswerResult(isCorrect: Bool) {
+        if isCorrect {
+            correctAnswers += 1
+        }
+        
+        imageView.layer.masksToBounds = true // даём разрешение на рисование рамки
+        imageView.layer.borderWidth = 8 // толщина рамки
+        imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor// делаем рамку зеленой (правильный ответ) или красной (не правильный ответ)
+        imageView.layer.cornerRadius = 20 // радиус скругления углов рамки
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { // запускаем задачу через 1 секунду
+            // код, который вы хотите вызвать через 1 секунду,
+            self.showNextQuestionOrResults()
+        }
+    }
+    
+    private func showNextQuestionOrResults() {
+        
+        imageView.layer.borderColor = UIColor.clear.cgColor // очищаем цвет рамки перед следующим вопросом
+        
+      if currentQuestionIndex == questions.count - 1 { // - 1 потому что индекс начинается с 0, а длинна массива — с 1
+        // показать результат квиза
+          let text = "Ваш результат: \(correctAnswers) из 10"
+          let viewModel = QuizResultsViewModel(
+                title: "Этот раунд окончен!",
+                text: text,
+                buttonText: "Сыграть ещё раз")
+                show(quiz: viewModel)
+      } else {
+        currentQuestionIndex += 1 // увеличиваем индекс текущего урока на 1; таким образом мы сможем получить следующий урок
+        // показать следующий вопрос
+        let nextQuestion = questions[currentQuestionIndex]
+        let viewModel = convert(model: nextQuestion)
+                  	
+        show(quiz: viewModel)
+      }
     }
 }
 
