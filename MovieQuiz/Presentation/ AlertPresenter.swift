2adaeb837d1ed0a9c3626a2395a7b2764a9ddbd2
NutsCraker <farizanov@gmail.com>
2022-12-06 18:48:37 +0300

    не выходи реализовать корректный функционал по кнопке Сыграть еще....

--- MovieQuiz/Presentation/ AlertPresenter.swift
+++ MovieQuiz/Presentation/ AlertPresenter.swift
@@ -19,7 +19,7 @@ class AlertPresenter {
     
     func show(result : AlertModel) {
         let alert = UIAlertController(title: result.title, message: result.message, preferredStyle: .alert)
-        let action = UIAlertAction(title: result.buttonText, style: .default ) { _ in self.questionFactory?.requestNextQuestion()}
+        let action = UIAlertAction(title: result.buttonText, style: .default, handler: { (action) in  self.questionFactory?.requestNextQuestion()} )
    
         alert.addAction(action)
         
