8106f948ae8f7d78360aa84cf283551ac55b3b31
Stanny-Starczewski <spaceboyua@yahoo.com>
2022-12-09 22:05:21 +0200

    Задание по Спринт 5/10: 5 → Тема 3/4: Хранение данных → Урок 7/10: Структура JSON и сериализация. Задание (авторское решение)

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -49,20 +49,63 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate {
         let data = jsonString?.data(using: .utf8)!
         
         //class func jsonObject(with data: Data, options opt: JSONSerialization.ReadingOptions = []) throws -> Any
-        
+        struct Actor {
+            let id: String
+            let image: String
+            let name: String
+            let asCharacter: String
+        }
+        struct Movie {
+            let id: String
+            let title: String
+            let year: Int
+            let image: String
+            let releaseDate: String
+            let runtimeMins: Int
+            let directors: String
+            let actorList: [Actor]
+        }
         do {
             let json = try JSONSerialization.jsonObject(with: data!, options: []) as? [String: Any]
-            let title = json?["title"]
-            //print(json)
-            let year = json?["year"]
-            print(json)
-            let actorList = json!["actorList"] as! [Any]
-            
-            for actor in actorList {
-                if let actor = actor as? [String: Any] {
-                    print(actor["asCharacter"])
-                }
+
+              guard let json = json,
+                    let id = json["id"] as? String,
+                    let title = json["title"] as? String,
+                    let jsonYear = json["year"] as? String,
+                    let year = Int(jsonYear),
+                    let image = json["image"] as? String,
+                    let releaseDate = json["releaseDate"] as? String,
+                    let jsonRuntimeMins = json["runtimeMins"] as? String,
+                    let runtimeMins = Int(jsonRuntimeMins),
+                    let directors = json["directors"] as? String,
+                    let actorList = json["actorList"] as? [Any] else {
+                return
             }
+
+            var actors: [Actor] = []
+
+            for actor in actorList {
+                guard let actor = actor as? [String: Any],
+                        let id = actor["id"] as? String,
+                        let image = actor["image"] as? String,
+                        let name = actor["name"] as? String,
+                        let asCharacter = actor["asCharacter"] as? String else {
+                    return
+                }
+                let mainActor = Actor(id: id,
+                                        image: image,
+                                        name: name,
+                                        asCharacter: asCharacter)
+                actors.append(mainActor)
+            }
+            let movie = Movie(id: id,
+                                title: title,
+                                year: year,
+                                image: image,
+                                releaseDate: releaseDate,
+                                runtimeMins: runtimeMins,
+                                directors: directors,
+                                actorList: actors)
         } catch {
             print("Failed to parse: \(jsonString)")
         }
