c468aed29237f11d5d486753f288813e5bfefed1
KellerDmitriy <dmit.keller@gmail.com>
2022-12-12 23:58:52 +0300

    create interface

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,4 +1,16 @@
 import Foundation
 import UIKit
 
-extension UIColor { }
+extension UIColor {
+    static var ypBlack: UIColor { UIColor(named: "black")!}
+    static var ypWhite: UIColor { UIColor(named: "white")!}
+    static var ypGreen: UIColor { UIColor(named: "green")!}
+    static var ypRed: UIColor { UIColor(named: "red")!}
+    static var ypGrey: UIColor { UIColor(named: "grey")!}
+    static var ypBackground: UIColor { UIColor(named: "background")!}
+    }
+
+        
+    
+    
+
