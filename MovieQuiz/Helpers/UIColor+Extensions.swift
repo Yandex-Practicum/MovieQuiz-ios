3db12dfb7bfcad4689e9846e5c1391542533b0ab
BROlegovich <regspeedy@gmail.com>
2022-12-17 19:42:13 +0300

    all changes

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,4 +1,12 @@
 import Foundation
 import UIKit
 
-extension UIColor { }
+extension UIColor {
+    static var ypBlack: UIColor { UIColor (named: "ypBlack")! }
+    static var ypwhite: UIColor { UIColor (named: "ypWhite")! }
+    static var ypGreen: UIColor { UIColor (named: "ypGreen")! }
+    static var ypRed: UIColor { UIColor (named: "ypRed")! }
+    static var ypGrey: UIColor { UIColor (named: "ypGrey")! }
+    static var ypBackground: UIColor { UIColor (named:"ypBackground")! }
+}
+        
--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -12,17 +12,17 @@ final class MovieQuizViewController: UIViewController {
         let question: String
         let questionNumber: String
     }
-
+    
     struct QuizResultViewModel {
         let title: String
         let text: String
         let buttonText: String
     }
-
+    
     struct QuizQuestion {
-      let image: String
-      let text: String
-      let correctAnswer: Bool
+        let image: String
+        let text: String
+        let correctAnswer: Bool
     }
     
     private let questions: [QuizQuestion] = [
@@ -35,9 +35,9 @@ final class MovieQuizViewController: UIViewController {
             text: "Рейтинг этого фильма больше чем 6?",
             correctAnswer: true),
         QuizQuestion(
-                image: "Kill Bill",
-                text: "Рейтинг этого фильма больше чем 6?",
-                correctAnswer: true),
+            image: "Kill Bill",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
         QuizQuestion(
             image: "The Avengers",
             text: "Рейтинг этого фильма больше чем 6?",
@@ -67,7 +67,7 @@ final class MovieQuizViewController: UIViewController {
             text: "Рейтинг этого фильма больше чем 6?",
             correctAnswer: false),
     ]
-
+    
     
     private var currentQuestionIndex: Int = 0
     private var currentQuestion : QuizQuestion {questions[currentQuestionIndex]}
@@ -93,7 +93,7 @@ final class MovieQuizViewController: UIViewController {
             title: result.title,
             message: result.text,
             preferredStyle: .alert)
-
+        
         let action = UIAlertAction(title: result.buttonText, style: .default) {_ in self.currentQuestionIndex = 0
             
             self.correctAnswers = 0
@@ -102,18 +102,18 @@ final class MovieQuizViewController: UIViewController {
             let viewModel = self.convert(model: firstQuestion)
             self.show(quiz: viewModel)
         }
-
+        
         alert.addAction(action)
-
+        
         self.present(alert, animated: true, completion: nil)
-
+        
     }
     
     private func convert(model: QuizQuestion) -> QuizStepViewModel {
         return QuizStepViewModel(image: UIImage(named: model.image) ?? UIImage(),
                                  question: model.text,
                                  questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)")
-      }
+    }
     
     private func showAnswerResult(isCorrect: Bool) {
         if isCorrect {
@@ -121,7 +121,7 @@ final class MovieQuizViewController: UIViewController {
         }
         imageView.layer.masksToBounds = true
         imageView.layer.borderWidth = 8
-        imageView.layer.borderColor = isCorrect ? UIColor.green.cgColor : UIColor.red.cgColor
+        imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
         
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
             self.showNextQuestionOrResults()
@@ -154,13 +154,24 @@ final class MovieQuizViewController: UIViewController {
     @IBAction private func noButtonClicked(_ sender: UIButton) {
         let currentQuestion = questions[currentQuestionIndex]
         let givenAnswer = false
+        let button = sender
+        delayedResult(button: button)
         showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
     }
     
     @IBAction private func yesButtonClicked(_ sender: UIButton) {
         let currentQuestion = questions[currentQuestionIndex]
         let givenAnswer = true
+        let button = sender
+        delayedResult(button: button)
         showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
+    }
+    
+    func delayedResult(button : UIButton) {
+        button.isEnabled = false
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            button.isEnabled = true
+        }
     }
 }
 
