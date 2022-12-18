fddfaa300bbb62ac657d2c2b1eb3edefbb7528e7
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2022-12-18 19:36:31 +0500

    Adds a weak self to the closure capture list

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -53,7 +53,8 @@ final class MovieQuizViewController: UIViewController {
                                       message: result.text,
                                       preferredStyle: .alert)
         
-        let alertAction = UIAlertAction(title: result.buttonText, style: .default) { _ in
+        let alertAction = UIAlertAction(title: result.buttonText, style: .default) { [weak self] _ in
+            guard let self = self else { return }
             self.currentQuestionIndex = 0
             self.correctAnswers = 0
             let firstQuestion = self.questions[self.currentQuestionIndex]
@@ -82,7 +83,8 @@ final class MovieQuizViewController: UIViewController {
         yesButton.isEnabled = false
         noButton.isEnabled = false
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak self] in
+            guard let self = self else { return }
             self.showNextQuestionOrResults()
             
             self.yesButton.isEnabled = true
