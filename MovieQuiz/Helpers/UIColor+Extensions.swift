898f6ebfb3c078999c2d7b3d30b8e2bdafc31fa2
mpashuk-jr <116366043+mpashuk-jr@users.noreply.github.com>
2022-12-22 12:13:13 +0300

    ланч скрин + шрифты + цвета + элементы в интерфейс билдере + констрейты

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,4 +1,11 @@
 import Foundation
 import UIKit
 
-extension UIColor { }
+extension UIColor {
+       static var ypBlack: UIColor { UIColor(named: "black")! }
+    static var ypWhite: UIColor { UIColor(named: "ypWhite")! }
+    static var ypGreen: UIColor { UIColor(named: "ypGreen")! }
+    static var ypBackground: UIColor { UIColor(named: "ypBackground")! }
+    static var ypRed: UIColor { UIColor(named: "ypRed")! }
+    static var ypGray: UIColor { UIColor(named: "ypGray")! }
+}
