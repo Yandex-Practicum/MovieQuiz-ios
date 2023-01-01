31c9d8edb5d753a22042b5c87a3697058630c042
Diana Simonyan <simonyand95@gmail.com>
2023-01-01 22:57:21 +0300

    testing again and again

--- MovieQuiz/AppDelegate.swift
+++ MovieQuiz/AppDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        print("Hello world for branch to merges_")
+        print("Hello world for branch to merg")
         // Override point for customization after application launch.
         return true
     }
