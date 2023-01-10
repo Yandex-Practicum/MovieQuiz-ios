262c18413522d08aac4b454a943b07d1835cf6e1
Andrey Paramonov <a.paramonov@dodopizza.com>
2023-01-10 22:45:33 +0600

    Use weak ref

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -4,65 +4,66 @@ final class MovieQuizViewController: UIViewController {
     @IBOutlet private var imageView: UIImageView!
     @IBOutlet private var textLabel: UILabel!
     @IBOutlet private var counterLabel: UILabel!
-
+    
     private var currentQuestionIndex: Int = 0
     private var correctAnswers: Int = 0
     private var isAnswerButtonPressed: Bool = false
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.layer.cornerRadius = 20
         showNewQuestion()
     }
-
+    
     @IBAction func noButtonClicked(_ sender: UIButton) {
         let givenAnswer = false
         showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
     }
-
+    
     @IBAction func yesButtonClicked(_ sender: UIButton) {
         let givenAnswer = true
         showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
     }
-
+    
     private var currentQuestion:QuizQuestion {
         questions[currentQuestionIndex]
     }
-
+    
     private func showNewQuestion(){
         imageView.layer.borderWidth = 0
         let question = questions[currentQuestionIndex]
         let viewModel = convert(model: question)
         show(quiz: viewModel)
     }
-
+    
     private func show(quiz step: QuizStepViewModel) {
         let view = convert(model: currentQuestion)
         imageView.image = view.image
         textLabel.text = view.question
         counterLabel.text = view.questionNumber
     }
-
+    
     private func show(quiz result: QuizResultsViewModel) {
         let alert = UIAlertController(
             title: result.title,
             message: result.text,
             preferredStyle: .alert)
-        let action = UIAlertAction(title: result.buttonText, style: .default) { _ in
+        let action = UIAlertAction(title: result.buttonText, style: .default) { [weak self] _ in
+            guard let self else {return}
             (self.currentQuestionIndex, self.correctAnswers) = (0,0)
             self.showNewQuestion()
         }
         alert.addAction(action)
         self.present(alert, animated: true, completion: nil)
     }
-
+    
     private func convert(model: QuizQuestion) -> QuizStepViewModel {
         return QuizStepViewModel(
             image: UIImage(named: model.image) ?? UIImage(),
             question: model.text,
             questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)")
     }
-
+    
     private func showAnswerResult(isCorrect: Bool) {
         guard isAnswerButtonPressed else {
             isAnswerButtonPressed = true
@@ -70,20 +71,21 @@ final class MovieQuizViewController: UIViewController {
                 correctAnswers += 1
             }
             drawBorder(color: isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor)
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak self] in
+                guard let self else {return}
                 self.showNextQuestionOrResults()
                 self.isAnswerButtonPressed = false
             }
             return
         }
     }
-
+    
     private func drawBorder(color: CGColor) {
         imageView.layer.masksToBounds = true
         imageView.layer.borderWidth = 8
         imageView.layer.borderColor = color
     }
-
+    
     private func showNextQuestionOrResults() {
         if currentQuestionIndex == questions.count - 1 {
             let text = "Ваш результат: \(correctAnswers) из 10"
@@ -97,7 +99,7 @@ final class MovieQuizViewController: UIViewController {
             showNewQuestion()
         }
     }
-
+    
     private let questions: [QuizQuestion] = [
         QuizQuestion(
             image: "The Godfather",
@@ -168,62 +170,62 @@ struct QuizResultsViewModel {
 
 /*
  Mock-данные
-
-
+ 
+ 
  Картинка: The Godfather
  Настоящий рейтинг: 9,2
  Вопрос: Рейтинг этого фильма больше чем 6?
  Ответ: ДА
-
-
+ 
+ 
  Картинка: The Dark Knight
  Настоящий рейтинг: 9
  Вопрос: Рейтинг этого фильма больше чем 6?
  Ответ: ДА
-
-
+ 
+ 
  Картинка: Kill Bill
  Настоящий рейтинг: 8,1
  Вопрос: Рейтинг этого фильма больше чем 6?
  Ответ: ДА
-
-
+ 
+ 
  Картинка: The Avengers
  Настоящий рейтинг: 8
  Вопрос: Рейтинг этого фильма больше чем 6?
  Ответ: ДА
-
-
+ 
+ 
  Картинка: Deadpool
  Настоящий рейтинг: 8
  Вопрос: Рейтинг этого фильма больше чем 6?
  Ответ: ДА
-
-
+ 
+ 
  Картинка: The Green Knight
  Настоящий рейтинг: 6,6
  Вопрос: Рейтинг этого фильма больше чем 6?
  Ответ: ДА
-
-
+ 
+ 
  Картинка: Old
  Настоящий рейтинг: 5,8
  Вопрос: Рейтинг этого фильма больше чем 6?
  Ответ: НЕТ
-
-
+ 
+ 
  Картинка: The Ice Age Adventures of Buck Wild
  Настоящий рейтинг: 4,3
  Вопрос: Рейтинг этого фильма больше чем 6?
  Ответ: НЕТ
-
-
+ 
+ 
  Картинка: Tesla
  Настоящий рейтинг: 5,1
  Вопрос: Рейтинг этого фильма больше чем 6?
  Ответ: НЕТ
-
-
+ 
+ 
  Картинка: Vivarium
  Настоящий рейтинг: 5,8
  Вопрос: Рейтинг этого фильма больше чем 6?
