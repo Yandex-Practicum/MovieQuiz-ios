9c41ad8e04d8e69e97c402ed5f7a37ff46e07a58
Andrey Paramonov <a.paramonov@dodopizza.com>
2023-01-12 22:31:24 +0600

    Add HTTP client

---
+++ MovieQuiz/Services/MoviesLoader.swift
@@ -0,0 +1,38 @@
+//
+// Created by Андрей Парамонов on 12.01.2023.
+//
+
+import Foundation
+
+protocol MoviesLoading {
+    func loadMovies(handler: @escaping (Result<MostPopularMovies, Error>) -> Void)
+}
+
+struct MoviesLoader: MoviesLoading {
+    private let networkClient = NetworkClient()
+    private let mostPopularMoviesUrl: URL
+
+    public init() {
+        let textUrl = "https://imdb-api.com/en/API/Top250Movies/k_4w0p6ftx"
+        guard let url = URL(string: textUrl) else {
+            preconditionFailure("Unable to construct url from string: \(textUrl)")
+        }
+        mostPopularMoviesUrl = url
+    }
+
+    func loadMovies(handler: @escaping (Result<MostPopularMovies, Error>) -> Void) {
+        networkClient.fetch(url: mostPopularMoviesUrl) { result in
+            switch result {
+            case .success(let data):
+                do {
+                    let movies = try JSONDecoder().decode(MostPopularMovies.self, from: data)
+                    handler(.success(movies))
+                } catch {
+                    handler(.failure(error))
+                }
+            case .failure(let error):
+                handler(.failure(error))
+            }
+        }
+    }
+}
