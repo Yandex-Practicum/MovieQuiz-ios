83a34c9cde677f6519d30c45b5bfe6249e3ff094
dch000 <dmitriy.ev.ch@yandex.ru>
2023-01-22 14:20:35 +0300

    Прога сделана Но не работает

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -3,12 +3,18 @@ import UIKit
 final class MovieQuizViewController: UIViewController {
     // MARK: - Lifecycle
     
+    private var currentQuestionIndex : Int = 0
+    private var correctAnswers : Int = 0
+    
     //Изображение создано и готово к показу
     override func viewDidLoad() {
         super.viewDidLoad()
         /*
          заполнение view данными
          */
+        
+        // ВЫЗОВ ПЕРВОГО ВОПРОСА
+        
     }
     
     // View собираются показать
@@ -41,6 +47,21 @@ final class MovieQuizViewController: UIViewController {
         /*
          Тут имеет смысл оповестить систему аналитики, что экран перестал показываться и привести его в изначальное состояние.
          */
+    }
+    
+    
+    // для состояния "Вопрос задан"
+    struct QuizStepViewModel {
+      let image: UIImage
+      let question: String
+      let questionNumber: String
+    }
+
+    // для состояния "Результат квиза"
+    struct QuizResultsViewModel {
+      let title: String
+      let text: String
+      let buttonText: String
     }
     
     // Реализация логики приложения
@@ -95,14 +116,95 @@ final class MovieQuizViewController: UIViewController {
     
     @IBOutlet private var imageView: UIImageView! //изображение фильма
     @IBOutlet private var textLabel: UILabel! //текст вопроса
-    @IBOutlet private var counterLabel: UILabel! // счетчик вопросов
+    @IBOutlet private var counterLabel: UILabel! // счетчик вопросов counterLabel
+    
     // Функции кнопок
     // Нажатие кнопки НЕТ
     @IBAction func noButtonClicked(_ sender: UIButton) {
+        let currentQuestion = questions[currentQuestionIndex]
+        let giveAnswer = false
+        showAnswerResult(isCorrect: giveAnswer == currentQuestion.correctAnswer)
+        
     }
     //  Нажатие кнопки ДА
     @IBAction func yesButtonClicked(_ sender: UIButton) {
+        let currentQuestion = questions[currentQuestionIndex]
+        let giveAnswer = true
+        showAnswerResult(isCorrect: giveAnswer == currentQuestion.correctAnswer)
     }
+    
+    // здесь мы заполняем нашу картинку, текст и счётчик данными
+    private func show(quiz step: QuizStepViewModel) {
+        
+    }
+    
+    //здесь мы показываем результат прохождения квиза
+    private func show(quiz result: QuizResultsViewModel){
+        let alert = UIAlertController(
+                title: result.title, message: result.text, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: result.buttonText, style: .default) {_ in
+            self.currentQuestionIndex = 0
+            
+            //счетчик правильных ответов в 0
+            self.correctAnswers = 0
+            
+            // Заново показываем первый вопрос
+            let firstQuestion = self.questions[self.currentQuestionIndex]
+            let viewModel = self.convert(model: firstQuestion)
+            self.show(quiz: viewModel)
+        }
+        
+        alert.addAction(action)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
+    
+    //Функция конвертации из QuizQuestion в QuizStepViewModel
+    private func convert(model: QuizQuestion) -> QuizStepViewModel {
+        return QuizStepViewModel(
+         image: UIImage(named: model.image) ?? UIImage(), // распаковываем картинку
+         question: model.text, // берём текст вопроса
+         questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)") // высчитываем номер вопроса
+         
+        }
+    
+    //состояние показа результата ответа
+    private func showAnswerResult(isCorrect: Bool) {
+        //счетчик правильных ответов
+        if isCorrect {
+            correctAnswers += 1
+        }
+        //Рамки в зеленый
+        imageView.layer.masksToBounds = true
+        imageView.layer.borderWidth = 8
+        imageView.layer.borderColor = isCorrect ? UIColor.green.cgColor : UIColor.red.cgColor
+        
+        // Запуск следующего вопроса через 1 секунду
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+             self.showNextQuestionOrResults()
+            }
+    }
+    
+    //показ следующего вопроса или результата
+    private func showNextQuestionOrResults(){
+        if currentQuestionIndex == questions.count - 1 {
+        // показать результат
+            let text = "Ваш результат: \(correctAnswers) из 10"
+            let viewModel = QuizResultsViewModel(title: "Этот раунд окончен", text: text, buttonText: "Сыграть еще раз")
+            show(quiz: viewModel)
+        } else {
+            currentQuestionIndex += 1 // увеличим индекс вопроса на + 1
+        // показать вопрос
+            let nextQuestion = questions[currentQuestionIndex]
+            let viewModel = convert(model: nextQuestion)
+            
+            show(quiz: viewModel)
+        }
+    }
+    
 }
 
 
