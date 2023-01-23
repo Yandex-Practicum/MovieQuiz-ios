f8d8a1f667717f33312bc600a4d21dce43f5865b
Igor Polunin <garry.818@yandex.ru>
2023-01-23 17:16:52 +0300

    Исправьте ошибки

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -5,10 +5,10 @@ extension UIColor { }
 
 
 extension UIColor {
-       static var ypBlack: UIColor { UIColor(named: "black") ?? UIColor.black }
-       static var ypGreen: UIColor { UIColor(named: "green") ?? UIColor.green }
-       static var ypRed: UIColor { UIColor(named: "red") ?? UIColor.red }
+       static var ypBlack: UIColor { UIColor(named: "YP Black (ios)") ?? UIColor.black }
+       static var ypGreen: UIColor { UIColor(named: "YP Green (ios)") ?? UIColor.green }
+       static var ypRed: UIColor { UIColor(named: "YP Red (ios)") ?? UIColor.red }
        static var ypDarkGray: UIColor { UIColor(named: "darkGray") ?? UIColor.darkGray }
-       static var ypGray: UIColor { UIColor(named: "gray") ?? UIColor.gray }
-       static var ypWhite: UIColor { UIColor(named: "white") ?? UIColor.white }
+       static var ypGray: UIColor { UIColor(named: "YP Grey (ios)") ?? UIColor.gray }
+       static var ypWhite: UIColor { UIColor(named: "YP White (ios)") ?? UIColor.white }
 }
