8d546f0a8f09f42fb8bacd80ed66d82a0338b1b0
Alexey Litvinov <maillit@icloud.com>
2023-02-06 00:44:53 +0700

    Сделать PR

--- MovieQuiz/Presentation/AlertPresenter.swift
+++ MovieQuiz/Presentation/AlertPresenter.swift
@@ -16,17 +16,17 @@ final class AlertPresenter: AlertPresenterProtocol {
     
     func show(result: AlertModel) {
         let alert = UIAlertController(title: result.title,      // заголовок всплывающего окна
-                                      message: result.message, // текст 
+                                      message: result.message, // текст
                                       preferredStyle: .alert)
-    
+        
         let action = UIAlertAction(title: result.buttonText, style: .default) { _ in
             result.completion()
         }
-    
+        
         alert.addAction(action)
         showAlert(alert)
     }
-        
+    
     func showAlert(_ alert: UIAlertController) {
         delegate?.present(alert, animated: true)
     }
