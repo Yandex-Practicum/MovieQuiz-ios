27f8e463a6c3ba0ce9ba19bc21ec32875776bf07
Anastasiya <67753168+AnaKud@users.noreply.github.com>
2023-02-15 15:29:45 +0700

    все готово

---
+++ MovieQuiz/Helpers/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  MovieQuiz
+//
+//  Created by Никита Гончаров on 13.02.2023.
+//
+import UIKit
+
+struct Constants {
+    static let mediumFont = UIFont(name:"YS Display-Medium", size: 20)
+    static let boldFont = UIFont(name:"YS Display-Bond", size: 23)
+}
--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,3 +1,11 @@
+import Foundation
 import UIKit
 
-extension UIColor { }
+extension UIColor {
+       static var ypBlack: UIColor { UIColor(named: "YP Black (iOS)") ?? UIColor.black }
+       static var ypGreen: UIColor { UIColor(named: "YP Green (iOS)") ?? UIColor.green }
+       static var ypRed: UIColor { UIColor(named: "YP Red (iOS)") ?? UIColor.red }
+       static var ypBackGround: UIColor { UIColor(named: "YP Background (iOS)") ?? UIColor.ypBackGround }
+       static var ypGray: UIColor { UIColor(named: "YP Gray (iOS)") ?? UIColor.gray }
+       static var ypWhite: UIColor { UIColor(named: "YP White (iOS)") ?? UIColor.white }
+}
