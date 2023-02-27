5a08735a314d16ca1c7cd107785a44a77c47e539
Igor Polunin <garry.818@yandex.ru>
2023-02-27 20:43:39 +0300

    totalAccuracy

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -127,28 +127,7 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate,A
         
     }
     
-//
-//    private func show(quiz result: QuizResultsViewModel ) {
-//        let alert = UIAlertController (
-//            title: result.title,
-//            message: result.text,
-//            preferredStyle: .alert)
-//
-//        let action = UIAlertAction(title : result.buttonText, style: .default) { [weak self] _ in
-//
-//            guard let self = self else { return }
-//
-//            self.currentQuestionIndex = 0
-//            self.correctAnswers = 0 //Обнуляем счетчик правильных ответов
-//
-//            self.questionFactory?.requestNextQuestion()
-//        }
-//
-//        alert.addAction(action)
-//        self.present(alert, animated: true, completion: nil)
-//
-//    }
-    
+
     
     
     
