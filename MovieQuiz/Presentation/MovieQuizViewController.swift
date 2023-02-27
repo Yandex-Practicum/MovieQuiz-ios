4a57a7476afcb149d1c6754f39ca7699cb32b13d
nnkmk <124089478+timeXI@users.noreply.github.com>
2023-02-28 00:10:04 +0400

    1. Элементы интерфейса адаптируются под разрешения экранов iPhone, начиная с X. 2. Interface Builder не показывает ошибок с констрейнтами. 3. Добавлена блокировка кнопок после нажатия до показа следующего вопроса. 4. Удалены пустые папки Models и Services.

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,14 +1,12 @@
 import UIKit
 
 final class MovieQuizViewController: UIViewController {
-    
     @IBOutlet private var questionTitleLabel: UILabel!
     @IBOutlet private var textLabel: UILabel!
     @IBOutlet private var imageView: UIImageView!
     @IBOutlet private var counterLabel: UILabel!
     @IBOutlet private var noButton: UIButton!
     @IBOutlet private var yesButton: UIButton!
-    
     @IBAction private func noButtonClicked(_ sender: Any) {
         let currentQuestion = questions[currentQuestionIndex]
         let givenAnswer = false
@@ -21,7 +19,7 @@ final class MovieQuizViewController: UIViewController {
     }
     private var currentQuestionIndex: Int = 0
     private var correctAnswers: Int = 0
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         let currentQuestion = questions[currentQuestionIndex]
@@ -32,6 +30,10 @@ final class MovieQuizViewController: UIViewController {
         textLabel.font = UIFont(name: "YSDisplay-Bold", size: 23)
         counterLabel.font = UIFont(name: "YSDisplay-Medium", size: 20)
         questionTitleLabel.font = UIFont(name: "YSDisplay-Medium", size: 20)
+        // включаем кнопки
+        yesButton.isEnabled = true
+        noButton.isEnabled = true
+        //
        }
     
     private func convert(model: QuizQuestion) -> QuizStepViewModel {
@@ -50,17 +52,17 @@ final class MovieQuizViewController: UIViewController {
 
     }
     
-    
     private func showAnswerResult(isCorrect: Bool) {
-        
         if isCorrect {
             correctAnswers += 1
         }
-        
         imageView.layer.masksToBounds = true
         imageView.layer.borderWidth = 8
         imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
-      
+        // выключаем кнопки
+        yesButton.isEnabled = false
+        noButton.isEnabled = false
+        //
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
             self.showNextQuestionOrResults()
         }
@@ -71,7 +73,6 @@ final class MovieQuizViewController: UIViewController {
             title: result.title,
             message: result.text,
             preferredStyle: .alert)
-            
         let action = UIAlertAction(title: result.buttonText, style: .default) { _ in
             self.currentQuestionIndex = 0
             self.correctAnswers = 0
@@ -80,9 +81,7 @@ final class MovieQuizViewController: UIViewController {
             let viewModel = self.convert(model: firstQuestion)
             self.show(quiz: viewModel)
         }
-            
         alert.addAction(action)
-            
         self.present(alert, animated: true, completion: nil)
     }
     
@@ -94,11 +93,23 @@ final class MovieQuizViewController: UIViewController {
                 text: text,
                 buttonText: "Сыграть ещё раз")
             show(quiz: viewModel)
+            // включаем кнопки
+            yesButton.isEnabled = true
+            noButton.isEnabled = true
+            //
         } else {
             currentQuestionIndex += 1
+            // выключаем кнопки
+            yesButton.isEnabled = false
+            noButton.isEnabled = false
+            //
             let nextQuestion = questions[currentQuestionIndex]
             let viewModel = convert(model: nextQuestion)
             show(quiz: viewModel)
+            // включаем кнопки
+            yesButton.isEnabled = true
+            noButton.isEnabled = true
+            //
             imageView.layer.borderColor = UIColor.clear.cgColor
         }
     }
@@ -163,6 +174,4 @@ final class MovieQuizViewController: UIViewController {
             text: "Рейтинг этого фильма больше чем 6?",
             correctAnswer: false)
     ]
-    
 }
-
