a23d0b372155bb2733ccb85cbf9657ed3b2fb32b
vovascool <mirdydik@icloud.com>
2023-03-01 15:58:28 +0300

    Работа над проектом. Тема 1 - Figma. Добавлены шрифты, цвета, изображение. Внесены изменения в plist.

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,3 +1,10 @@
 import UIKit
 
-extension UIColor { }
+extension UIColor {
+    static var ypBlack: UIColor { UIColor(named: "ypBlack") ?? UIColor.black }
+    static var ypGreen: UIColor { UIColor(named: "ypGreen") ?? UIColor.green }
+    static var ypRed: UIColor { UIColor(named: "ypRed") ?? UIColor.red }
+    static var ypDarkGray: UIColor { UIColor(named: "ypDarkGray") ?? UIColor.darkGray }
+    static var ypGray: UIColor { UIColor(named: "ypGray") ?? UIColor.gray }
+    static var ypWhite: UIColor { UIColor(named: "ypWhite") ?? UIColor.white }
+}
