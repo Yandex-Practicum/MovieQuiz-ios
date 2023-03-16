dcaeffd26b18c35743a63f63732992aa769fe497
EmiliaMilanovich <yourbelovedemilia@ya.ru>
2023-03-16 22:03:52 +0300

    Верстка и реализация логики по макету

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,4 +1,11 @@
 import Foundation
 import UIKit
 
-extension UIColor { }
+extension UIColor {
+    static var ypBlack: UIColor { UIColor(named: "ypBlack") ?? UIColor.black }
+    static var ypWhite: UIColor { UIColor(named: "ypWhite") ?? UIColor.white }
+    static var ypRed: UIColor { UIColor(named: "ypRed") ?? UIColor.red }
+    static var ypGreen: UIColor { UIColor(named: "ypGreen") ?? UIColor.green }
+    static var ypGray: UIColor { UIColor(named: "ypGray") ?? UIColor.gray }
+    static var ypBackground: UIColor { UIColor(named: "ypBackground") ?? UIColor.darkGray }
+}
