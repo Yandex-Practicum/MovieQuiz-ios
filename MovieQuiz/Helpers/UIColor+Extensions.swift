8bb65d7363530932f9bb20833036e5028cc71c6a
dayterr <mvdayter@gmail.com>
2023-03-18 00:36:05 +0300

    fixed colors and question

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -2,10 +2,10 @@ import Foundation
 import UIKit
 
 extension UIColor {
-        static var ypBlack: UIColor { UIColor(named: "ypBlack") ?? UIColor.black }
-        static var ypGreen: UIColor { UIColor(named: "ypGreen") ?? UIColor.green }
-        static var ypRed: UIColor { UIColor(named: "ypRed") ?? UIColor.red }
-        static var ypDarkGray: UIColor { UIColor(named: "ypDarkGray") ?? UIColor.darkGray }
-        static var ypGray: UIColor { UIColor(named: "ypGray") ?? UIColor.gray }
-        static var ypWhite: UIColor { UIColor(named: "ypWhite") ?? UIColor.white }
+        static var ypBlack: UIColor { UIColor(named: "black") ?? UIColor.black }
+        static var ypGreen: UIColor { UIColor(named: "green") ?? UIColor.green }
+        static var ypRed: UIColor { UIColor(named: "red") ?? UIColor.red }
+        static var ypDarkGray: UIColor { UIColor(named: "darkGray") ?? UIColor.darkGray }
+        static var ypGray: UIColor { UIColor(named: "gray") ?? UIColor.gray }
+        static var ypWhite: UIColor { UIColor(named: "white") ?? UIColor.white }
      }
