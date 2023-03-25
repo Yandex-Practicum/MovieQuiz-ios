6f1bbc8ee3b07720a95745f6f0995eed7ce60fc7
Lada <l.e.belogortseva@gmail.com>
2023-03-25 14:42:59 +0300

    Cleared comments

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -57,14 +57,12 @@ final class MovieQuizViewController: UIViewController {
         super.viewDidLoad()
     }
     
-    // для состояния "Вопрос задан"
     struct QuizStepViewModel {
       let image: UIImage
       let question: String
       let questionNumber: String
     }
 
-    // для состояния "Результат квиза"
     struct QuizResultsViewModel {
       let title: String
       let text: String
@@ -81,14 +79,12 @@ final class MovieQuizViewController: UIViewController {
     private func convertQuestion(model: QuizQuestion) -> QuizStepViewModel {
         
         return QuizStepViewModel(
-                image: UIImage(named: model.image) ?? UIImage(), // распаковываем картинку
-                question: model.text, // берём текст вопроса
-                questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)") // высчитываем номер вопроса
+                image: UIImage(named: model.image) ?? UIImage(),
+                question: model.text,
+                questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)")
     }
     
-    
     private func show(quiz step: QuizStepViewModel) {
-        // здесь мы заполняем нашу картинку, текст и счётчик данными
         
         imageView.image = step.image
         counterLabel.text = step.questionNumber
@@ -96,30 +92,24 @@ final class MovieQuizViewController: UIViewController {
         imageView.layer.borderWidth = 0
         
     }
-
     
     private func show(quiz result: QuizResultsViewModel) {
-      // здесь мы показываем результат прохождения квиза
         
         let alert = UIAlertController(title: result.title,
                                       message: result.text,
                                       preferredStyle: .alert)
 
-        // создаём для него кнопки с действиями
         let action = UIAlertAction(title: result.buttonText, style: .default) { _ in
             self.currentQuestionIndex = 0
             self.correctAnswers = 0
             
-            // заново показываем первый вопрос
             let firstQuestion = self.questions[self.currentQuestionIndex]
             let viewModel = self.convertQuestion(model: firstQuestion)
             self.show(quiz: viewModel)
         }
 
-        // добавляем в алерт кнопки
         alert.addAction(action)
 
-        // показываем всплывающее окно
         self.present(alert, animated: true, completion: nil)
         
     }
@@ -163,7 +153,6 @@ final class MovieQuizViewController: UIViewController {
                                     buttonText: "Сыграть еще раз")
     }
     
-    
     private func showNextQuestionOrResults() {
         
         if currentQuestionIndex == questions.count - 1 {
@@ -195,67 +184,3 @@ final class MovieQuizViewController: UIViewController {
 }
 
 
-
-/*
- Mock-данные
- 
- 
- Картинка: The Godfather
- Настоящий рейтинг: 9,2
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: The Dark Knight
- Настоящий рейтинг: 9
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: Kill Bill
- Настоящий рейтинг: 8,1
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: The Avengers
- Настоящий рейтинг: 8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: Deadpool
- Настоящий рейтинг: 8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: The Green Knight
- Настоящий рейтинг: 6,6
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: Old
- Настоящий рейтинг: 5,8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
-
-
- Картинка: The Ice Age Adventures of Buck Wild
- Настоящий рейтинг: 4,3
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
-
-
- Картинка: Tesla
- Настоящий рейтинг: 5,1
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
-
-
- Картинка: Vivarium
- Настоящий рейтинг: 5,8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
- */
