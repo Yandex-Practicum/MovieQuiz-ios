5dc1ed815b35d29b8ea401d855457740cf6332c1
Elena Mikhailova <mikhaylova.e.dm@gmail.com>
2023-04-05 14:37:12 +0200

    Sprint 4 homework

--- MovieQuiz/AppDelegate.swift
+++ MovieQuiz/AppDelegate.swift
@@ -7,6 +7,7 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
         print("Hello world for branch to merge!")
+        sleep(3)
         return true
     }
 
