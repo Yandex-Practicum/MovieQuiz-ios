43321c8f1821e5d1e43525630b43309adad80acd
ilya <Squid87@list.ru>
2023-04-08 13:22:54 +0700

    fix bugs

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,66 +1,78 @@
-import UIKit
+    import UIKit
 
-final class MovieQuizViewController: UIViewController {
+    final class MovieQuizViewController: UIViewController {
 
     // переменная с индексом текущего вопроса,
     private var currentQuestionIndex: Int = 0
     // переменная со счётчиком правильных ответов
     private var correctAnswers: Int = 0
-    
+
     //вывод картинки
     @IBOutlet weak private var imageView: UIImageView!
     //вывод текста
     @IBOutlet weak private var textLabel: UILabel!
     //вывод текущего номера вопроса
     @IBOutlet weak private var counterLabel: UILabel!
-
     
+    //свойства кнопоки да
+    @IBOutlet weak private var yesButtonOutlet: UIButton!
+    //свойства кнопоки нет
+    @IBOutlet weak private var noButtonOutlet: UIButton!
+        
+        
+        
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         show(quiz: convert(model: questions[currentQuestionIndex]))
     }
-    
+
     @IBAction private func yesButtonClicked(_ sender: Any) {
         // берём текущий вопрос из массива вопросов по индексу текущего вопроса
         let currentQuestion = questions[currentQuestionIndex]
         let givenAnswer = true
-        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
+        self.showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
     }
-    
+
     @IBAction private func noButtonClicked(_ sender: Any) {
         let currentQuestion = questions[currentQuestionIndex]
         let givenAnswer = false
-        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
+        self.showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
     }
-    
+
     // приватный метод вывода на экран вопроса, который принимает на вход вью модель вопроса и ничего не возвращает
-    func show(quiz step: QuizStepViewModel) {
+    private func show(quiz step: QuizStepViewModel) {
         textLabel.text = step.question
         imageView.image = step.image
         counterLabel.text = step.questionNumber
         
     }
-    
+
     // приватный метод, который меняет цвет рамки
     // принимает на вход булевое значение и ничего не возвращает
     private func showAnswerResult(isCorrect: Bool) {
+        
+        yesButtonOutlet.isEnabled = false
+        noButtonOutlet.isEnabled = false
         if isCorrect {
                 correctAnswers += 1
             }
        // метод красит рамку
         imageView.layer.masksToBounds = true
         imageView.layer.borderWidth = 8
+        imageView.layer.cornerRadius = 6
         imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor: UIColor.ypRed.cgColor
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
                 self.showNextQuestionOrResults()
             }
+        yesButtonOutlet.isEnabled = true
+        noButtonOutlet.isEnabled = true
     }
-    
+
     // приватный метод, который содержит логику перехода в один из сценариев
     // метод ничего не принимает и ничего не возвращает
     private func showNextQuestionOrResults() {
-        imageView.layer.borderColor = UIColor.ypWhite.cgColor //перекрасили рамку в белый
+        imageView.layer.borderColor = UIColor.white.cgColor //перекрасили рамку в белый
         // идём в состояние "Результат квиза"
         if currentQuestionIndex == questions.count - 1 {
             let text = "Ваш результат: \(correctAnswers)/10"
@@ -87,7 +99,7 @@ final class MovieQuizViewController: UIViewController {
                 questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)") // 4
             return questionStep
     }
-    
+
     // приватный метод для показа результатов раунда квиза
     // принимает вью модель QuizResultsViewModel и ничего не возвращает
     private func show(quiz result: QuizResultsViewModel) {
@@ -109,7 +121,7 @@ final class MovieQuizViewController: UIViewController {
         
         self.present(alert, animated: true, completion: nil)
     }
-    
+
     // для состояния "Результат квиза"
     struct QuizResultsViewModel {
       // строка с заголовком алерта
@@ -119,8 +131,8 @@ final class MovieQuizViewController: UIViewController {
       // текст для кнопки алерта
       let buttonText: String
     }
-    
-    
+
+
     // вью модель для состояния "Вопрос показан"
     struct QuizStepViewModel {
       // картинка с афишей фильма с типом UIImage
@@ -130,7 +142,7 @@ final class MovieQuizViewController: UIViewController {
       // строка с порядковым номером этого вопроса (ex. "1/10")
       let questionNumber: String
     }
-    
+
     struct QuizQuestion{
         // строка с названием фильма,
         // совпадает с названием картинки афиши фильма в Assets
@@ -154,8 +166,8 @@ final class MovieQuizViewController: UIViewController {
             QuizQuestion(image: "Tesla", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false),
             QuizQuestion(image: "Vivarium", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false)
         ]
-    
-}
+
+    }
 
 
 
