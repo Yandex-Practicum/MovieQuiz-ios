4fda43c54f731c4330c3c0b8775b7b944dd547f6
PLDDev <wildmagicactive@gmail.com>
2023-04-14 17:48:09 +0300

    Сдача 4-го спринта. Добавлены роковые данные, сделан интерфейс в сториборде, а также проработана логика. Рабочий мувиквиз с боковыми данными.

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,11 +1,227 @@
 import UIKit
 
 final class MovieQuizViewController: UIViewController {
+    
+    @IBOutlet private var imageView: UIImageView!
+    @IBOutlet private var textLabel: UILabel!
+    @IBOutlet private var counterLabel: UILabel!
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        let currentQuestion = questions[currentQuestionIndex]
+        let viewModel = convert(model: currentQuestion)
+        show(quiz: viewModel)
+//      show(quiz: convert(model: currentQuestion))
     }
+    
+    //MARK: Структуры
+    
+    struct ViewModel {
+        let image: UIImage
+        let question: String
+        let questionNumber: String
+    }
+    
+    // Для состояния "Вопрос показан"
+    struct QuizStepViewModel {
+        // Картинка с афишей фильма
+        let image: UIImage
+        // Вопрос о рейтинге фильма
+        let question: String
+        // Строка в порядковым номером вопроса 1/10
+        let questionNumber: String
+    }
+    
+    // Для состояния "Результат квиза"
+    struct QuizResultsViewModel {
+        // строка с заголовком алерта
+        let title: String
+        // строка с текстом о количестве набранных очков
+        let text: String
+        // текс для кнопки алерта
+        let buttonText: String
+    }
+    // Состояние «Результат ответа» можно описать одной переменной типа Bool — ответ правильный или нет.
+    
+    struct QuizQuestion {
+        // Строка с названием фильма, совпадает с Assets
+        let image: String
+        // Строка с вопросом о рейтинге фильма
+        let text: String
+        // Булево значение true, false, правильный ответ на вопрос
+        let correctAnswer: Bool
+    }
+    
+    //MARK: Переменные
+    
+    // Переменная с индексом текущего вопроса, начальное значение 0
+    // По индексу будем искать вопрос в массиве, поэтому 0
+    private var currentQuestionIndex = 0
+    
+    // Переменная со счётчиком правильных ответов
+    private var correctAnswers = 0
+    
+    // Берем текущий вопрос из массива вопросов по индексу текущего вопроса
+//    private let currentQuestion = questions[currentQuestionIndex]
+    
+    //MARK: Массив с моковыми данными
+    
+    private let questions: [QuizQuestion] = [
+        QuizQuestion(
+            image: "The Godfather",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "The Dark Knight",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "Kill Bill",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "The Avengers",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "Deadpool",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "The Green Knight",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "Old",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false),
+        QuizQuestion(
+            image: "The Ice Age Adventures of Buck Wild",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false),
+        QuizQuestion(
+            image: "Tesla",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false),
+        QuizQuestion(
+            image: "Vivarium",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false)
+    ]
+    
+    //MARK: Методы
+    
+    // Метод конвертации, который принимает моковый вопрос и возвращает вью модель для экрана вопроса
+    private func convert(model: QuizQuestion) -> QuizStepViewModel {
+        // 1. Создаем константу questionStep и вызываем конструктор QuizStepViewModel
+        let questionStep = QuizStepViewModel(
+            
+            // 2. Инициализируем картинку с помощью конструктора UIImage(named: ),
+            // и если не найдется картинка с таким названием, то подставляем пустую
+            image: UIImage(named: model.image) ?? UIImage(),
+            
+            // 3. Просто выбираем уже готовый вопрос из мокового вопроса
+            question: model.text,
+            
+            // 4. Высчитываем номер вопроса с помощью переменной текущего вопроса
+            // и массива со списком вопросов questions. С помощью интрополяции заполняем String
+            questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)")
+        
+        // 5. Метод конвертации должен вернуть объект структуры QuizStepViewModel. Созданный questionStep
+        return questionStep
+    }
+    
+    // Приватный метод вывода на экран вопроса, который принимает на вход вью модель вопроса и ничего не возвращает
+    private func show(quiz step: QuizStepViewModel) {
+        imageView.image = step.image
+        textLabel.text = step.question
+        counterLabel.text = step.questionNumber
+    }
+    
+    private func showAnswerResult(isCorrect: Bool) {
+        if isCorrect {
+            correctAnswers += 1
+        }
+        
+        imageView.layer.masksToBounds = true
+        imageView.layer.borderWidth = 8
+        imageView.layer.cornerRadius = 15
+        imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            self.showNextQuestionOrResults()
+        }
+    }
+    
+    private func show(quiz result: QuizResultsViewModel) {
+        let alert = UIAlertController(
+            title: result.title,
+            message: result.text,
+            preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: result.buttonText, style: .default) { _ in
+                self.currentQuestionIndex = 0
+                self.correctAnswers = 0
+                
+                let firstQuestion = self.questions[self.currentQuestionIndex]
+                let viewModel = self.convert(model: firstQuestion)
+                
+                self.show(quiz: viewModel)
+            }
+        
+        alert.addAction(action)
+        
+        self.present(alert, animated: true, completion: nil)
+
+    }
+    
+    private func showNextQuestionOrResults() {
+        if currentQuestionIndex == questions.count - 1 {
+            // Идем в состояние "Результат квиза"
+            let text = "Ваш результат: \(correctAnswers)/10"
+            let viewModel = QuizResultsViewModel(
+                title: "Этот раунд окончен!",
+                text: text,
+                buttonText: "Сыграть еще раз")
+            show(quiz: viewModel)
+
+            
+        } else {
+            currentQuestionIndex += 1
+            
+            // Идем в состояние "Вопрос показан"
+            let nextQuestion = questions[currentQuestionIndex]
+            let viewModel = convert(model: nextQuestion)
+            
+            show(quiz: viewModel)
+        }
+    }
+    
+    @IBAction private func yesButtonClicked(_ sender: UIButton) {
+        let currentQuestion = questions[currentQuestionIndex]
+        let givenAnswer = true
+        
+        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
+    }
+    
+    @IBAction private func noButtonClicked(_ sender: UIButton) {
+        let currentQuestion = questions[currentQuestionIndex]
+        let givenAnswer = false
+        
+        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
+    }
+    
+    
 }
+
+/*
+ На данный момент у нас начал вырисовываться алгоритм, подходящий для написания кода любого экрана:
+ 1. Получить макеты экрана и краткое описание того, что должно происходить.
+ 2. Описать конечный автомат, который будет иллюстрировать все состояния экрана.
+ 3. Продумать вью модели, которые будут описывать каждое из этих состояний.
+ 4. Написать логику перехода между этими состояниями и показать данные в каждом из них исходя из описания задачи.
+ */
 
 /*
  Mock-данные
