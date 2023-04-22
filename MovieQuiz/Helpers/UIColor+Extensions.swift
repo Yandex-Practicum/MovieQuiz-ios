f03067840d758d503d34f3e6ee71b964e3c4cc8c
Elena Mikhailova <mikhaylova.e.dm@gmail.com>
2023-04-22 22:16:49 +0200

    Sprint 6 Homework

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,11 +1,11 @@
 import UIKit
 
 extension UIColor {
-    static var ypBlack: UIColor { UIColor(named: "ypBlack") ?? UIColor.black }
-            static var ypGreen: UIColor { UIColor(named: "ypGreen") ?? UIColor.green }
-            static var ypRed: UIColor { UIColor(named: "ypRed") ?? UIColor.red }
-            static var ypDarkGray: UIColor { UIColor(named: "ypDarkGray") ?? UIColor.darkGray }
-            static var ypGray: UIColor { UIColor(named: "ypGray") ?? UIColor.gray }
-            static var ypWhite: UIColor { UIColor(named: "ypWhite") ?? UIColor.white }
+    static var ypBlack = UIColor(named: "ypBlack")
+    static var ypGreen = UIColor(named: "ypGreen")
+    static var ypRed = UIColor(named: "ypRed")
+    static var ypDarkGray = UIColor(named: "ypDarkGray")
+    static var ypGray = UIColor(named: "ypGray")
+    static var ypWhite = UIColor(named: "ypWhite")
          }
 
