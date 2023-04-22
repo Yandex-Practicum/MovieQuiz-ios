8ca755040a5a812f2b126e6f091a94c80ee508db
aglamov <aglamov@yandex.ru>
2023-04-22 19:46:43 +0300

    Задание по спринту 5

---
+++ MovieQuiz/Models/QuizStepViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  QuizStepViewModel.swift
+//  MovieQuiz
+//
+//  Created by Рамиль Аглямов on 09.04.2023.
+//
+
+import Foundation
+import UIKit
+
+struct QuizStepViewModel {
+    let image: UIImage
+    let question: String
+    let questionNumber: String
+}
---
+++ MovieQuiz/Services/AlertProtocol.swift
@@ -0,0 +1,11 @@
+//
+//  AlertProtocol.swift
+//  MovieQuiz
+//
+//  Created by Рамиль Аглямов on 17.04.2023.
+//
+
+import Foundation
+protocol AlertProtocol {
+    func show(alertModel: AlertModel)
+}
