b031092abd0d4a82b6486b134d78ad25e878fb94
Elena Mikhailova <mikhaylova.e.dm@gmail.com>
2023-04-24 18:24:37 +0200

    commit

--- MovieQuiz/Services/NetworkClient.swift
+++ MovieQuiz/Services/NetworkClient.swift
@@ -7,7 +7,11 @@
 
 import Foundation
 /// Отвечает за загрузку данных по URL
-struct NetworkClient {
+protocol NetworkRouting {
+    func fetch(url: URL, handler: @escaping (Result<Data, Error>) -> Void)
+}
+
+struct NetworkClient: NetworkRouting {
 
     private enum NetworkError: Error {
         case codeError
@@ -25,7 +29,7 @@ struct NetworkClient {
             
             // Проверяем, что нам пришёл успешный код ответа
             if let response = response as? HTTPURLResponse,
-                response.statusCode < 200 || response.statusCode >= 300 {
+                response.statusCode < 200 && response.statusCode >= 300 {
                 handler(.failure(NetworkError.codeError))
                 return
             }
---
+++ MovieQuizUITests/MovieQuizUITests.swift
@@ -0,0 +1,93 @@
+//
+//  MovieQuizUITests.swift
+//  MovieQuizUITests
+//
+//  Created by Елена Михайлова on 23.04.2023.
+//
+
+import XCTest
+@testable import MovieQuiz
+
+class MovieQuizUITests: XCTestCase {
+    var app: XCUIApplication!
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        
+        app = XCUIApplication()
+        app.launch()
+        
+        continueAfterFailure = false
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        app.terminate()
+        app = nil
+    }
+    
+    func testYesButton() {
+        sleep(3)
+        let firstPoster = app.images["Poster"]
+        let firstPosterData = firstPoster.screenshot().pngRepresentation
+        app.buttons["Yes"].tap()
+        sleep(3)
+        let secondPoster = app.images["Poster"]
+        let secondPosterData = secondPoster.screenshot().pngRepresentation
+        let indexLabel = app.staticTexts["Index"]
+        XCTAssertEqual(indexLabel.label, "2/10")
+        XCTAssertFalse(firstPosterData == secondPosterData)
+    }
+    
+    func testNoButton() {
+        sleep(3)
+        let firstPoster = app.images["Poster"]
+        let firstPosterData = firstPoster.screenshot().pngRepresentation
+        app.buttons["No"].tap()
+        sleep(3)
+        let secondPoster = app.images["Poster"]
+        let secondPosterData = secondPoster.screenshot().pngRepresentation
+        let indexLabel = app.staticTexts["Index"]
+        XCTAssertEqual(indexLabel.label, "2/10")
+        XCTAssertFalse(firstPosterData == secondPosterData)
+    }
+    
+    func testGameFinish() {
+        sleep(2)
+        for _ in 1...10 {
+            app.buttons["No"].tap()
+            sleep(2)
+        }
+        
+        let alert = app.alerts["Game Results"]
+        XCTAssertTrue(alert.exists)
+        XCTAssertTrue(alert.label == "Этот раунд окончен!")
+        XCTAssertTrue(alert.buttons.firstMatch.label == "Сыграть еще раз")
+    }
+    
+    func testAlertDismiss() {
+        sleep(2)
+        for _ in 1...10 {
+            app.buttons["No"].tap()
+            sleep(2)
+        }
+        
+        let alert = app.alerts["Game results"]
+        alert.buttons.firstMatch.tap()
+        
+        sleep(2)
+        
+        let indexLabel = app.staticTexts["Index"]
+        
+        XCTAssertFalse(alert.exists)
+        XCTAssertTrue(indexLabel.label == "1/10")
+    }
+    
+    func testExample() throws {
+        
+        let app = XCUIApplication()
+        app.launch()
+
+        
+    }
+}
