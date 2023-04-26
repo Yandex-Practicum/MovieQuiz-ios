960512ffb9f398c674736d7411316a9a896b547e
Mnkvch <andrey.mankevich@yandex.ru>
2023-04-26 23:23:23 +0300

    обновил MoviesLoader перед тестовой реализацией протокола

--- MovieQuiz/Services/MoviesLoader.swift
+++ MovieQuiz/Services/MoviesLoader.swift
@@ -11,17 +11,21 @@ protocol MoviesLoading {
     func loadMovies(handler: @escaping (Result<MostPopularMovies, Error>) -> Void)
 }
 struct MoviesLoader: MoviesLoading {
-    // MARK: - NetworkClient
-    private let networkClient = NetworkClient()
+  // MARK: - NetworkClient
+  private let networkClient: NetworkRouting
+  
+  init(networkClient: NetworkRouting = NetworkClient()) {
+      self.networkClient = networkClient
+  }
     
     // MARK: - URL
     private var mostPopularMoviesUrl: URL {
-        
         guard let url = URL(string: "https://imdb-api.com/en/API/Top250Movies/k_o9rwnyx7") else {
             preconditionFailure("Unable to construct mostPopularMoviesUrl")
         }
         return url
     }
+    
     func loadMovies(handler: @escaping (Result<MostPopularMovies, Error>) -> Void) {
         networkClient.fetch(url: mostPopularMoviesUrl) { result in
             switch result {
