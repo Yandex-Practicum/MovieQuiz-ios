532ad568fdbb0cc2c098907ced929da9c93c8162
Mnkvch <andrey.mankevich@yandex.ru>
2023-04-27 01:38:56 +0300

    удалил моки

--- MovieQuizUITests/MovieQuizUITests.swift
+++ MovieQuizUITests/MovieQuizUITests.swift
@@ -12,31 +12,26 @@ final class MovieQuizUITests: XCTestCase {
     var app: XCUIApplication!
 
     override func setUpWithError() throws {
-        try super.setUpWithError()// Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
         app = XCUIApplication()
         app.launch()
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+       
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDownWithError() throws {
         app.launch()
-        app = nil// Put teardown code here. This method is called after the invocation of each test method in the class.
+        app = nil
     }
 
     func testExample() throws {
-        // UI tests must launch the application that they test.
+       
         let app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
             measure(metrics: [XCTApplicationLaunchMetric()]) {
                 app.launch()
                 app.buttons["Нет"].tap()
@@ -45,15 +40,15 @@ final class MovieQuizUITests: XCTestCase {
     }
     func testYesButton() {
         sleep(3)
-        let firstPoster = app.images["Poster"] // находим первоначальный постер
+        let firstPoster = app.images["Poster"]
         let firstPosterData = firstPoster.screenshot().pngRepresentation
         XCTAssertTrue(firstPoster.exists)
-        app.buttons["Yes"].tap() // находим кнопку `Да` и нажимаем её
+        app.buttons["Yes"].tap()
         sleep(3)
-        let secondPoster = app.images["Poster"] // ещё раз находим постер
+        let secondPoster = app.images["Poster"]
         let secondPosterData = secondPoster.screenshot().pngRepresentation
         XCTAssertTrue(secondPoster.exists)
-        XCTAssertFalse(firstPoster == secondPoster) // проверяем, что постеры разные
+        XCTAssertFalse(firstPoster == secondPoster)
         XCTAssertNotEqual(firstPosterData, secondPosterData)
     }
     func testNoButton() {
