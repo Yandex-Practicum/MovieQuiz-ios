aca10d8afb89445c3e658ba1fb662145234aea3b
Mnkvch <andrey.mankevich@yandex.ru>
2023-04-26 23:20:02 +0300

    обновил код в NetworkClient для тестирования, добавлен протокол

--- MovieQuiz/Services/NetworkClient.swift
+++ MovieQuiz/Services/NetworkClient.swift
@@ -7,8 +7,12 @@
 
 import Foundation
 
-struct NetworkClient {
+protocol NetworkRouting {
+    func fetch(url: URL, handler: @escaping (Result<Data, Error>) -> Void)
+}
 
+struct NetworkClient: NetworkRouting {
+    
     private enum NetworkError: Error {
         case codeError
     }
@@ -17,14 +21,14 @@ struct NetworkClient {
         let request = URLRequest(url: url)
         
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
-         
+        
             if let error = error {
                 handler(.failure(error))
                 return
             }
             
             if let response = response as? HTTPURLResponse,
-                response.statusCode < 200 || response.statusCode >= 300 {
+                response.statusCode < 200 && response.statusCode >= 300 {
                 handler(.failure(NetworkError.codeError))
                 return
             }
---
+++ MovieQuizTests/ArrayTests.swift
@@ -0,0 +1,36 @@
+//
+//  ArrayTests.swift
+//  MovieQuizTests
+//
+//  Created by Андрей Манкевич on 26.04.2023.
+//
+
+import Foundation
+
+import XCTest
+@testable import MovieQuiz
+
+class ArrayTests: XCTestCase {
+    func testGetValueInRange() throws { // тест на успешное взятие элемента по индексу
+        // Given
+        let array = [1, 1, 2, 3, 5]
+        
+        // When
+        let value = array[safe: 2]
+        
+        // Then
+        XCTAssertNotNil(value)
+        XCTAssertEqual(value, 2)
+    }
+    
+    func testGetValueOutOfRange() throws {
+        // Given
+        let array = [1, 1, 2, 3, 5]
+        
+        // When
+        let value = array[safe: 20]
+        
+        // Then
+        XCTAssertNil(value)
+    }
+}
