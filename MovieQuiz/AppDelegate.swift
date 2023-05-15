4221e53e1a8f2782bf987385d95e1ecae1d331a5
Arkhip Semenov <liteplay03@ya.ru>
2023-05-15 18:21:30 +0300

    еще один тестовый коммит

--- MovieQuiz/AppDelegate.swift
+++ MovieQuiz/AppDelegate.swift
@@ -23,6 +23,6 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         _ application: UIApplication,
         didDiscardSceneSessions sceneSessions: Set<UISceneSession>
     ) {
-        print("hello world!")
+        print("Hello world for branch to merge")
     }
 }
