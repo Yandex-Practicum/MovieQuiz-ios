09eaebc908d69c957f61e37c944f41b1871fcfe5
dsiberia9s <dsiberia9s@icloud.com>
2023-05-17 17:56:42 +0700

    Удалён файл AlertPresenter.swift, созданный ошибочно в папке Models. Ранее ссылка на него внутри проекта была удалена.

---
+++ MovieQuiz/Models/QuizResultsViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  QuizResultsViewModel.swift
+//  MovieQuiz
+//
+//  Created by Dmitrii on 15.05.2023.
+//
+
+import Foundation
+
+// для состояния "Результат квиза"
+struct QuizResultsViewModel {
+  // строка с заголовком алерта
+  let title: String
+  // строка с текстом о количестве набранных очков
+  let text: String
+  // текст для кнопки алерта
+  let buttonText: String
+}
