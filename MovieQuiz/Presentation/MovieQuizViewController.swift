7e443bc4ae6443ea5bafa2707cc26e08c856d5bd
IrinaGolubinskaya <irenegolub111@yandex.ru>
2023-06-01 13:44:29 +0300

    исправления после ревью

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -8,14 +8,14 @@ final class MovieQuizViewController: UIViewController {
     @IBOutlet weak private var noButton: UIButton!
     @IBOutlet weak private var yesButton: UIButton!
     
-    //переменная с индексом текущего вопроса, начальное значение 0
-    //(по этому индексу будем искать вопрос в массиве, где индекс первого элемента = 0
+    ///переменная с индексом текущего вопроса, начальное значение 0
+    ///(по этому индексу будем искать вопрос в массиве, где индекс первого элемента = 0
     private var currentQuestionIndex = 0
     
-    //переменная со счетчиком правильных ответов, начальное значение закономерно =0
+    ///переменная со счетчиком правильных ответов, начальное значение закономерно =0
     private var correctAnswers = 0
     
-    // массив вопросов
+    /// массив вопросов
     private let questions: [QuizQuestion] = [
         QuizQuestion(image: "The Godfather", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
         QuizQuestion(image: "The Dark Knight", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
@@ -32,30 +32,29 @@ final class MovieQuizViewController: UIViewController {
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        let currentQuestion = questions[currentQuestionIndex]/// Это первая структура из массива, первая потому что индекс вначале равен 0
+        // Это первая структура из массива, первая потому что индекс вначале равен 0
+        let currentQuestion = questions[currentQuestionIndex]
         let stepViewModel = convert(model: currentQuestion)
         show(quiz: stepViewModel)
+        imageView.layer.cornerRadius = 20
     }
     
     @IBAction private func noButtonClicked(_ sender: Any) {
-        let currentQuestion = questions[currentQuestionIndex]/// Это первая структура из массива, первая потому что индекс вначале равен 0
-        if currentQuestion.correctAnswer == false {
-            showAnswerResult(isCorrect: true)
-        } else {
-            showAnswerResult(isCorrect: false)
-        }
+        //достаём текущий вопрос:
+        let currentQuestion = questions[currentQuestionIndex]
+        //булевая переменная которая содержит ТРУ если ответ сошелся
+        let isCorrectAnswer = currentQuestion.correctAnswer == false
+        showAnswerResult(isCorrect: isCorrectAnswer)
     }
     
     @IBAction private func yesButtonClicked(_ sender: Any) {
+        //достаём текущий вопрос
         let currentQuestion = questions[currentQuestionIndex]
-        if currentQuestion.correctAnswer == true {
-            showAnswerResult(isCorrect: true)
-        } else {
-            showAnswerResult(isCorrect: false)
-        }
+        let isCorrectAnswer = currentQuestion.correctAnswer == true
+        showAnswerResult(isCorrect: isCorrectAnswer)
     }
     
-    // метод конвертации, который принимает моковый вопрос и возвращает вью модель для экрана вопроса
+    /// метод конвертации, который принимает моковый вопрос и возвращает вью модель для экрана вопроса
     private func convert(model: QuizQuestion) -> QuizStepViewModel {
         let image1 = UIImage(named: model.image) ??  UIImage()
         let questionNumber1 = "\(currentQuestionIndex + 1)/\(questions.count)"
@@ -65,20 +64,22 @@ final class MovieQuizViewController: UIViewController {
         return viewModel
     }
     
-    //приватный метод вывода на экран вопроса,
-    //который принимает на вход вью модель вопроса и ничего не возвращает
+    ///приватный метод вывода на экран вопроса,
+    ///который принимает на вход вью модель вопроса и ничего не возвращает
     private func show(quiz step: QuizStepViewModel) {
         imageView.image = step.image
         textLabel.text = step.question
         counterLabel.text = step.questionNumber
     }
     
+    /// метод красит рамку, в зависимости от ответа ДА/НЕТ
+    /// isCorrect это параметр который указывает верный ответ или нет.  Если true, ответ ВЕРНЫЙ, если false - неверный
+    ///
     private func showAnswerResult(isCorrect: Bool) {
-        // метод красит рамку
         imageView.layer.masksToBounds = true
         imageView.layer.borderWidth = 8
-        imageView.layer.cornerRadius = 6
-        imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
+        //красим рамку в зависимости от того, правильный ответ или нет
+        imageView.layer.borderColor = isCorrect == true ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
         if isCorrect == true {
             correctAnswers += 1
         }
@@ -89,8 +90,8 @@ final class MovieQuizViewController: UIViewController {
         }
     }
     
-    //приватный метод, который содержит логику перехода в один из сценариев
-    //метод ничего не принимает и ничего не возвращает
+    ///приватный метод, который содержит логику перехода в один из сценариев
+    ///метод ничего не принимает и ничего не возвращает
     private func showNextQuestionOrResult() {
         imageView.layer.borderColor = UIColor.clear.cgColor
         if currentQuestionIndex == questions.count - 1 {
@@ -101,7 +102,7 @@ final class MovieQuizViewController: UIViewController {
                                                  buttonText: "Сыграть еще раз")
             show(quiz: viewModel)
         } else {
-            //Переходим к слудующему вопросу
+            //Переходим к следующему вопросу
             currentQuestionIndex += 1
             let nextQuestion = questions[currentQuestionIndex]
             let viewModel = convert(model: nextQuestion)
@@ -121,7 +122,7 @@ final class MovieQuizViewController: UIViewController {
             //обнуляем счетчик с результатами квиза:
             self.correctAnswers = 0
             
-            let currentQuestion = self.questions[self.currentQuestionIndex]/// Это первая структура из массива, первая потому что индекс вначале равен 0
+            let currentQuestion = self.questions[self.currentQuestionIndex]
             let stepViewModel = self.convert(model: currentQuestion)
             self.show(quiz: stepViewModel)
         }
@@ -131,10 +132,9 @@ final class MovieQuizViewController: UIViewController {
         self.present(alert, animated: true)
     }
     
-    // приватный метод для показа результатов раунда квиза
-    // принимает вью модель QuizResultsViewModel и ничего не возвращает
+    /// приватный метод для показа результатов раунда квиза
+    /// принимает вью модель QuizResultsViewModel и ничего не возвращает
     private func show(quiz result: QuizResultsViewModel) {
-        // попробуйте написать код создания и показа алерта с результатами
         let alert = UIAlertController(title: result.title,
                                       message: result.text,
                                       preferredStyle: .alert)
@@ -154,24 +154,24 @@ final class MovieQuizViewController: UIViewController {
 }
 
 struct QuizQuestion {
-    //Строка с названием фильма,
-    //Совпадает с названием картинки афиши фильма в Assets
+    ///Строка с названием фильма,
+    ///Совпадает с названием картинки афиши фильма в Assets
     let image: String
-    //строка с вопросом о рейтинге фильма
+    ///строка с вопросом о рейтинге фильма
     let text: String
-    //Булевое значение (true,false), правильный ответ на вопрос
+    ///Булевое значение (true,false), правильный ответ на вопрос
     let correctAnswer: Bool
 }
 
 struct QuizStepViewModel {
-    //картинка с афишей фильма с типом UIImage
+    ///картинка с афишей фильма с типом UIImage
     let image : UIImage
-    //вопрос о рейтинге квиза
+    ///вопрос о рейтинге квиза
     let question: String
-    //строка с порядковым номером этого вопроса (ex. "1/10")
+    ///строка с порядковым номером этого вопроса (ex. "1/10")
     let questionNumber: String
 }
-// для состояния "Результат квиза"
+/// для состояния "Результат квиза"
 struct QuizResultsViewModel {
   let title: String
   let text: String
