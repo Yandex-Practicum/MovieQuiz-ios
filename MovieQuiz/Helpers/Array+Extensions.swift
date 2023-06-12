7ec26ddcf768c8b6c3089be5662b2e10de3d4f0a
Wilwous <134493446+Wilwous@users.noreply.github.com>
2023-06-12 15:49:35 +0300

    Delete MovieQuiz directory

--- MovieQuiz/Helpers/Array+Extensions.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-extension Array {
-    subscript(safe index: Index) -> Element? {
-        indices ~= index ? self[index] : nil
-    }
-}
--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++
@@ -1,3 +0,0 @@
-import UIKit
-
-extension UIColor { }
