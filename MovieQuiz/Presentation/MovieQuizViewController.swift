af35d7df9a5ce491b020a856ad1664ba3ff52071
ArthurM2 <38484496+ArthurM2@users.noreply.github.com>
2023-06-25 18:55:45 +0500

    добавлен алерт, вьюмодели

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,6 +1,17 @@
 import UIKit
 
 final class MovieQuizViewController: UIViewController {
+    
+    @IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet private weak var textLabel: UILabel!
+    @IBOutlet private weak var counterLabel: UILabel!
+    
+    // переменная с индексом текущего вопроса
+    private var currentQuestionIndex = 0
+    // переменная со счётчиком правильных ответов
+    private var correctAnswers = 0
+
+    
     // MARK: - Mock Data
     private let questions: [QuizQuestion] = [
         QuizQuestion(
@@ -53,43 +64,33 @@ final class MovieQuizViewController: UIViewController {
             text: "Рейтинг этого фильма больше чем 6?", // настоящий 5,8
             correctAnswer: false)
     ]
-    
-    // MARK: - Variables
-    // переменная с индексом текущего вопроса
-    private var currentQuestionIndex = 0
-    // переменная со счётчиком правильных ответов
-    private var correctAnswers = 0
+
     // берём текущий вопрос из массива вопросов по индексу текущего вопроса
 //    let currentQuestion = questions[currentQuestionIndex]
-    
-    // аутлеты картинки, текста и счетчика
-    @IBOutlet private var imageView: UIImageView!
-    @IBOutlet private var textLabel: UILabel!
-    @IBOutlet private var counterLabel: UILabel!
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
     
     // MARK: - View Models
     struct QuizQuestion {
-        // строка с названием фильма,
-        // совпадает с названием картинки фильма в Assets
         let image: String
-        //  строка с вопросом о рейтинге фильма
         let text: String
-        // булевое значение, проверяет правильный ответ на вопрос
         let correctAnswer: Bool
     }
     
     struct QuizStepViewModel {
-        // картинка с афишей фильма с типом UIImage
         let image: UIImage
-        // вопрос о рейтинге квиза
         let question: String
-        // счетчик номера вопроса (прим. 2/10)
         let questionNumber: String
+    }
+    
+    struct QuizResultViewModel {
+        let title: String
+        let text: String
+        let buttonText: String
     }
     
     // MARK: - Methods
@@ -110,22 +111,63 @@ final class MovieQuizViewController: UIViewController {
         counterLabel.text = step.questionNumber
     }
     
+    private func show(quiz result: QuizResultViewModel) {
+        let alert = UIAlertController(
+            title: result.title,
+            message: result.text,
+            preferredStyle: .alert
+        )
+        
+        let action = UIAlertAction(title: result.buttonText, style: .default) { _ in
+            self.currentQuestionIndex = 0
+            self.correctAnswers = 0
+            
+            let firstQuestion = self.questions[self.currentQuestionIndex]
+            let viewModel = self.convert(model: firstQuestion)
+            self.show(quiz: viewModel)
+        }
+        alert.addAction(action)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
     // приватный метод, который меняет цвет рамки
     private func showAnswerResult(isCorrect: Bool) {
         imageView.layer.masksToBounds = true // даём разрешение на рисование рамки
         imageView.layer.borderWidth = 8 // толщина рамки
         imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
+        
+        if isCorrect {
+            correctAnswers += 1
+        }
+        
+        // запускаем задачу через 1 секунду c помощью диспетчера задач
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+           // код, который мы хотим вызвать через 1 секунду
+           self.showNextQuestionOrResults()
+        }
     }
     
     // приватный метод, который содержит логику перехода в один из сценариев
     // метод ничего не принимает и ничего не возвращает
     private func showNextQuestionOrResults() {
-      if currentQuestionIndex == questions.count - 1 { // 1
-        // идём в состояние "Результат квиза"
-      } else { // 2
-        currentQuestionIndex += 1
-        // идём в состояние "Вопрос показан"
-      }
+        if currentQuestionIndex == questions.count - 1 {
+            let text = "Ваш результат: \(correctAnswers)/\(questions.count)"
+            let viewModel = QuizResultViewModel(
+                title: "Этот раунд окончен!",
+                text: text,
+                buttonText: "Сыграть еще раз"
+            )
+            show(quiz: viewModel)
+        } else {
+            currentQuestionIndex += 1
+            let nextQuestion = questions[currentQuestionIndex]
+            let viewModel = convert(model: nextQuestion)
+            
+            show(quiz: viewModel)
+        }
+    }
+
     
     
     // MARK: - IBActions
