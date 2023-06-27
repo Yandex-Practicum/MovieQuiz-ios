fe7787d21e0034d3393a851125367ab6ec085cef
Iluxa147 <pokolewIlja@gmail.com>
2023-06-28 00:30:20 +0300

    add fonts and colours

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,3 +1,10 @@
 import UIKit
 
-extension UIColor { }
+extension UIColor {
+    static var ypBg: UIColor { UIColor(named: "YP Bg") ?? UIColor.darkGray }
+    static var ypGreen: UIColor { UIColor(named: "YP Green") ?? UIColor.green }
+    static var ypRed: UIColor { UIColor(named: "YP Red") ?? UIColor.red }
+    static var ypBlack: UIColor { UIColor(named: "YP Black") ?? UIColor.black}
+    static var ypGray: UIColor { UIColor(named: "YP Gray") ?? UIColor.gray }
+    static var ypWhite: UIColor { UIColor(named: "YP White") ?? UIColor.white}
+ }
