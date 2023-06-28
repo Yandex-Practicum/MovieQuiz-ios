a00ec6b071150383b7945edf0dbf27ffaefb0457
Iluxa147 <pokolewIlja@gmail.com>
2023-06-29 01:23:46 +0300

    question show logic

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,72 +1,76 @@
 import UIKit
 
-final class MovieQuizViewController: UIViewController {
-    // MARK: - Lifecycle
-    override func viewDidLoad() {
-        super.viewDidLoad()
+struct QuizStepViewModel {
+    let filmPosterImage: UIImage
+    let question: String
+    let questionCounterStr: String
+    
+    init(_ filmPosterImage: UIImage, _ question: String, _ questionCounterStr: String) {
+        self.filmPosterImage = filmPosterImage
+        self.question = question
+        self.questionCounterStr = questionCounterStr
     }
 }
 
-/*
- Mock-данные
- 
- 
- Картинка: The Godfather
- Настоящий рейтинг: 9,2
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
+struct QuizQuestion {
+    private static let questionDefault: String = "Is this film rating greater than 6?"
+    
+    let filmPosterName: String
+    let question: String
+    let correctAnswer: Bool
+    
+    init(_ filmPosterName: String, _  correctAnswer: Bool, _ question: String = "") {
+        self.filmPosterName = filmPosterName
+        self.question = question.isEmpty ? QuizQuestion.questionDefault : question
+        self.correctAnswer = correctAnswer
+    }
+}
 
-
- Картинка: The Dark Knight
- Настоящий рейтинг: 9
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: Kill Bill
- Настоящий рейтинг: 8,1
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: The Avengers
- Настоящий рейтинг: 8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: Deadpool
- Настоящий рейтинг: 8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: The Green Knight
- Настоящий рейтинг: 6,6
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: Old
- Настоящий рейтинг: 5,8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
-
-
- Картинка: The Ice Age Adventures of Buck Wild
- Настоящий рейтинг: 4,3
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
-
-
- Картинка: Tesla
- Настоящий рейтинг: 5,1
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
-
-
- Картинка: Vivarium
- Настоящий рейтинг: 5,8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
- */
+final class MovieQuizViewController: UIViewController {
+    private let questions: [QuizQuestion] = [
+            QuizQuestion("The Godfather", true),
+            QuizQuestion("The Dark Knight", true),
+            QuizQuestion("Kill Bill", true),
+            QuizQuestion("The Avengers", true),
+            QuizQuestion("Deadpool", true),
+            QuizQuestion("The Green Knight", true),
+            QuizQuestion("Old", false),
+            QuizQuestion("The Ice Age Adventures of Buck Wild", false),
+            QuizQuestion("Tesla", false),
+            QuizQuestion("Vivarium", false)
+        ]
+    
+    private var currentQuestionIdx: Int = 0
+    private var correctAnswersCount: Int = 0
+    
+    @IBOutlet private weak var filmPosterImage: UIImageView!
+    @IBOutlet private weak var questionCounterLabel: UILabel!
+    @IBOutlet private weak var questionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let quizStep0 = convertToQuizStep(questions[currentQuestionIdx])
+        //ShowQuizStep(quizStep0)
+    }
+    
+    private func convertToQuizStep(_ from: QuizQuestion) -> QuizStepViewModel {
+        let retVal = QuizStepViewModel(
+            UIImage(named: from.filmPosterName) ?? UIImage(), from.question,
+            "\(currentQuestionIdx + 1)/\(questions.count)")
+        
+        return retVal
+    }
+    
+    private func ShowQuizStep(_ quizStep: QuizStepViewModel) {
+        filmPosterImage.image = quizStep.filmPosterImage
+        questionCounterLabel.text = quizStep.questionCounterStr
+        questionLabel.text = quizStep.question
+    }
+    
+    @IBAction private func noButtonTap(_ sender: Any) {
+    }
+    
+    @IBAction private func yesButtonTap(_ sender: Any) {
+    }
+}
