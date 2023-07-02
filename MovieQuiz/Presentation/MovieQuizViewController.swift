dd7527232aea756d5f4d7f99f84b5d61b61ea6c9
Чупрыненко Андрей <wotmaster187@icloud.com>
2023-07-02 22:10:05 +0300

    Изменен цвет статус бара, после ответа на вопрос рамка пропадает, добавлено скругление для афиши фильма

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -22,9 +22,10 @@ final class MovieQuizViewController: UIViewController {
     //событие кнопки нет
     @IBAction private func noButtonClicked(_ sender: UIButton) {
         let currentQuestion = questions[currentQuestionIndex]
-            let givenAnswer = false
-            
-            showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
+        let givenAnswer = false
+        var _: Bool
+        
+        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
     }
     
     // переменная с индексом текущего вопроса, начальное значение 0
@@ -60,8 +61,10 @@ final class MovieQuizViewController: UIViewController {
         imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
         imageView.layer.cornerRadius = 20
         
+        
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
                 self.showNextQuestionOrResults()
+            self.imageView.layer.borderWidth = 0
             }
     }
     
@@ -85,11 +88,18 @@ final class MovieQuizViewController: UIViewController {
         self.present(alert, animated: true, completion: nil)
     }
     
+    //изменяет статус бар на белый
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        .lightContent
+    }
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         let currentQuestion = convert(model: questions[currentQuestionIndex])
         show(quiz: QuizStepViewModel(image: currentQuestion.image, question: currentQuestion.question, questionNumber: currentQuestion.questionNumber))
+        
+        imageView.layer.cornerRadius = 20
     }
     
     // для состояния "Результат квиза"
