09ff3b8f7b8f8ea6fdfa4b5217a19a0707f85b69
Iluxa147 <pokolewIlja@gmail.com>
2023-07-03 03:13:00 +0300

    all done

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -26,6 +26,18 @@ struct QuizQuestion {
     }
 }
 
+struct QuizResultsViewModel {
+  let title: String
+  let text: String
+  let buttonText: String
+    
+    init(_ title: String, _ text: String, _ buttonText: String) {
+        self.title = title
+        self.text = text
+        self.buttonText = buttonText
+    }
+}
+
 final class MovieQuizViewController: UIViewController {
     private let questions: [QuizQuestion] = [
             QuizQuestion("The Godfather", true),
@@ -50,27 +62,78 @@ final class MovieQuizViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let quizStep0 = convertToQuizStep(questions[currentQuestionIdx])
-        //ShowQuizStep(quizStep0)
+        filmPosterImage.layer.masksToBounds = true
+        filmPosterImage.layer.cornerRadius = 20
+        
+        showQuizStep(getQuizStep(currentQuestionIdx))
+    }
+    
+    private func getCounterPostfix() -> String {
+        return "/\(questions.count)"
+    }
+    
+    private func getQuizStep(_ questionIdx: Int) -> QuizStepViewModel {
+        return convertToQuizStep(questions[questionIdx])
     }
     
     private func convertToQuizStep(_ from: QuizQuestion) -> QuizStepViewModel {
         let retVal = QuizStepViewModel(
             UIImage(named: from.filmPosterName) ?? UIImage(), from.question,
-            "\(currentQuestionIdx + 1)/\(questions.count)")
+            "\(currentQuestionIdx + 1)\(getCounterPostfix())")
         
         return retVal
     }
     
-    private func ShowQuizStep(_ quizStep: QuizStepViewModel) {
+    private func showQuizStep(_ quizStep: QuizStepViewModel) {
         filmPosterImage.image = quizStep.filmPosterImage
         questionCounterLabel.text = quizStep.questionCounterStr
         questionLabel.text = quizStep.question
+        
+        filmPosterImage.layer.borderWidth = 0
     }
     
-    @IBAction private func noButtonTap(_ sender: Any) {
+    private func showQuizResultAlert(_ result: QuizResultsViewModel) {
+        let alert = UIAlertController(title: result.title, message: result.text, preferredStyle: .alert)
+        let action = UIAlertAction(title: result.buttonText, style: .default) { _ in
+            self.currentQuestionIdx = 0
+            self.correctAnswersCount = 0
+            
+            self.showQuizStep(self.getQuizStep(self.currentQuestionIdx))
+        }
+        
+        alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
     }
     
-    @IBAction private func yesButtonTap(_ sender: Any) {
+    private func showNextQuestionOrResult() {
+        if currentQuestionIdx == questions.count - 1 {
+            let resultText = "Your result: \(correctAnswersCount)\(getCounterPostfix())"
+            let result = QuizResultsViewModel("This round is over!", resultText, "Play again")
+            showQuizResultAlert(result)
+        } else {
+            currentQuestionIdx += 1
+            showQuizStep(getQuizStep(currentQuestionIdx))
+        }
+    }
+    
+    private func showAnswerResultAndNextStep(isCorrect: Bool) {
+        if isCorrect {
+            correctAnswersCount += 1
+        }
+        
+        filmPosterImage.layer.borderWidth = 8
+        filmPosterImage.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0, execute: showNextQuestionOrResult)
+    }
+    
+    @IBAction private func noButtonTap(_ sender: UIButton) {
+        let correctAnswer = questions[currentQuestionIdx].correctAnswer
+        showAnswerResultAndNextStep(isCorrect : !correctAnswer)
+    }
+    
+    @IBAction private func yesButtonTap(_ sender: UIButton) {
+        let correctAnswer = questions[currentQuestionIdx].correctAnswer
+        showAnswerResultAndNextStep(isCorrect : correctAnswer)
     }
 }
