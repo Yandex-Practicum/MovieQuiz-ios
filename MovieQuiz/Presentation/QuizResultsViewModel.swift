175822c9ac7048d3775f0d15710150891474fe5e
Георгий Ксенодохов <gerk@MacBook-Pro-Georgij.local>
2023-07-05 02:46:16 +0300

    спринт 4

---
+++ MovieQuiz/Presentation/QuizResultsViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  MovieQuiz
+//
+//  Created by Георгий Ксенодохов on 05.07.2023.
+//
+
+import Foundation
+struct QuizResultsViewModel {
+  let title: String
+  let text: String
+  let buttonText: String
+}
---
+++ MovieQuiz/Presentation/QuizStepViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  MovieQuiz
+//
+//  Created by Георгий Ксенодохов on 05.07.2023.
+//
+
+import UIKit
+struct QuizStepViewModel {
+  let image: UIImage
+  let question: String
+  let questionNumber: String
+}
