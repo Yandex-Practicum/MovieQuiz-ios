7be878c2231454c7504bb0a6d662fad70d42e005
Vladislav Mishukov <vladg3@mail.ru>
2023-07-05 18:53:51 +0700

    Спринт 4/21: 4 → Тема 3/4: Реализация логики по макету → Урок 3/6 Настроил вывод первого вопроса на экран

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -2,9 +2,50 @@ import UIKit
 
 final class MovieQuizViewController: UIViewController {
     // MARK: - Lifecycle
+    // переменная с индексом текущего вопроса, начальное значение 0
+    private var correctAnswers = 0
+    // (по этому индексу будем искать вопрос в массиве, где индекс первого элемента 0, а не 1)
+    // переменная со счётчиком правильных ответов, начальное значение закономерно 0
+    private var currentQuestionIndex = 0
+    //массив вопросов
+    private let questions: [QuizQuestion] = [
+            QuizQuestion(image: "The Godfather"
+                         , text: "Рейтинг этого фильма больше чем 6?"
+                         , corrcetAnswer: true),
+            QuizQuestion(image: "The Dark Knight",
+                         text: "Рейтинг этого фильма больше чем 6?",
+                         corrcetAnswer: true),
+            QuizQuestion(image: "Kill Bill",
+                         text: "Рейтинг этого фильма больше чем 6?",
+                         corrcetAnswer: true),
+            QuizQuestion(image: "The Avengers",
+                         text: "Рейтинг этого фильма больше чем 6?",
+                         corrcetAnswer: true),
+            QuizQuestion(image: "Deadpool",
+                         text: "Рейтинг этого фильма больше чем 6?",
+                         corrcetAnswer: true),
+            QuizQuestion(image: "The Green Knight",
+                         text: "Рейтинг этого фильма больше чем 6?",
+                         corrcetAnswer: true),
+            QuizQuestion(image: "Old",
+                         text: "Рейтинг этого фильма больше чем 6?",
+                         corrcetAnswer: false),
+            QuizQuestion(image: "The Ice Age Adventures of Buck Wild",
+                         text: "Рейтинг этого фильма больше чем 6?",
+                         corrcetAnswer: false),
+            QuizQuestion(image: "Tesla",
+                         text: "Рейтинг этого фильма больше чем 6?",
+                         corrcetAnswer: false),
+            QuizQuestion(image: "Vivarium",
+                         text: "Рейтинг этого фильма больше чем 6?",
+                         corrcetAnswer: false)
+    ]
+    
     override func viewDidLoad() {
+        show(quiz: convert(model: questions[currentQuestionIndex]))
         super.viewDidLoad()
     }
+    
     @IBAction private func noButtonClicked(_ sender: Any) {
     }
     @IBAction private func yesButtonClicked(_ sender: Any) {
@@ -12,6 +53,21 @@ final class MovieQuizViewController: UIViewController {
     @IBOutlet private weak var imageView: UIImageView!
     @IBOutlet private weak var textLabel: UILabel!
     @IBOutlet private weak var counterLabel: UILabel!
+    
+    // приватный метод конвертации, который принимает моковый вопрос и возвращает вью модель для главного экрана
+    private func convert (model: QuizQuestion) -> QuizStepViewModel {
+        return QuizStepViewModel(
+            image: UIImage(named: model.image) ??  UIImage(),
+            question: model.text,
+            questionNumber:"\(currentQuestionIndex+1)/\(questions.count)")
+    }
+    
+    // приватный метод вывода на экран вопроса, который принимает на вход вью модель вопроса и ничего не возвращает
+    private func show(quiz step: QuizStepViewModel) {
+        imageView.image = step.image
+        textLabel.text = step.question
+        counterLabel.text = step.questionNumber
+    }
 }
 
 struct QuizQuestion {
@@ -24,39 +80,23 @@ struct QuizQuestion {
     let corrcetAnswer: Bool
 }
 
-//массив вопросов
-private let questions: [QuizQuestion] = [
-        QuizQuestion(image: "The Godfather"
-                     , text: "Рейтинг этого фильма больше чем 6?"
-                     , corrcetAnswer: true),
-        QuizQuestion(image: "The Dark Knight",
-                     text: "Рейтинг этого фильма больше чем 6?",
-                     corrcetAnswer: true),
-        QuizQuestion(image: "Kill Bill",
-                     text: "Рейтинг этого фильма больше чем 6?",
-                     corrcetAnswer: true),
-        QuizQuestion(image: "The Avengers",
-                     text: "Рейтинг этого фильма больше чем 6?",
-                     corrcetAnswer: true),
-        QuizQuestion(image: "Deadpool",
-                     text: "Рейтинг этого фильма больше чем 6?",
-                     corrcetAnswer: true),
-        QuizQuestion(image: "The Green Knight",
-                     text: "Рейтинг этого фильма больше чем 6?",
-                     corrcetAnswer: true),
-        QuizQuestion(image: "Old",
-                     text: "Рейтинг этого фильма больше чем 6?",
-                     corrcetAnswer: false),
-        QuizQuestion(image: "The Ice Age Adventures of Buck Wild",
-                     text: "Рейтинг этого фильма больше чем 6?",
-                     corrcetAnswer: false),
-        QuizQuestion(image: "Tesla",
-                     text: "Рейтинг этого фильма больше чем 6?",
-                     corrcetAnswer: false),
-        QuizQuestion(image: "Vivarium",
-                     text: "Рейтинг этого фильма больше чем 6?",
-                     corrcetAnswer: false)
-]
+struct QuizStepViewModel {
+  // картинка с афишей фильма с типом UIImage
+  let image: UIImage
+  // вопрос о рейтинге квиза
+  let question: String
+  // строка с порядковым номером этого вопроса (ex. "1/10")
+  let questionNumber: String
+}
+
+
+
+
+
+
+
+
+
 
 /*
  Mock-данные
