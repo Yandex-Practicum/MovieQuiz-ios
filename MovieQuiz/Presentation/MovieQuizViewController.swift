f128790a787538469cd35ae12416e3d96ba92926
Vladislav Mishukov <vladg3@mail.ru>
2023-07-05 19:31:09 +0700

    Спринт 4/21: 4 → Тема 3/4: Реализация логики по макету → Урок 4/6 Добавил рамки при ответе на вопрос

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -9,46 +9,86 @@ final class MovieQuizViewController: UIViewController {
     private var currentQuestionIndex = 0
     //массив вопросов
     private let questions: [QuizQuestion] = [
-            QuizQuestion(image: "The Godfather"
-                         , text: "Рейтинг этого фильма больше чем 6?"
-                         , corrcetAnswer: true),
-            QuizQuestion(image: "The Dark Knight",
-                         text: "Рейтинг этого фильма больше чем 6?",
-                         corrcetAnswer: true),
-            QuizQuestion(image: "Kill Bill",
-                         text: "Рейтинг этого фильма больше чем 6?",
-                         corrcetAnswer: true),
-            QuizQuestion(image: "The Avengers",
-                         text: "Рейтинг этого фильма больше чем 6?",
-                         corrcetAnswer: true),
-            QuizQuestion(image: "Deadpool",
-                         text: "Рейтинг этого фильма больше чем 6?",
-                         corrcetAnswer: true),
-            QuizQuestion(image: "The Green Knight",
-                         text: "Рейтинг этого фильма больше чем 6?",
-                         corrcetAnswer: true),
-            QuizQuestion(image: "Old",
-                         text: "Рейтинг этого фильма больше чем 6?",
-                         corrcetAnswer: false),
-            QuizQuestion(image: "The Ice Age Adventures of Buck Wild",
-                         text: "Рейтинг этого фильма больше чем 6?",
-                         corrcetAnswer: false),
-            QuizQuestion(image: "Tesla",
-                         text: "Рейтинг этого фильма больше чем 6?",
-                         corrcetAnswer: false),
-            QuizQuestion(image: "Vivarium",
-                         text: "Рейтинг этого фильма больше чем 6?",
-                         corrcetAnswer: false)
+        QuizQuestion(image: "The Godfather"
+                     , text: "Рейтинг этого фильма больше чем 6?"
+                     , corrcetAnswer: true),
+        QuizQuestion(image: "The Dark Knight",
+                     text: "Рейтинг этого фильма больше чем 6?",
+                     corrcetAnswer: true),
+        QuizQuestion(image: "Kill Bill",
+                     text: "Рейтинг этого фильма больше чем 6?",
+                     corrcetAnswer: true),
+        QuizQuestion(image: "The Avengers",
+                     text: "Рейтинг этого фильма больше чем 6?",
+                     corrcetAnswer: true),
+        QuizQuestion(image: "Deadpool",
+                     text: "Рейтинг этого фильма больше чем 6?",
+                     corrcetAnswer: true),
+        QuizQuestion(image: "The Green Knight",
+                     text: "Рейтинг этого фильма больше чем 6?",
+                     corrcetAnswer: true),
+        QuizQuestion(image: "Old",
+                     text: "Рейтинг этого фильма больше чем 6?",
+                     corrcetAnswer: false),
+        QuizQuestion(image: "The Ice Age Adventures of Buck Wild",
+                     text: "Рейтинг этого фильма больше чем 6?",
+                     corrcetAnswer: false),
+        QuizQuestion(image: "Tesla",
+                     text: "Рейтинг этого фильма больше чем 6?",
+                     corrcetAnswer: false),
+        QuizQuestion(image: "Vivarium",
+                     text: "Рейтинг этого фильма больше чем 6?",
+                     corrcetAnswer: false)
     ]
     
     override func viewDidLoad() {
         show(quiz: convert(model: questions[currentQuestionIndex]))
         super.viewDidLoad()
     }
-    
+    // метод вызывается, когда пользователь нажимает на кнопку "Нет"
     @IBAction private func noButtonClicked(_ sender: Any) {
+        //        if questions[currentQuestionIndex].corrcetAnswer == false {
+        //            showAnswerResult(isCorrect: true)
+        //            correctAnswers += 1
+        //        } else {
+        //            showAnswerResult(isCorrect: false)
+        //        }
+        //
+        //        if currentQuestionIndex+1 < questions.count {
+        //            currentQuestionIndex += 1
+        //        } else {
+        //            currentQuestionIndex = 0
+        //            correctAnswers = 0
+        //        }
+        //        show(quiz: convert(model: questions[currentQuestionIndex]))
+        
+        let currentQuestion = questions[currentQuestionIndex]
+        let givenAnswer = false
+        
+        showAnswerResult(isCorrect: givenAnswer == currentQuestion.corrcetAnswer)
+        
     }
+    // метод вызывается, когда пользователь нажимает на кнопку "Да"
     @IBAction private func yesButtonClicked(_ sender: Any) {
+        //        if questions[currentQuestionIndex].corrcetAnswer == true {
+        //            showAnswerResult(isCorrect: true)
+        //            correctAnswers += 1
+        //        } else {
+        //            showAnswerResult(isCorrect: false)
+        //        }
+        //
+        //        if currentQuestionIndex+1 < questions.count {
+        //            currentQuestionIndex += 1
+        //        } else {
+        //            currentQuestionIndex = 0
+        //            correctAnswers = 0
+        //        }
+        //        show(quiz: convert(model: questions[currentQuestionIndex]))
+        
+        let currentQuestion = questions[currentQuestionIndex]
+        let givenAnswer = true
+        
+        showAnswerResult(isCorrect: givenAnswer == currentQuestion.corrcetAnswer)
     }
     @IBOutlet private weak var imageView: UIImageView!
     @IBOutlet private weak var textLabel: UILabel!
@@ -67,6 +107,14 @@ final class MovieQuizViewController: UIViewController {
         imageView.image = step.image
         textLabel.text = step.question
         counterLabel.text = step.questionNumber
+    }
+    
+    private func showAnswerResult(isCorrect: Bool) {
+        imageView.layer.masksToBounds = true // даём разрешение на рисование рамки
+        imageView.layer.borderWidth = 1 // толщина рамки
+        imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor//
+        imageView.layer.cornerRadius = 6 // радиус скругления углов рамки
+        
     }
 }
 
