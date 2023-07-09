86c5dc63ba12aa0efd3c9b5cb7b3d1c0e4ef0091
Чупрыненко Андрей <wotmaster187@icloud.com>
2023-07-09 21:07:50 +0300

    Добавлено обращение к протоколу фабрики вопросов

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -51,8 +51,8 @@ final class MovieQuizViewController: UIViewController {
     //общее количество вопросов
     private let questionsAmount: Int = 10
     
-    //обращение к фабрике вопросов
-    private let questionFactory: QuestionFactory = QuestionFactory()
+    //обращение к протоколу фабрики вопросов
+    private let questionFactory: QuestionFactoryProtocol = QuestionFactory()
     
     //текущий вопрос, который видит пользователь
     private var currentQuestion: QuizQuestion?
