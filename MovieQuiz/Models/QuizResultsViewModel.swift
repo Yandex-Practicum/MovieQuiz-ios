a7c6b5954ba106be71d8a7f132d78a0bc6b39c29
Чупрыненко Андрей <wotmaster187@icloud.com>
2023-07-09 16:52:47 +0300

    созданная отдельный файлы для моделей

---
+++ MovieQuiz/Models/QuizResultsViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  QuizResultsViewModel.swift
+//  MovieQuiz
+//
+//  Created by Андрей Чупрыненко on 09.07.2023.
+//
+
+import Foundation
+
+// для состояния "Результат квиза"
+struct QuizResultsViewModel {
+  let title: String
+  let text: String
+  let buttonText: String
+}
---
+++ MovieQuiz/Models/QuizStepViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  QuizStepViewModel.swift
+//  MovieQuiz
+//
+//  Created by Андрей Чупрыненко on 09.07.2023.
+//
+
+import Foundation
+import UIKit
+
+// вью модель для состояния "Вопрос показан"
+struct QuizStepViewModel {
+  let image: UIImage
+  let question: String
+  let questionNumber: String
+}
