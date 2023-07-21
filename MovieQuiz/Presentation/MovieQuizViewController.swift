57d28e655840d5edbd1c7e7b4f676d495c5ef134
2Neo2 <ivandoronin22@gmail.com>
2023-07-21 13:46:13 +0300

    Changing the state of the day (sun and moon)  when you click on the button.

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,10 +1,64 @@
 import UIKit
 
 final class MovieQuizViewController: UIViewController {
+    
+    @IBOutlet weak var changeDayConditionButton: UIButton!
+    @IBOutlet weak var dayConditionImageView: UIImageView!
     // MARK: - Lifecycle
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            setupView()
+        }
+        
+        func setupView() {
+            // MARK: Setup Image.
+            dayConditionImageView.image = UIImage(systemName: "moon")
+            dayConditionImageView.contentMode = .scaleAspectFill
+            dayConditionImageView.tintColor = .black
+            
+            
+            // MARK: Setup button.
+            changeDayConditionButton.layer.cornerRadius = 7
+            
+            changeDayConditionButton.setTitle("Change color of a sun!", for: .normal)
+            
+            changeDayConditionButton.backgroundColor = .black
+            changeDayConditionButton.tintColor = .white
+            
+            
+            changeDayConditionButton.addTarget(self, action: #selector(changeImageColorHandler), for: .touchUpInside)
+            
+            
+            dayConditionImageView.translatesAutoresizingMaskIntoConstraints = false
+            
+            changeDayConditionButton.translatesAutoresizingMaskIntoConstraints = false
+            
+            NSLayoutConstraint.activate([
+                dayConditionImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+                dayConditionImageView.widthAnchor.constraint(equalToConstant: view.frame.width),
+                dayConditionImageView.heightAnchor.constraint(equalToConstant: 400),
+                dayConditionImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                dayConditionImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                changeDayConditionButton.topAnchor.constraint(equalTo: dayConditionImageView.bottomAnchor, constant: 150),
+                changeDayConditionButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            ])
+                
+        }
+        
+        @objc func changeImageColorHandler() {
+            
+            if (dayConditionImageView.tintColor == .yellow) {
+                dayConditionImageView.image = UIImage(systemName: "moon")
+                dayConditionImageView.tintColor = .black
+                changeDayConditionButton.backgroundColor = .black
+            } else {
+                dayConditionImageView.tintColor = .yellow
+                dayConditionImageView.image = UIImage(systemName: "sun.max")
+                changeDayConditionButton.backgroundColor = .blue
+            }
+            
+            
+        }
 }
 
 /*
