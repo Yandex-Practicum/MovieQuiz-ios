ec13299226ac9d30c1a95f57c11331848f603d0e
nyky75 <nyky1937@icloud.com>
2023-07-27 01:30:47 +0300

    added txt

--- MovieQuiz/AppDelegate.swift
+++ MovieQuiz/AppDelegate.swift
@@ -6,7 +6,7 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-        print("Hello world")
+        print("Hello world test brn")
         return true
     }
 
