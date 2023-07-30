afeaaa4946154874155fd8baefe570ac3baed378
Taisia Galkina <galkina.ttasya@gmail.com>
2023-07-30 09:51:26 +0300

    ;(

--- MovieQuizPresenterTests/MovieQuizPresenterTests.swift
+++ MovieQuizPresenterTests/MovieQuizPresenterTests.swift
@@ -3,27 +3,27 @@ import XCTest
 
 final class MovieQuizViewControllerMock: MovieQuizViewControllerProtocol {
     func show(quiz step: QuizStepViewModel) {
-    
+        
     }
     
     func show(quiz result: QuizResultsViewModel) {
-    
+        
     }
     
     func highlightImageBorder(isCorrectAnswer: Bool) {
-    
+        
     }
     
     func showLoadingIndicator() {
-    
+        
     }
     
     func hideLoadingIndicator() {
-    
+        
     }
     
     func showNetworkError(message: String) {
-    
+        
     }
 }
 
@@ -36,7 +36,7 @@ final class MovieQuizPresenterTests: XCTestCase {
         let question = QuizQuestion(image: emptyData, text: "Question Text", correctAnswer: true)
         let viewModel = sut.convert(model: question)
         
-         XCTAssertNotNil(viewModel.image)
+        XCTAssertNotNil(viewModel.image)
         XCTAssertEqual(viewModel.question, "Question Text")
         XCTAssertEqual(viewModel.questionNumber, "1/10")
     }
