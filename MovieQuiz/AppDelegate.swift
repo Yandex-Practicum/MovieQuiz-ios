8554b8be97997bb55194ccdadf926ca31fa81c5a
Aleksandra <aleksandra.kosnyreva@mail.ru>
2023-07-31 22:04:47 +0500

    Coздана новая запись в функции application

--- MovieQuiz/AppDelegate.swift
+++ MovieQuiz/AppDelegate.swift
@@ -7,7 +7,7 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
         return true
-        print("Hello world for branch to merge")
+        print("Hi, world for branch to merge! ")
     }
 
     // MARK: UISceneSession Lifecycle
