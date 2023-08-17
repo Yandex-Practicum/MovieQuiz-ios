24cc7cbab63eebefa8554cc13137c5491924543e
ArthurM2 <38484496+ArthurM2@users.noreply.github.com>
2023-08-17 12:21:50 +0500

    -фикс теста

--- MovieQuizTests/MoviesLoaderTests.swift
+++ MovieQuizTests/MoviesLoaderTests.swift
@@ -4,8 +4,8 @@ import XCTest
 class MoviesLoaderTests: XCTestCase {
     func testSuccessLoading()throws {
         // Given
-        let loader = MoviesLoader()
         let stubNetworkClient = StubNetworkClient(emulateError: false)
+        let loader = MoviesLoader(networkClient: stubNetworkClient)
         
         // When
         let expectation = expectation(description: "Loading expectation")
@@ -26,8 +26,8 @@ class MoviesLoaderTests: XCTestCase {
     
     func testFailureLoading() throws {
         // Given
-        let loader = MoviesLoader()
         let stubNetworkClient = StubNetworkClient(emulateError: true)
+        let loader = MoviesLoader(networkClient: stubNetworkClient)
         
         // When
         let expectation = expectation(description: "Loading expectation")
