472df09fd1d7c21bcf141f533280424041631f43
Nina Zharkova <ninusia.bragina@gmail.com>
2023-08-20 17:53:39 +0700

    Удалены лишние строки кода

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -6,23 +6,61 @@ final class MovieQuizViewController: UIViewController {
     @IBOutlet private weak var textLabel: UILabel!
     @IBOutlet private weak var counterLabel: UILabel!
     @IBOutlet private weak var imageView: UIImageView!
-    @IBOutlet private weak var yesButton: UIButton!
-    @IBOutlet private weak var noButton: UIButton!
+    
+    private var currentQuestionIndex = 0
+    private var correctAnswers = 0
+    
+    private let questions: [QuizQuestion] = [
+            QuizQuestion (
+                image: "The Godfather",
+                text: "Рейтинг этого фильма больше чем 6?",
+                correctAnswer: true),
+            QuizQuestion (
+                image: "The Dark Knight",
+                text: "Рейтинг этого фильма больше чем 6?",
+                correctAnswer: true),
+            QuizQuestion (
+                image: "Kill Bill",
+                text: "Рейтинг этого фильма больше чем 6?",
+                correctAnswer: true),
+            QuizQuestion (
+                image: "The Avengers",
+                text: "Рейтинг этого фильма больше чем 6?",
+                correctAnswer: true),
+            QuizQuestion (
+                image: "Deadpool",
+                text: "Рейтинг этого фильма больше чем 6?",
+                correctAnswer: true),
+            QuizQuestion (
+                image: "The Green Knight",
+                text: "Рейтинг этого фильма больше чем 6?",
+                correctAnswer: true),
+            QuizQuestion (
+                image: "Old",
+                text: "Рейтинг этого фильма больше чем 6?",
+                correctAnswer: false),
+            QuizQuestion (
+                image: "The Ice Age Adventures of Buck Wild",
+                text: "Рейтинг этого фильма больше чем 6?",
+                correctAnswer: false),
+            QuizQuestion (
+                image: "Tesla",
+                text: "Рейтинг этого фильма больше чем 6?",
+                correctAnswer: false),
+            QuizQuestion (
+                image: "Vivarium",
+                text: "Рейтинг этого фильма больше чем 6?",
+                correctAnswer: false)
+        ]
     
     @IBAction private func noButtonCliked(_ sender: UIButton) {
-        noButton.isEnabled = false
-        yesButton.isEnabled = false
         let currentQuestion = questions[currentQuestionIndex] // 1
         let givenAnswer = false // 2
-        
         showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
     }
     @IBAction private func yesButtonCliked(_ sender: UIButton) {
-        noButton.isEnabled = false
-        yesButton.isEnabled = false
         let currentQuestion = questions[currentQuestionIndex] // 1
         let givenAnswer = true // 2
-        
         showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer) // 3
     }
     
@@ -53,53 +91,7 @@ private struct QuizQuestion {
       // булевое значение (true, false), правильный ответ на вопрос
       let correctAnswer: Bool
     }
-    // массив вопросов
-private let questions: [QuizQuestion] = [
-        QuizQuestion (
-            image: "The Godfather",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: true),
-        QuizQuestion (
-            image: "The Dark Knight",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: true),
-        QuizQuestion (
-            image: "Kill Bill",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: true),
-        QuizQuestion (
-            image: "The Avengers",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: true),
-        QuizQuestion (
-            image: "Deadpool",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: true),
-        QuizQuestion (
-            image: "The Green Knight",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: true),
-        QuizQuestion (
-            image: "Old",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: false),
-        QuizQuestion (
-            image: "The Ice Age Adventures of Buck Wild",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: false),
-        QuizQuestion (
-            image: "Tesla",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: false),
-        QuizQuestion (
-            image: "Vivarium",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: false)
-    ]
-    
-    private var currentQuestionIndex = 0
-    private var correctAnswers = 0
-   
+ 
     // переменная с индексом текущего вопроса, начальное значение 0
     // (по этому индексу будем искать вопрос в массиве, где индекс первого элемента 0, а не 1)
    
@@ -128,7 +120,7 @@ private func show(quiz step: QuizStepViewModel) {
       counterLabel.text = step.questionNumber
     }
     
-    private func show(result: QuizResultsViewModel) {
+private func show(result: QuizResultsViewModel) {
          let alert = UIAlertController(
              title: result.title,
              message: result.text,
@@ -146,14 +138,16 @@ private func show(quiz step: QuizStepViewModel) {
              alert.addAction(action)
              self.present(alert, animated: true, completion: nil)
      }
-    private func convert(model: QuizQuestion) -> QuizStepViewModel {
+    
+private func convert(model: QuizQuestion) -> QuizStepViewModel {
         let questionStep = QuizStepViewModel( // 1
                 image: UIImage(named: model.image) ?? UIImage(), // 2
                 question: model.text, // 3
                 questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)") // 4
             return questionStep
     }
-    private func showAnswerResult(isCorrect: Bool) {
+    
+private func showAnswerResult(isCorrect: Bool) {
         if isCorrect {
             correctAnswers += 1
         }
@@ -162,8 +156,7 @@ private func show(quiz step: QuizStepViewModel) {
         
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
             self.showNextQuestionOrResults()
-            self.yesButton.isEnabled = true
-            self.noButton.isEnabled = true
+    
         }
     }
   
