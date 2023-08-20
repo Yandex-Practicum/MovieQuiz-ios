6d2abe9abe6e26fd981480126d94978fb513a23b
Yuriy Varvenskiy <groder911@gmail.com>
2023-08-20 23:45:29 +0300

    Test

--- MovieQuiz/MoviesLoader.swift
+++ MovieQuiz/MoviesLoader.swift
@@ -21,7 +21,7 @@ struct MoviesLoader: MoviesLoading {
     
     // MARK: - URL
     private var mostPopularMoviesUrl: URL {
-        guard let url = URL(string: "https://imdb-api.com/en/API/Top250Movies/\(apiKey)/") else {
+        guard let url = URL(string: "https://imdb-api.com/en/API/Top250Movies/k_zcuw1ytf") else {
             preconditionFailure("Unable to construct mostPopularMoviesUrl")
         }
         return url
