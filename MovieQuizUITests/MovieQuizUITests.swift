fea640045c6d94118a8b7a4d1c7daeca942a9648
chrnivan <van.chrn@gmail.com>
2023-08-21 19:28:54 +0300

    ва

--- MovieQuizUITests/MovieQuizUITests.swift
+++ MovieQuizUITests/MovieQuizUITests.swift
@@ -67,7 +67,7 @@ class MovieQuizUITests: XCTestCase {
             sleep(2)
         }
         
-        let alert = app.alerts["Game results"]
+        let alert = app.alerts["Этот раунд окончен!"]
         
         XCTAssertTrue(alert.exists)
         XCTAssertTrue(alert.label == "Этот раунд окончен!")
@@ -81,7 +81,7 @@ class MovieQuizUITests: XCTestCase {
             sleep(2)
         }
         
-        let alert = app.alerts["Game results"]
+        let alert = app.alerts["Этот раунд окончен!"]
         alert.buttons.firstMatch.tap()
         
         sleep(2)
