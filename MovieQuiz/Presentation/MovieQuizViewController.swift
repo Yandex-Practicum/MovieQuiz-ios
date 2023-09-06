377051651da33827564cf7299bf164afa93c52b1
Kirill Arefyev <arefyev2009@yandex.ru>
2023-09-07 01:26:35 +0300

    -- добавлены и интегрированы в конфигурацию шрифты по макету (Fonts, Info) -- добавлены в ассеты и расширение цвета по макету (Assets, UIColor+Extensions) -- cоздан экран запуска по макету (LaunchScreen) -- создан главный экран по макету (Main) -- реализована логика главного экрана приложения: 	- при запуске главного экрана появляется индекс текущего вопроса, постер фильма, текст вопроса, кнопки "Нет" и "Да" (вопросы на основе Mock-данных ) 	- при нажатии на вариант ответа появляется рамка вокруг постера с соответствующим цветом 	- через 1 секунду появляется экран следующего вопроса, меняется индекс текущего вопроса (1/10 -> 2/10 ...) 	- по завершении раунда из 10 вопросов появляется алерт "Раунд окончен" со статистикой раунда (количеством правильных ответов) и возможностью запустить новый раунд квиза

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,11 +1,200 @@
 import UIKit
 
 final class MovieQuizViewController: UIViewController {
+    
+    // MARK: - Outlets & Actions
+    
+    @IBOutlet private weak var moviePosterImageView: UIImageView!
+    @IBOutlet private weak var textOfQuestionLabel: UILabel!
+    @IBOutlet private weak var indexOfQuestionLabel: UILabel!
+    
+    @IBAction private func noButtonTapped(_ sender: Any) {
+        let userAnswer = false
+        
+        showAnswerResult(isCorrect: userAnswer)
+    }
+    
+    @IBAction private func yesButtonTapped(_ sender: Any) {
+        let userAnswer = true
+        
+        showAnswerResult(isCorrect: userAnswer)
+    }
+    
+    // MARK: - Structures
+    
+    /// вью модель состояния "Вопрос показан"
+    private struct QuizStepViewModel {
+        let image: UIImage
+        let question: String
+        let questionNumber: String
+    }
+    
+    /// вью модель состояния "Результат квиза"
+    private struct QuizResultViewModel {
+        let title: String
+        let text: String
+        let buttonText: String
+    }
+    
+    /// модель вопроса квиза
+    private struct QuizQuestion {
+        let image: String
+        let text: String
+        let correctAnswer: Bool
+    }
+    
+    // MARK: - Arrays, Variables, Constants
+    
+    /// массив моковых вопросов
+    private var questions: [QuizQuestion] = [
+        QuizQuestion(
+            image: "The Godfather",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "The Dark Knight",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "Kill Bill",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "The Avengers",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "Deadpool",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "The Green Knight",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "Old",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false),
+        QuizQuestion(
+            image: "The Ice Age Adventures of Buck Wild",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false),
+        QuizQuestion(
+            image: "Tesla",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false),
+        QuizQuestion(
+            image: "Vivarium",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false)
+    ]
+    
+    /// переменная с индексом текущего вопроса
+    private var currentQuizQuestionIndex = 0
+    
+    /// переменная счетчика правильных ответов
+    private var userCorrectAnswers = 0
+    
     // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        /// метод для перемешивания элементов массива - немного рандома для интереса
+        questions.shuffle()
+        
+        let questionStep = questions[currentQuizQuestionIndex]
+        let viewModel = convert(model: questionStep)
+        
+        showStep(quiz: viewModel)
+    }
+    
+    // MARK: - Helper methods
+    
+    /// метод конвертации модели вопроса квиза во вью модель состояния "Вопрос показан"
+    private func convert(model: QuizQuestion) -> QuizStepViewModel {
+        let questionStep = QuizStepViewModel(
+            image: UIImage(named: model.image) ?? UIImage(),
+            question: model.text,
+            questionNumber: "\(currentQuizQuestionIndex + 1)/\(questions.count)")
+        return questionStep
+    }
+    
+    /// метод для состояния "Вопрос показан"
+    private func showStep(quiz step: QuizStepViewModel) {
+        moviePosterImageView.image = step.image
+        textOfQuestionLabel.text = step.question
+        indexOfQuestionLabel.text = step.questionNumber
+    }
+    
+    /// метод для изменения цвета рамки
+    private func showAnswerResult(isCorrect: Bool) {
+        moviePosterImageView.layer.masksToBounds = true
+        moviePosterImageView.layer.borderWidth = 8
+        moviePosterImageView.layer.cornerRadius = 20
+        
+        if isCorrect == questions[currentQuizQuestionIndex].correctAnswer {
+            moviePosterImageView.layer.borderColor = UIColor.ypGreen.cgColor
+            userCorrectAnswers += 1
+        } else {
+            moviePosterImageView.layer.borderColor = UIColor.ypRed.cgColor
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            self.showNextQuestionOrResults()
+            
+            /// "убираем" рамку после предыдущего ответа
+            self.moviePosterImageView.layer.borderWidth = 0
+            self.moviePosterImageView.layer.borderColor = UIColor.clear.cgColor
+        }
+    }
+    
+    /// метод перехода в один из сценариев конечного автомата
+    private func showNextQuestionOrResults() {
+        if currentQuizQuestionIndex == questions.count - 1 {
+            showResult(quiz: QuizResultViewModel(
+                title: "Этот раунд окончен!",
+                text: "Ваш результат: \(userCorrectAnswers)/\(questions.count)",
+                buttonText: "Сыграть еще раз"))
+        } else {
+            currentQuizQuestionIndex += 1
+            
+            let questionStep = questions[currentQuizQuestionIndex]
+            let viewModel = convert(model: questionStep)
+            
+            showStep(quiz: viewModel)
+        }
+    }
+    
+    /// метод для состояния "Результат квиза"
+    private func showResult(quiz result: QuizResultViewModel) {
+        let alert = UIAlertController(
+            title: result.title,
+            message: result.text,
+            preferredStyle: .alert)
+        
+        let alertAction = UIAlertAction(
+            title: result.buttonText,
+            style: .default) {_ in
+                self.currentQuizQuestionIndex = 0
+                self.userCorrectAnswers = 0
+                
+                /// снова перемешиваем элементы массива
+                self.questions.shuffle()
+                
+                let startQuestion = self.questions[self.currentQuizQuestionIndex]
+                let startView = self.convert(model: startQuestion)
+            
+                self.showStep(quiz: startView)
+            }
+        
+        alert.addAction(alertAction)
+        
+        self.present(alert, animated: true, completion: nil)
     }
 }
+
+// MARK: - Mocks
 
 /*
  Mock-данные
