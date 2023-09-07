4dde4fefb1fd2c328f10d0b3afcf128c7ebde87a
Aleksandra <aleksandra.kosnyreva@mail.ru>
2023-09-08 04:33:39 +0500

    Created AlertPresenter.

---
+++ MovieQuiz/Models/QuizResultsViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  QuizResultsViewModel.swift
+//  MovieQuiz
+//
+//  Created by Александра Коснырева on 07.09.2023.
+//
+
+import Foundation
+
+struct QuizResultsViewModel{
+    let title: String
+    let text: String
+    let buttonText: String
+}
---
+++ MovieQuiz/Services/AlertPresenerProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  AlertPresenerProtocol.swift
+//  MovieQuiz
+//
+//  Created by Александра Коснырева on 07.09.2023.
+//
+
+import Foundation
+
+protocol AlertPresenterProtocol {
+    func show(alertModel: AlertModel)
+}
