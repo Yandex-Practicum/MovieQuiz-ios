4dec5debbfdfc60f6c814124b155d855dd0034f0
Khamin Gleb <nimik19@yandex.ru>
2023-09-12 17:54:29 +0300

    Кнопкам забыл прописать приватность, увидел после размещения первого комита

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -88,8 +88,8 @@ final class MovieQuizViewController: UIViewController {
                 correctAnswer: false)
         ]
 
-    @IBOutlet weak var noButton: UIButton!
-    @IBOutlet weak var yesButton: UIButton!
+    @IBOutlet private weak var noButton: UIButton!
+    @IBOutlet private weak var yesButton: UIButton!
     @IBOutlet private weak var imageView: UIImageView!
     @IBOutlet private weak var textLabel: UILabel!
     @IBOutlet private weak var counterLabel: UILabel!
