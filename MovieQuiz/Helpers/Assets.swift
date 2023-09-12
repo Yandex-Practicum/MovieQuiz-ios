58014a192c2ec4599a3f48a59d4e4b2a8478faa6
LolaChernysheva <lolita-chernysheva@mail.ru>
2023-09-12 12:30:00 +0700

    Added displaying questions, added class for saving images

---
+++ MovieQuiz/Helpers/Assets.swift
@@ -0,0 +1,41 @@
+//
+//  Assets.swift
+//  MovieQuiz
+//
+//  Created by Лолита Чернышева on 03.09.2023.
+//
+
+import UIKit
+
+public class Assets: NSObject {
+    public static var deadpoolImageName = "Deadpool"
+    public static var deadpoolImage: UIImage? { UIImage(named: Assets.deadpoolImageName) }
+    
+    public static var killBillImageName = "Kill Bill"
+    public static var killBillImage: UIImage? { UIImage(named: Assets.killBillImageName) }
+    
+    public static var oldImageName = "Old"
+    public static var oldImage: UIImage? { UIImage(named: Assets.oldImageName) }
+    
+    public static var teslaImageName = "Tesla"
+    public static var teslaImage: UIImage? { UIImage(named: Assets.teslaImageName) }
+    
+    public static var avengersImageName = "The Avengers"
+    public static var avengersImage: UIImage? { UIImage(named: Assets.avengersImageName) }
+    
+    public static var darkKnightImageName = "The Dark Knight"
+    public static var darkKnightImage: UIImage? { UIImage(named: Assets.darkKnightImageName) }
+    
+    public static var godfatherImageName = "The Godfather"
+    public static var godfatherImage: UIImage? { UIImage(named: Assets.godfatherImageName) }
+    
+    public static var greenKnightImageName = "The Green Knight"
+    public static var greenKnightImage: UIImage? { UIImage(named: Assets.greenKnightImageName) }
+    
+    public static var iceAgeAdventuresImageName = "The Ice Age Adventures of Buck Wild"
+    public static var iceAgeAdventuresImage: UIImage? { UIImage(named: Assets.iceAgeAdventuresImageName) }
+    
+    public static var vivariumImageName = "Vivarium"
+    public static var vivariumImage: UIImage? { UIImage(named: Assets.vivariumImageName) }
+    
+}
