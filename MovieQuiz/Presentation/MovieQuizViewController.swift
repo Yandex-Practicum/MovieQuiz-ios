ae005f8d912f68c2173e60ed2cb69b5d1b0c37fe
shatt21 <poller2224@yandex.ru>
2023-09-15 00:49:31 +0300

    Верстка по макету

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,10 +1,41 @@
 import UIKit
 
+
 final class MovieQuizViewController: UIViewController {
+   
+    
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
 /*
