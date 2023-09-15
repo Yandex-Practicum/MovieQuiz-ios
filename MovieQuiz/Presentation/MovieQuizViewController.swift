c930706d87f8b14edd316a61ac9f7770ab0f0df3
shatt21 <poller2224@yandex.ru>
2023-09-16 01:31:19 +0300

    Готовый проект

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -2,41 +2,187 @@ import UIKit
 
 
 final class MovieQuizViewController: UIViewController {
-   
+    // MARK: - Constants
+    private let question: [QuizeQuestion] = [
+        QuizeQuestion(image: "The Godfather",
+                      text: "Рейтинг этого фильма больше чем 6?",
+                      correctAnswer: true),
+        QuizeQuestion(image: "The Dark Knight",
+                      text: "Рейтинг этого фильма больше чем 6?",
+                      correctAnswer: true),
+        QuizeQuestion(image: "Kill Bill",
+                      text: "Рейтинг этого фильма больше чем 6?",
+                      correctAnswer: true),
+        QuizeQuestion(image: "The Avengers",
+                      text: "Рейтинг этого фильма больше чем 6?",
+                      correctAnswer: true),
+        QuizeQuestion(image: "Deadpool",
+                      text: "Рейтинг этого фильма больше чем 6?",
+                      correctAnswer: true),
+        QuizeQuestion(image: "The Green Knight",
+                      text: "Рейтинг этого фильма больше чем 6?",
+                      correctAnswer: true),
+        QuizeQuestion(image: "Old",
+                      text: "Рейтинг этого фильма больше чем 6?",
+                      correctAnswer: false),
+        QuizeQuestion(image: "The Ice Age Adventures of Buck Wild",
+                      text: "Рейтинг этого фильма больше чем 6?",
+                      correctAnswer: false),
+        QuizeQuestion(image: "Tesla",
+                      text: "Рейтинг этого фильма больше чем 6?",
+                      correctAnswer: false),
+        QuizeQuestion(image: "Vivarium",
+                      text: "Рейтинг этого фильма больше чем 6?",
+                      correctAnswer: false)
+    
+    ]
+    
+    private var currentQuestionIndex = 0             // номер вопроса
+    private var correctAnswers = 0                    // счетчик правильных ответов
+    
+    
+    // MARK: - IBAction
+    
+    @IBAction func noButtonClicked(_ sender: UIButton) {
+        let currectQuestion = question[currentQuestionIndex]
+        let giveAnswer = false
+        
+        showAnswerResult(isCorrect: giveAnswer == currectQuestion.correctAnswer)
+    }
+    @IBAction private func yesButtonClicked(_ sender: UIButton) {
+        let currectQuestion = question[currentQuestionIndex]
+        let giveAnswer = true
+        
+        showAnswerResult(isCorrect: giveAnswer == currectQuestion.correctAnswer)
+    }
+    
+    // MARK: - IBOutlet
+    
+    @IBOutlet private var imageView: UIImageView!
+    @IBOutlet private var textView: UILabel!
+    @IBOutlet private var counterLabel: UILabel!
+    
     
     
     // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let firstQuestion = question[currentQuestionIndex]
+        let model = convert(firstQuestion)
+        self.show(quiz: model)
+        
+        
     }
     
+    // MARK: - Struct
     
+    struct ViewModel {
+        let image: UIImage
+        let question: String
+        let questionNumber: String
+    }
     
+    // Вопрос показан
+    struct QuizStepViewModel {
+        let image: UIImage
+        let question: String
+        let questionNumber: String
+    }
     
+    // Результат квиза
+    struct QuizResultsViewModel {
+        let title: String
+        let text: String
+        let buttonText: String
+    }
     
+    // Структура вопроса
+    struct QuizeQuestion {
+        let image: String
+        let text: String
+        let correctAnswer: Bool
+    }
     
+
     
+    // MARK: - Private Methods
     
+    private func convert(_ model: QuizeQuestion) -> QuizStepViewModel {
+        let questionStep = QuizStepViewModel(
+            image: UIImage(named: model.image) ?? UIImage(),
+            question: model.text,
+            questionNumber: "\(currentQuestionIndex + 1)/\(question.count)")
+        return questionStep
+    }
+
+    private func show(quiz step: QuizStepViewModel) {
+        imageView.image = step.image
+        textView.text = step.question
+        counterLabel.text = step.questionNumber
+    }
     
+    private func showAnswerResult(isCorrect: Bool) {
+       if isCorrect {
+            correctAnswers += 1
+        }
+        imageView.layer.masksToBounds = true
+        imageView.layer.borderWidth = 8
+        imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+           self.showNextQuestionOrResults()
+        }
+    }
     
+    private func showNextQuestionOrResults() {
+        if currentQuestionIndex == question.count - 1 {
+            let text = "Ваш результат: \(correctAnswers)/10"
+            let viewModel = QuizResultsViewModel(
+                title: "Этот раунд окончен!",
+                text: text,
+                buttonText: "Сыграть ещё раз")
+            show(quiz: viewModel)
+            
+        } else {
+            currentQuestionIndex += 1
+            let firstQuestion = question[currentQuestionIndex]
+            let model = convert(firstQuestion)
+            self.show(quiz: model)
+            
+        }
+    }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    // приватный метод для показа результатов раунда квиза
+    // принимает вью модель QuizResultsViewModel и ничего не возвращает
+    private func show(quiz result: QuizResultsViewModel) {
+        let alert = UIAlertController(
+            title: result.title,
+            message: result.text,
+            preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: result.buttonText, style: .default) { _ in
+            self.currentQuestionIndex = 0
+            self.correctAnswers = 0
+            
+            let firstQuestion = self.question[self.currentQuestionIndex]
+            let viewModel = self.convert(firstQuestion)
+            self.show(quiz: viewModel)
+        }
+        
+        alert.addAction(action)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
     
 }
+
+
+
+
+
+
 
 /*
  Mock-данные
