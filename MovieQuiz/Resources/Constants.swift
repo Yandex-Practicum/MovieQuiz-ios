b552e7b612b8ffb01314370cac6fd4ee6e367847
ArtemChalkov <F9152005012@yandex.ru>
2023-09-27 14:52:29 +0300

    задача спринта 4 на ревью

---
+++ MovieQuiz/Resources/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  MovieQuiz
+//
+//  Created by Артем Чалков on 20.09.2023.
+//
+
+import UIKit
+
+enum Colors {
+    static let ypBlack = UIColor(red: 0.102, green: 0.106, blue: 0.133, alpha: 1)
+    static let ypWhite = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+    static let ypGreen = UIColor(red: 0.376, green: 0.761, blue: 0.557, alpha: 1)
+    static let ypRed = UIColor(red: 0.961, green: 0.42, blue: 0.424, alpha: 1)
+    static let ypGray = UIColor(red: 0.902, green: 0.91, blue: 0.922, alpha: 1)
+    static let ypBackground = UIColor(red: 0.102, green: 0.106, blue: 0.133, alpha: 0.6)
+}
