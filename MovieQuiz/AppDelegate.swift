4e9076d4630ed8f61cf5b61f97928928c6a788f3
Alex Petrenko <alex.pt@ya.ru>
2023-09-30 19:49:24 +0300

    Добавить LaunchScreen, привязать constraints

--- MovieQuiz/AppDelegate.swift
+++ MovieQuiz/AppDelegate.swift
@@ -6,7 +6,6 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-        print("Hello world for branch to merge")
         return true
     }
 
