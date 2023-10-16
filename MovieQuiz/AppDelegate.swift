8384f239a566071ace0d4d7f7700b3e3d157b441
loshilov ilya <loshilovilya@yandex.ru>
2023-10-16 17:44:36 +0300

    изменен тестовый вывод в консоль

--- MovieQuiz/AppDelegate.swift
+++ MovieQuiz/AppDelegate.swift
@@ -6,7 +6,7 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-        print("hello world")
+        print("Hello world for branch to merge")
         return true
     }
 
