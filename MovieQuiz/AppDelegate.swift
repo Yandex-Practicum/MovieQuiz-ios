5de54c246f0ad0d2e067e0f3497817f1844d3602
Fedor <f.zavyalov@mail.ru>
2023-10-17 01:05:41 -0700

    Отработка навыков создания "коммита" на GitHub.

--- MovieQuiz/AppDelegate.swift
+++ MovieQuiz/AppDelegate.swift
@@ -6,6 +6,7 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
+        print("Привет! Тест с Git Commit!")
         return true
     }
 
