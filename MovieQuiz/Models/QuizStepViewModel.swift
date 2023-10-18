d702815f3c9c42552c12520c1f4b57c0181e1382
shatt21 <poller2224@yandex.ru>
2023-10-18 03:50:06 +0300

    создание фактории

---
+++ MovieQuiz/Models/QuizStepViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  MovieQuiz
+//
+//  Created by Паша Шатовкин on 18.10.2023.
+//
+
+import Foundation
---
+++ MovieQuiz/Services/QuestionFactory.swift
@@ -0,0 +1,8 @@
+//
+//  QuestionFactory.swift
+//  MovieQuiz
+//
+//  Created by Паша Шатовкин on 18.10.2023.
+//
+
+import Foundation
