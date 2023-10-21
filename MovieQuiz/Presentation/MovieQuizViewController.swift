a95fc83cb5bd1eb3d4a9f981a42eaf1bfb984810
alekzenwork <alekzenmusic@gmail.com>
2023-10-21 22:57:30 +0300

    Законченный проект. Дополнен: 1. приватным методом, который меняет цвет рамки принимает на вход булевое значение и ничего не возвращает 2. приватным методом, который содержит логику перехода в один из сценариев метод ничего не принимает и ничего не возвращает 3. alert

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -25,91 +25,167 @@ final class MovieQuizViewController: UIViewController {
         self.show(quiz: firstQuestionViewModel)
     }
     
+    
     @IBAction func noButtonClicked(_ sender: UIButton) {
+        let currentQuestion = questions[currentQuestionIndex]
+        let givenAnswer = false
+        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
+        
     }
     @IBAction func yesButtonClicked(_ sender: UIButton) {
+        let currentQuestion = questions[currentQuestionIndex]
+        let givenAnswer = true
+        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
     }
     
+    // структура для преобразования моков во вью модель
     struct QuizQuestion {
         let image: String
         let text: String
         let correctAnswer: Bool
     }
-
-    private let questions: [QuizQuestion] = [
-                QuizQuestion(
-                    image: "The Godfather",
-                    text: "Рейтинг этого фильма больше чем 6?",
-                    correctAnswer: true),
-                QuizQuestion(
-                    image: "The Dark Knight",
-                    text: "Рейтинг этого фильма больше чем 6?",
-                    correctAnswer: true),
-                QuizQuestion(
-                    image: "Kill Bill",
-                    text: "Рейтинг этого фильма больше чем 6?",
-                    correctAnswer: true),
-                QuizQuestion(
-                    image: "The Avengers",
-                    text: "Рейтинг этого фильма больше чем 6?",
-                    correctAnswer: true),
-                QuizQuestion(
-                    image: "Deadpool",
-                    text: "Рейтинг этого фильма больше чем 6?",
-                    correctAnswer: true),
-                QuizQuestion(
-                    image: "The Green Knight",
-                    text: "Рейтинг этого фильма больше чем 6?",
-                    correctAnswer: true),
-                QuizQuestion(
-                    image: "Old",
-                    text: "Рейтинг этого фильма больше чем 6?",
-                    correctAnswer: false),
-                QuizQuestion(
-                    image: "The Ice Age Adventures of Buck Wild",
-                    text: "Рейтинг этого фильма больше чем 6?",
-                    correctAnswer: false),
-                QuizQuestion(
-                    image: "Tesla",
-                    text: "Рейтинг этого фильма больше чем 6?",
-                    correctAnswer: false),
-                QuizQuestion(
-                    image: "Vivarium",
-                    text: "Рейтинг этого фильма больше чем 6?",
-                    correctAnswer: false)
-                ]
     
-    // переменная с индексом текущего вопроса, начальное значение 0 (так как индекс в массиве начинается с 0)
-    private var currentQuestionIndex = 0
-    // переменная со счётчиком правильных ответов, начальное значение закономерно 0
-    private var correctAnswers = 0
-
     // вью модель для состояния "Вопрос показан"
     struct QuizStepViewModel {
-        // картинка с афишей фильма с типом UIImage
         let image: UIImage
-        // вопрос о рейтинге квиза
         let question: String
-        // строка с порядковым номером этого вопроса (ex. "1/10")
         let questionNumber: String
     }
-        // метод конвертации, который принимает моковый вопрос и возвращает вью модель для экрана вопроса
-       private func convert(model: QuizQuestion) -> QuizStepViewModel {
-            let questionStep = QuizStepViewModel(
-                image: UIImage(named: model.image) ?? UIImage(),
-                question: model.text,
-                questionNumber:
-                    "\(currentQuestionIndex + 1) / \(questions.count)")
+    
+    // вью модель для состояния "Результат квиза"
+    struct QuizResultsViewModel {
+      // строка с заголовком алерта
+      let title: String
+      // строка с текстом о количестве набранных очков
+      let text: String
+      // текст для кнопки алерта
+      let buttonText: String
+    }
+    
+    private let questions: [QuizQuestion] = [
+        QuizQuestion(
+            image: "The Godfather",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "The Dark Knight",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "Kill Bill",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "The Avengers",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "Deadpool",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "The Green Knight",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "Old",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false),
+        QuizQuestion(
+            image: "The Ice Age Adventures of Buck Wild",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false),
+        QuizQuestion(
+            image: "Tesla",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false),
+        QuizQuestion(
+            image: "Vivarium",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false)
+    ]
+    
+    
+    private var currentQuestionIndex = 0
+    private var correctAnswers = 0
+    
+    // метод конвертации, который принимает моковый вопрос и возвращает вью модель для экрана вопроса
+    private func convert(model: QuizQuestion) -> QuizStepViewModel {
+        let questionStep = QuizStepViewModel(
+            image: UIImage(named: model.image) ?? UIImage(),
+            question: model.text,
+            questionNumber:
+                "\(currentQuestionIndex + 1) / \(questions.count)")
         return questionStep
+    }
+    
+    // приватный метод вывода на экран вопроса, который принимает на вход вью модель вопроса и ничего не возвращает
+    private func show(quiz step: QuizStepViewModel) {
+        imageView.image = step.image
+        textLabel.text = step.question
+        counterLabel.text = step.questionNumber
+    }
+    
+    // приватный метод, который меняет цвет рамки
+    // принимает на вход булевое значение и ничего не возвращает
+    private func showAnswerResult(isCorrect: Bool) {
+        if isCorrect {
+            correctAnswers += 1
         }
         
-        // приватный метод вывода на экран вопроса, который принимает на вход вью модель вопроса и ничего не возвращает
-       private func show(quiz step: QuizStepViewModel) {
-            imageView.image = step.image
-            textLabel.text = step.question
-            counterLabel.text = step.questionNumber
+        imageView.layer.masksToBounds = true
+        imageView.layer.borderWidth = 8
+        imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
+        
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            self.showNextQuestionOrResults()
         }
     }
+    
+    // приватный метод, который содержит логику перехода в один из сценариев
+    // метод ничего не принимает и ничего не возвращает
+    private func showNextQuestionOrResults() {
+        if currentQuestionIndex == questions.count - 1 { // 1
+            // идём в состояние "Результат квиза"
+            let text = "Ваш результат: \(correctAnswers)/10" // 1
+                    let viewModel = QuizResultsViewModel( // 2
+                        title: "Этот раунд окончен!",
+                        text: text,
+                        buttonText: "Сыграть ещё раз")
+                    show(quiz: viewModel) // 3
+        } else { // 2
+            currentQuestionIndex += 1
+            // идём в состояние "Вопрос показан"
+            let nextQuestion = questions[currentQuestionIndex]
+            let viewModel = convert(model: nextQuestion)
+            
+            show(quiz: viewModel)
+        }
+    }
+    
+    // приватный метод для показа результатов раунда квиза
+    // принимает вью модель QuizResultsViewModel и ничего не возвращает
+    private func show(quiz result: QuizResultsViewModel) {
+        let alert = UIAlertController(
+            title: result.title,
+            message: result.text,
+            preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: result.buttonText, style: .default) { _ in
+            self.currentQuestionIndex = 0
+            self.correctAnswers = 0
+            
+            let firstQuestion = self.questions[self.currentQuestionIndex]
+            let viewModel = self.convert(model: firstQuestion)
+            self.show(quiz: viewModel)
+        }
+        
+        alert.addAction(action)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+}
 /*
  Mock-данные
  
