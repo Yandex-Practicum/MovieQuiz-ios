5f775c5129260dc40f9c840d8eb72a1972e8752a
Fedor <starbreeze@yandex.ru>
2023-11-01 04:32:59 -0700

    Создание новой ветки разработки приложения MovieQuiz. Экспорт цветов и изображений.

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,3 +1,10 @@
 import UIKit
 
-extension UIColor { }
+extension UIColor {
+    static var ypblack: UIColor{ UIColor(named: "YP Black") ?? UIColor.black }
+    static var ypwhite: UIColor{ UIColor(named: "YP White") ?? UIColor.white }
+    static var ypgreen: UIColor{ UIColor(named: "YP Green") ?? UIColor.green }
+    static var ypgray: UIColor{ UIColor(named: "YP Gray") ?? UIColor.gray }
+    static var ypred: UIColor{ UIColor(named: "YP Red") ?? UIColor.red }
+    static var ypbackground: UIColor{ UIColor(named: "YP Background") ?? UIColor.darkGray}
+}
