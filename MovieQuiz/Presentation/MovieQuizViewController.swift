e340a4e257eeaae384d39afc443b4b856c765f25
Fedor <starbreeze@yandex.ru>
2023-11-02 07:41:04 -0700

    Создан интерфейс приложения и моки.

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -2,10 +2,47 @@ import UIKit
 
 final class MovieQuizViewController: UIViewController {
     // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
 }
+
+struct QuizQuestion {
+    //строка с названием фильма
+    //свпадает с названием картинки афиши фильма в Assets
+    var image: String
+    
+    //строка с названием вопроса о фильме
+    var text: String
+    
+    //будевое значение (true, false), дающее правильный ответ на вопрос
+    var correctAnswer: Bool
+}
+
+var theGodfather: QuizQuestion = QuizQuestion(image: "The theGodFather", text: "Рейтин этого фильма больше чем 6?", correctAnswer: true)
+
+var theDarkKnight: QuizQuestion = QuizQuestion(image: "The Dark Knight", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true)
+
+var killBill = QuizQuestion(image: "Kill Bill", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true)
+
+var theAvengers = QuizQuestion(image: "The Avengers", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true)
+
+var deadpool = QuizQuestion(image: "Deadpool", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true)
+
+var theGreenKnight = QuizQuestion(image: "The Green Knight", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true)
+
+var old = QuizQuestion(image: "Old", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false)
+
+var theIceAgeAdvanturesOfBuckWild = QuizQuestion(image: "The Ice Age Adventures of Buck Wild", text: "Рейтинг этого фильма больше 6?", correctAnswer: false)
+
+var tesla = QuizQuestion(image: "Tesla", text: "Рейтинг этого фильма больше 6?", correctAnswer: false)
+
+var vivarium = QuizQuestion(image: "Vivarium", text: "Рейтинг этого фильма больше 6?", correctAnswer: false)
+
+private let questions: [QuizQuestion] = [theGodfather, theDarkKnight, killBill, theAvengers, deadpool, theGreenKnight, old, theIceAgeAdvanturesOfBuckWild, tesla, vivarium]
+
 
 /*
  Mock-данные
