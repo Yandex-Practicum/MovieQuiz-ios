d20e6ebddda50f930ab22bf4ef73771ceb2764f1
Fedor <starbreeze@yandex.ru>
2023-11-03 06:08:39 -0700

    Добавлена логика приложения и обновления постера фильма.

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,14 +1,5 @@
 import UIKit
 
-final class MovieQuizViewController: UIViewController {
-    // MARK: - Lifecycle
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
-}
-
 struct QuizQuestion {
     //строка с названием фильма
     //свпадает с названием картинки афиши фильма в Assets
@@ -21,7 +12,15 @@ struct QuizQuestion {
     var correctAnswer: Bool
 }
 
-var theGodfather: QuizQuestion = QuizQuestion(image: "The theGodFather", text: "Рейтин этого фильма больше чем 6?", correctAnswer: true)
+struct QuizStepViewModel {
+    
+    var image: UIImage
+    var question: String
+    var questionNumber: String
+    
+}
+
+var theGodfather: QuizQuestion = QuizQuestion(image: "The Godfather", text: "Рейтин этого фильма больше чем 6?", correctAnswer: true)
 
 var theDarkKnight: QuizQuestion = QuizQuestion(image: "The Dark Knight", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true)
 
@@ -41,8 +40,80 @@ var tesla = QuizQuestion(image: "Tesla", text: "Рейтинг этого фил
 
 var vivarium = QuizQuestion(image: "Vivarium", text: "Рейтинг этого фильма больше 6?", correctAnswer: false)
 
-private let questions: [QuizQuestion] = [theGodfather, theDarkKnight, killBill, theAvengers, deadpool, theGreenKnight, old, theIceAgeAdvanturesOfBuckWild, tesla, vivarium]
+final class MovieQuizViewController: UIViewController {
+    // MARK: - Lifecycle
+    
+    @IBOutlet private weak var counterLabel: UILabel!
+    
+    
+    @IBOutlet private weak var imageView: UIImageView!
+    
+    
+    @IBOutlet private weak var textLabel: UILabel!
+    
+    private let questions: [QuizQuestion] = [theGodfather, theDarkKnight, killBill, theAvengers, deadpool, theGreenKnight, old, theIceAgeAdvanturesOfBuckWild, tesla, vivarium]
+    
+    // Переменная со счетчиком текущих вопросов
+    private var currentQuestionIndex = 0
+    
+    // Счетчик правильных вариантов ответа
+    private var correctAnswers = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imageView.layer.masksToBounds = true
+        imageView.layer.cornerRadius = 6
+        var currentQuestion = questions[currentQuestionIndex]
+        var currentView = convert(model: currentQuestion)
+        show(quiz: currentView)
+        
+    }
+    
+    private func convert(model: QuizQuestion) -> QuizStepViewModel {
+        
+        var image = UIImage(named: model.image) ?? UIImage()
+        var questionText: String = model.text
+        var questionNumber: String = "\(currentQuestionIndex + 1)/\(questions.count)"
+        
+        return QuizStepViewModel(image: image, question: questionText, questionNumber: questionNumber)
+        
+    }
+    
+    private func show(quiz step: QuizStepViewModel) {
+        counterLabel.text = step.questionNumber
+        imageView.image = step.image
+        textLabel.text = step.question
+    }
+    
+     private func showAndwerResult(isCorrect: Bool) {
 
+        imageView.layer.borderWidth = 8
+        imageView.layer.borderColor = isCorrect ? UIColor.ypgreen.cgColor : UIColor.ypred.cgColor
+
+         DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+             self.showNextQuestionOrResult()
+         }
+    }
+    
+    private func showNextQuestionOrResult() {
+        if currentQuestionIndex == questions.count - 1 {
+        } else {
+            currentQuestionIndex += 1
+            show(quiz: convert(model: questions[currentQuestionIndex]))
+        }
+    }
+    
+    @IBAction private func yesButtonClicked(_ sender: Any) {
+        var givenAnswer = true
+        showAndwerResult(isCorrect: questions[currentQuestionIndex].correctAnswer == givenAnswer)
+    }
+    
+    @IBAction private func noButtonClicked(_ sender: Any) {
+        var givenAnswer = false
+        showAndwerResult(isCorrect: questions[currentQuestionIndex].correctAnswer == givenAnswer)
+        }
+}
 
 /*
  Mock-данные
