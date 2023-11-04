9b359e1e4116ec928664f408c5f3c0e66c405c63
lomakina-mariya <145546282+lomakina-mariya@users.noreply.github.com>
2023-11-04 14:45:47 +0300

    Revert "Revert "Merge pull request #7 from lomakina-mariya/sprint_5""
    This reverts commit 2b147b8132e1e887770f80410c0a84086068da62.

---
+++ MovieQuiz/Models/QuizStepViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  QuizStepViewModel.swift
+//  MovieQuiz
+
+import UIKit
+
+// вью модель для состояния "Вопрос показан"
+struct QuizStepViewModel {
+    // картинка с афишей фильма с типом UIImage
+    let image: UIImage
+    // вопрос о рейтинге квиза
+    let question: String
+    // строка с порядковым номером этого вопроса (ex. "1/10")
+    let questionNumber: String
+}
+
