d665aeda5354819cb58cd32b1e2f78bb417c27c3
lomakina-mariya <145546282+lomakina-mariya@users.noreply.github.com>
2023-11-06 02:36:16 +0300

    Восстановление спринта 5

---
+++ MovieQuiz/Models/QuizQuestion 2.swift
@@ -0,0 +1,14 @@
+//
+//  QuizQuestion.swift
+//  MovieQuiz
+
+import Foundation
+
+struct QuizQuestion {
+    // строка с названием фильма,
+    let image: String
+    // строка с вопросом о рейтинге фильма
+    let text: String
+    // булевое значение (true, false), правильный ответ на вопрос
+    let correctAnswer: Bool
+}
