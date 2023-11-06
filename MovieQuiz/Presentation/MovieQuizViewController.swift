7d45adcc4a1271b878fbe7e766fc84caafacc273
Mikhail <drozdnik@list.ru>
2023-11-06 23:42:18 +0300

    show first question done, bug with QuestionLabel

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,23 +1,36 @@
 import UIKit
 
 final class MovieQuizViewController: UIViewController {
-    @IBOutlet weak var QuestionLabel: UILabel!
-    @IBOutlet weak var QuestionTitleLabel: UILabel!
-    @IBOutlet weak var NoButton: UIButton!
-    @IBOutlet weak var IndexLabel: UILabel!
-    @IBOutlet weak var PreviewImage: UIImageView!
-    @IBOutlet weak var YesButton: UIButton!
+    
+    private var currentQuestionIndex = 0
+    private var correctAnswers = 0
+    
+    @IBOutlet private weak var questionLabel: UILabel!
+    @IBOutlet private weak var textLabel: UILabel!
+    @IBOutlet private weak var noButton: UIButton!
+    @IBOutlet private weak var counterLabel: UILabel!
+    @IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet private weak var YesButton: UIButton!
+    @IBAction private func yesButtonPressed(_ sender: Any) {
+    }
+    @IBAction private func noButtonPressed(_ sender: Any) {
+    }
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        let currentQuestion = questions[currentQuestionIndex]
+        let quizStep = convert(model: currentQuestion)
+        show(quiz: quizStep)
+
+        
         
         view.backgroundColor = UIColor.ypBlack
         
-        NoButton.setTitle("Нет", for: .normal)
-        NoButton.setTitleColor(UIColor.ypBlack, for: .normal)
-        NoButton.backgroundColor = UIColor.ypWhite
-        NoButton.layer.cornerRadius = 15
-        NoButton.frame.size = CGSize(width: 157, height: 60)
+        noButton.setTitle("Нет", for: .normal)
+        noButton.setTitleColor(UIColor.ypBlack, for: .normal)
+        noButton.backgroundColor = UIColor.ypWhite
+        noButton.layer.cornerRadius = 15
+        noButton.frame.size = CGSize(width: 157, height: 60)
         
         YesButton.setTitle("Да", for: .normal)
         YesButton.setTitleColor(UIColor.ypBlack, for: .normal)
@@ -25,71 +38,98 @@ final class MovieQuizViewController: UIViewController {
         YesButton.layer.cornerRadius = 15
         YesButton.frame.size = CGSize(width: 157, height: 60)
         
-        QuestionTitleLabel.text = "Вопрос:"
-        QuestionTitleLabel.textColor = UIColor.ypWhite
-        QuestionTitleLabel.frame.size = CGSize(width: 72, height: 24)
-        QuestionTitleLabel.font = UIFont(name: "YS Display", size: 20)
+        textLabel.text = "Вопрос:"
+        textLabel.textColor = UIColor.ypWhite
+        textLabel.frame.size = CGSize(width: 72, height: 24)
+        textLabel.font = UIFont(name: "YS Display", size: 20)
         
-        IndexLabel.text = "1/10"
-        IndexLabel.textColor = UIColor.ypWhite
-        IndexLabel.frame.size = CGSize(width: 72, height: 24)
-        IndexLabel.font = UIFont(name: "YS Display", size: 20)
-        QuestionLabel.textColor = UIColor.ypWhite
-        QuestionLabel.frame.size = CGSize(width: 72, height: 24)
-        QuestionLabel.font = UIFont(name: "YS Display", size: 23)
-        QuestionLabel.text = "Рейтинг этого фильма меньше 5?"
+       
+        counterLabel.textColor = UIColor.ypWhite
+        counterLabel.frame.size = CGSize(width: 72, height: 24)
+        counterLabel.font = UIFont(name: "YS Display", size: 20)
+        questionLabel.textColor = UIColor.ypWhite
+        questionLabel.frame.size = CGSize(width: 72, height: 24)
+        questionLabel.font = UIFont(name: "YS Display", size: 23)
+      
+        
+        
+    }
+    
+    struct QuizQuestion {
+        // строка с названием фильма,
+        // совпадает с названием картинки афиши фильма в Assets
+        let image: String
+        // строка с вопросом о рейтинге фильма
+        let text: String
+        // булевое значение (true, false), правильный ответ на вопрос
+        let correctAnswer: Bool
+    }
+    
+    // вью модель для состояния "Вопрос показан"
+    struct QuizStepViewModel {
+        // картинка с афишей фильма с типом UIImage
+        let image: UIImage
+        // вопрос о рейтинге квиза
+        let question: String
+        // строка с порядковым номером этого вопроса (ex. "1/10")
+        let questionNumber: String
+    }
+    
+    private let questions: [QuizQuestion] = [
+        QuizQuestion(
+            image: "The Godfather",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "The Dark Knight",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "Kill Bill",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "The Avengers",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "Deadpool",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "The Green Knight",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "Old",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false),
+        QuizQuestion(
+            image: "The Ice Age Adventures of Buck Wild",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false),
+        QuizQuestion(
+            image: "Tesla",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false),
+        QuizQuestion(
+            image: "Vivarium",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false)
+    ]
+
+    
+    private func convert(model: QuizQuestion) -> QuizStepViewModel {
+        let questionStep = QuizStepViewModel( // 1
+            image: UIImage(named: model.image) ?? UIImage(), // 2
+            question: model.text, // 3
+            questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)") // 4
+        return questionStep
+    }
+    
+    private func show(quiz step: QuizStepViewModel) {
+        imageView.image = step.image
+        questionLabel.text = step.question
+        counterLabel.text = step.questionNumber
     }
 }
-struct QuizQuestion {
-  // строка с названием фильма,
-  // совпадает с названием картинки афиши фильма в Assets
-  let image: String
-  // строка с вопросом о рейтинге фильма
-  let text: String
-  // булевое значение (true, false), правильный ответ на вопрос
-  let correctAnswer: Bool
-}
-
-
- private let questions: [QuizQuestion] = [
-         QuizQuestion(
-             image: "The Godfather",
-             text: "Рейтинг этого фильма больше чем 6?",
-             correctAnswer: true),
-         QuizQuestion(
-             image: "The Dark Knight",
-             text: "Рейтинг этого фильма больше чем 6?",
-             correctAnswer: true),
-         QuizQuestion(
-             image: "Kill Bill",
-             text: "Рейтинг этого фильма больше чем 6?",
-             correctAnswer: true),
-         QuizQuestion(
-             image: "The Avengers",
-             text: "Рейтинг этого фильма больше чем 6?",
-             correctAnswer: true),
-         QuizQuestion(
-             image: "Deadpool",
-             text: "Рейтинг этого фильма больше чем 6?",
-             correctAnswer: true),
-         QuizQuestion(
-             image: "The Green Knight",
-             text: "Рейтинг этого фильма больше чем 6?",
-             correctAnswer: true),
-         QuizQuestion(
-             image: "Old",
-             text: "Рейтинг этого фильма больше чем 6?",
-             correctAnswer: false),
-         QuizQuestion(
-             image: "The Ice Age Adventures of Buck Wild",
-             text: "Рейтинг этого фильма больше чем 6?",
-             correctAnswer: false),
-         QuizQuestion(
-             image: "Tesla",
-             text: "Рейтинг этого фильма больше чем 6?",
-             correctAnswer: false),
-         QuizQuestion(
-             image: "Vivarium",
-             text: "Рейтинг этого фильма больше чем 6?",
-             correctAnswer: false)
-     ]
