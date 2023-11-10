280ffeeb2981f137f0b215d37464e94f69cdf3ae
IlyaGrevtsov <ilyagrevtsov00@gmail.com>
2023-11-10 15:36:57 +0300

    Sprint 5

---
+++ MovieQuiz/Models/QuizQuestion.swift
@@ -0,0 +1,6 @@
+import Foundation
+struct QuizQuestion {
+    let image: String
+    let text: String
+    let correctAnswer: Bool
+}
---
+++ MovieQuiz/Services/AlertPresenterDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  AlertPresenterDelegate.swift
+//  MovieQuiz
+//
+//  Created by user on 04.11.2023.
+//
+
+import UIKit
+
+protocol AlertPresenterDelegate: UIViewController{
+    func didRecieveAlert()
+}
