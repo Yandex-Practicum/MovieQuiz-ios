44da04b7230b87c2bc176d428a5ed891fc5dc6ce
IlyaGrevtsov <ilyagrevtsov00@gmail.com>
2023-11-13 16:56:00 +0300

    Исправил алерт Добавил скобку

---
+++ MovieQuiz/Models/MostPopularMovies.swift
@@ -0,0 +1,8 @@
+//
+//  MostPopularMovies.swift
+//  MovieQuiz
+//
+//  Created by user on 19.11.2023.
+//
+
+import Foundation
---
+++ MovieQuiz/Services/NetworkClient.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkClient.swift
+//  MovieQuiz
+//
+//  Created by user on 17.11.2023.
+//
+
+import Foundation
