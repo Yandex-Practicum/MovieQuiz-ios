d88d2f44fe5660d3a6bae184f7529681ca9af39f
Timurkaev Malik <timurkaev_malik@icloud.com>
2023-11-28 19:01:46 +0300

    на проверку ошибок

---
+++ MovieQuiz/Models/BestGame.swift
@@ -0,0 +1,8 @@
+//
+//  BestGame.swift
+//  MovieQuiz
+//
+//  Created by Malik Timurkaev on 26.11.2023.
+//
+
+import Foundation
---
+++ MovieQuiz/Services/StatisticService.swift
@@ -0,0 +1,8 @@
+//
+//  StatisticService.swift
+//  MovieQuiz
+//
+//  Created by Malik Timurkaev on 25.11.2023.
+//
+
+import Foundation
