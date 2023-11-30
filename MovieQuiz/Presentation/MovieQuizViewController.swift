0d50822bfbdbd36a64ded3cf0184faccc3b33c59
NeX0Zz <den.nickol1406@gmail.com>
2023-11-30 14:04:29 +0300

    Убран радиус скругления в showAnswerResult

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -125,7 +125,6 @@ final class MovieQuizViewController: UIViewController {
         if isCorrect {
             correctAnswers += 1
         }
-        imageView.layer.cornerRadius = 20
         imageView.layer.masksToBounds = true
         imageView.layer.borderWidth = 8
         imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
