6d1b2270d120719deafadcab3292601343c22488
Aleksei Bondarenko <LexaBondRec@gmail.com>
2023-12-02 12:22:15 +0200

    To XXX XXX

--- MovieQuiz/AppDelegate.swift
+++ MovieQuiz/AppDelegate.swift
@@ -6,7 +6,7 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-        printContent("Check")
+        print("XXXXXXX")
         return true
     }
 
