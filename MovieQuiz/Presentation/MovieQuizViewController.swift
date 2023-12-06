c4a77b70c79263133178e645aaf24acad8198537
Giya077 <giyaDevIos@gmail.com>
2023-12-06 12:20:14 +0600

    внес измениние после ревью: 1. borderWidth 2. borderColor = clear 3. cornerRadius 4. отключил sleep в launchScreen 5. Добавил по рекомендации метод включение отключения кнопок, теперь быстрое двойное нажатие кнопки не влияет на результат квиза.

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -6,6 +6,8 @@ final class MovieQuizViewController: UIViewController {
     private var currentQuestionIndex = 0
     private var correctAnswers = 0
     
+    private var isProcessinqQuestion = false //флаг по обработке след. вопроса для блок. и разблк. кнопки
+    
     private let questions: [QuizQuestion] = [
         QuizQuestion(image: "The Godfather",
                      text: "Рейтинг этого фильма больше чем 6?",
@@ -48,11 +50,14 @@ final class MovieQuizViewController: UIViewController {
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        imageView.layer.borderWidth = 5
-        imageView.layer.borderColor = UIColor.ypWhite.cgColor
-        imageView.layer.cornerRadius = 6
+        imageView.layer.borderWidth = 8
+        imageView.layer.borderColor = UIColor.clear.cgColor
+        imageView.layer.cornerRadius = 20
         
         showNextQuestionOrResults()
+    }
+    override var preferredStatusBarStyle: UIStatusBarStyle{
+        return .lightContent //меняем цвет status bara,  так же в info добавил ключ
     }
     
     //actions
@@ -79,6 +84,8 @@ final class MovieQuizViewController: UIViewController {
     }
     
     private func showNextQuestionOrResults() {
+        enableButtons(false) //выкл.
+        
         if currentQuestionIndex == questions.count  {
             // идём в состояние Результат квиза
             showQuizResultsAlert(buttonTitle: "Сыграть ещё раз")
@@ -86,12 +93,18 @@ final class MovieQuizViewController: UIViewController {
             let nextQuestion = questions[currentQuestionIndex]
             let viewModel = convert(model: nextQuestion)
             show(quiz: viewModel)
+            enableButtons(true) //включаю кнопку
+    
         }
     }
     
     // приватный метод, который меняет цвет рамки
-    // принимает на вход булевое значение и ничего не возвращает
     private func showAnswerResult(isCorrect: Bool) {
+        guard !isProcessinqQuestion else {
+            return
+        }
+        isProcessinqQuestion = true
+        
         let currentQuestion = questions[currentQuestionIndex]
         let isCorrect = isCorrect == currentQuestion.correctAnswer
         
@@ -105,8 +118,9 @@ final class MovieQuizViewController: UIViewController {
         print("кол-во правильных ответов \(correctAnswers)") //понимаю что логика работает
         
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak self] in //добавил задержку
-            self?.imageView.layer.borderColor = UIColor.white.cgColor // Сброс цвета рамки перед отображением следующего вопроса
+            self?.imageView.layer.borderColor = UIColor.clear.cgColor // Сброс цвета рамки перед отображением следующего вопроса
             self?.showNextQuestionOrResults()
+            self?.isProcessinqQuestion = false // возращаю кнопку в исходное значение
         }
     }
     
@@ -131,13 +145,18 @@ final class MovieQuizViewController: UIViewController {
         let action = UIAlertAction(title: buttonTitle, style: .default) { _ in
             self.currentQuestionIndex = 0
             self.correctAnswers = 0 // Здесь обнуляем correctAnswers перед переходом на новый раунд
-            
+            self.enableButtons(true) //включаю кнопки на след. раунд
             let firstQuestion = self.questions[self.currentQuestionIndex] // заново показываем первый вопрос
             let viewModel = self.convert(model: firstQuestion) // заново показываем первый вопрос
             self.show(quiz: viewModel) // заново показываем первый вопрос
         }
         alert.addAction(action)
         self.present(alert, animated: true, completion: nil)
+    }
+    
+    private func enableButtons(_ enable: Bool) { //метод вкл,откл кнопок
+        noButton.isEnabled = enable
+        yesButton.isEnabled = enable
     }
 }
 
