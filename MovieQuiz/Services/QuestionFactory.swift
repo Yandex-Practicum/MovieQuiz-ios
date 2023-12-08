bf0fbd3f806860acf345bb30dfaafe3469471b3e
KosAleks <aleksandra.kosnireva@yandex.ru>
2023-12-09 02:35:31 +0500

    Create a Network Client. Created request for API IMDb. Network data is connected to the project.

--- MovieQuiz/Services/QuestionFactory.swift
+++ MovieQuiz/Services/QuestionFactory.swift
@@ -46,8 +46,8 @@ final class QuestionFactory: QuestionFactoryProtocol {
             
             var imageData = Data()
            
-           do {
-               imageData = try Data(contentsOf: movie.imageURL)
+            do {
+                imageData = try Data(contentsOf: movie.resizedImageURL)
             } catch {
                 print("Failed to load image")
             }
