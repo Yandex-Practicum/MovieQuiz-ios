c320d5fa4d97255cfdb359493a0076450bd49a32
@rostovHomer <filipprnd161@gmail.com>
2023-12-11 00:05:32 +0300

    добавил логику и верстку

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,9 +1,103 @@
 import UIKit
 
 final class MovieQuizViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var textLabel: UILabel!
+    @IBOutlet var counterLabel: UILabel!
+    
+    struct QuizQuestion {
+      // строка с названием фильма,
+      // совпадает с названием картинки афиши фильма в Assets
+      let image: String
+      // строка с вопросом о рейтинге фильма
+      let text: String
+      // булевое значение (true, false), правильный ответ на вопрос
+      let correctAnswer: Bool
+    }
+    
+    struct QuizStepViewModel {
+      // картинка с афишей фильма с типом UIImage
+      let image: UIImage
+      // вопрос о рейтинге квиза
+      let question: String
+      // строка с порядковым номером этого вопроса (ex. "1/10")
+      let questionNumber: String
+    }
+    
+    private let questions: [QuizQuestion] = [
+            QuizQuestion(
+                image: "The Godfather",
+                text: "Рейтинг этого фильма больше чем 6?",
+                correctAnswer: true),
+            QuizQuestion(
+                image: "The Dark Knight",
+                text: "Рейтинг этого фильма больше чем 6?",
+                correctAnswer: true),
+            QuizQuestion(
+                image: "Kill Bill",
+                text: "Рейтинг этого фильма больше чем 6?",
+                correctAnswer: true),
+            QuizQuestion(
+                image: "The Avengers",
+                text: "Рейтинг этого фильма больше чем 6?",
+                correctAnswer: true),
+            QuizQuestion(
+                image: "Deadpool",
+                text: "Рейтинг этого фильма больше чем 6?",
+                correctAnswer: true),
+            QuizQuestion(
+                image: "The Green Knight",
+                text: "Рейтинг этого фильма больше чем 6?",
+                correctAnswer: true),
+            QuizQuestion(
+                image: "Old",
+                text: "Рейтинг этого фильма больше чем 6?",
+                correctAnswer: false),
+            QuizQuestion(
+                image: "The Ice Age Adventures of Buck Wild",
+                text: "Рейтинг этого фильма больше чем 6?",
+                correctAnswer: false),
+            QuizQuestion(
+                image: "Tesla",
+                text: "Рейтинг этого фильма больше чем 6?",
+                correctAnswer: false),
+            QuizQuestion(
+                image: "Vivarium",
+                text: "Рейтинг этого фильма больше чем 6?",
+                correctAnswer: false)
+        ]
+    
+    @IBAction private func noButtonClicked(_ sender: UIButton) {
+    }
+    
+    @IBAction private func yesButtonClicked(_ sender: UIButton) {
+    }
+    
+    private var currentQuestionIndex = 0
+    private var correctAnswers = 0
+    
+    private func convert(model: QuizQuestion) -> QuizStepViewModel {
+        let questionStep = QuizStepViewModel( // 1
+            image: UIImage(named: model.image) ?? UIImage(), // 2
+            question: model.text, // 3
+            questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)") // 4
+        return questionStep
+    }
+    
+    private func show(quiz step: QuizStepViewModel) {
+        print(step.image)
+        imageView.image = step.image
+      textLabel.text = step.question
+      counterLabel.text = step.questionNumber
+    }
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+//        let currentQuestion = questions[currentQuestionIndex]
+//        let convertModel = convert(model: currentQuestion)
+        show(quiz: convert(model: questions[currentQuestionIndex]))
+ 
+    
     }
 }
 
