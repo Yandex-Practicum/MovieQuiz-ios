b9f0cacdf64e1744e29ca8ae0b62f8242b0a4d5d
Fokusnik909 <Fokusnik90999@gmail.com>
2023-12-10 15:41:44 +0500

    completed project

---
+++ MovieQuiz/Models/QuizResultsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  QuizResultsViewModel.swift
+//  MovieQuiz
+//
+//  Created by Артур  Арсланов on 08.12.2023.
+//
+
+import Foundation
---
+++ MovieQuiz/Models/QuizStepViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  QuizStepViewModel.swift
+//  MovieQuiz
+//
+//  Created by Артур  Арсланов on 08.12.2023.
+//
+
+import Foundation
