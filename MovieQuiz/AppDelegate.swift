d2719b67d3d40bac905ada0fb9d9a010d5a57eee
TimurMiskurov23 <timur.miskurov@yandex.ru>
2023-12-19 21:29:25 +0300

    В AppDelegate добавлен вывод на консоль для тестирования коммита через систему контроля версий

--- MovieQuiz/AppDelegate.swift
+++ MovieQuiz/AppDelegate.swift
@@ -7,6 +7,7 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
         return true
+        print("Hello World")
     }
 
     // MARK: UISceneSession Lifecycle
