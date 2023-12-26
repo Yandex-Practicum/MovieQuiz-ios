754f41ca4c582087b4c4745b58beb41fa1b05122
Aleksei Bondarenko <LexaBondRec@gmail.com>
2023-12-26 14:23:42 +0200

    Sprint 4. UI and main logic were added

--- MovieQuiz/AppDelegate.swift
+++ MovieQuiz/AppDelegate.swift
@@ -6,7 +6,6 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-        printContent("Check")
         return true
     }
 
