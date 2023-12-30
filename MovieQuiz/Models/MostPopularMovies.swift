892a4b1c8cd2d8abe40a621c59a415b3b45aa691
Sergey Ivanov <ivanov.sergey.ekb@gmail.com>
2023-12-31 02:14:16 +0600

    Выполнил Спринт 6

---
+++ MovieQuiz/Models/MostPopularMovies.swift
@@ -0,0 +1,8 @@
+//
+//  MostPopularMovies.swift
+//  MovieQuiz
+//
+//  Created by Sergey Ivanov on 30.12.2023.
+//
+
+import Foundation
---
+++ MovieQuiz/Services/MoviesLoader.swift
@@ -0,0 +1,8 @@
+//
+//  MoviesLoader.swift
+//  MovieQuiz
+//
+//  Created by Sergey Ivanov on 30.12.2023.
+//
+
+import Foundation
