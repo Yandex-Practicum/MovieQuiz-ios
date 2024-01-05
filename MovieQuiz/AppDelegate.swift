d5e71614c5c921579b1d9d76f5dd34b0ac4ff71c
Ekaterina Bardanova <bardanovaekaterina7@gmail.com>
2024-01-05 01:06:32 +0100

    commit

--- MovieQuiz/AppDelegate.swift
+++ MovieQuiz/AppDelegate.swift
@@ -6,7 +6,7 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-        print("h")
+        print("hh")
         return true
     }
 
