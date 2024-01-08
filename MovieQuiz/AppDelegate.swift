c00b72a1128f44b8163933246605c10a9dc03de7
comme-il-faut <rovenkoaa@icloud.com>
2024-01-08 21:18:47 +0300

    complete homework sprint4

--- MovieQuiz/AppDelegate.swift
+++ MovieQuiz/AppDelegate.swift
@@ -9,18 +9,11 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
-
     func application(
         _ application: UIApplication,
         configurationForConnecting connectingSceneSession: UISceneSession,
         options: UIScene.ConnectionOptions
     ) -> UISceneConfiguration {
- testBranch
-        print("Hello world for branch to merge!")
-
-        print("Hello world")
- project_sprint_3_start
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
