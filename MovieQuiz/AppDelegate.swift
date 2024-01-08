d19ea7896865916d34102fdfcb6bcc30c0137e41
Alex Seliverstov <150807899+A0ex@users.noreply.github.com>
2024-01-08 15:44:47 +0300

    Update AppDelegate.swift

--- MovieQuiz/AppDelegate.swift
+++ MovieQuiz/AppDelegate.swift
@@ -1,12 +1,12 @@
 import UIKit
 
 @main
+
 final class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-        print("Hello world for branch to merge!")
         return true
     }
 
