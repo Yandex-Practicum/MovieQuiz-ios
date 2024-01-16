3697a4542d1ac5be79b43ec474aa0444153e5217
Pavel Popov <80453586+Neenjahw@users.noreply.github.com>
2024-01-16 23:33:33 +0300

    Add files via upload

---
+++ Sprint_05/MovieQuiz/Models/QuizQuestion.swift
@@ -0,0 +1,14 @@
+//
+//  QuizQuestion.swift
+//  MovieQuiz
+//
+//  Created by Pavel Popov on 11.01.2024.
+//
+
+import Foundation
+
+struct QuizQuestion {
+    let image: String
+    let text: String
+    let correctAnswer: Bool
+}
