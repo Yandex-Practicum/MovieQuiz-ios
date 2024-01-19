a0040c22b1977d9fc3225ccd3e26f8d7e11d69df
roudYStorm <anzhelikaroud@yandex.ru>
2024-01-19 23:07:51 +0300

    no sleep

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,72 +1,208 @@
 import UIKit
 
 final class MovieQuizViewController: UIViewController {
-    // MARK: - Lifecycle
+    
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var counterLabel: UILabel!
+    @IBOutlet var textLabel: UILabel!
+    @IBOutlet var questionTitleLabel: UILabel!
+    
+    @IBOutlet var yesButton: UIButton!
+    @IBOutlet var noButton: UIButton!
+    
+    @IBOutlet var questionLabel: UILabel!
+    
+    private var currentQuestionIndex = 0
+    private var correctAnswers = 0
+    
+    
+    
+    private let questions: [QuizQuestion] = [
+        QuizQuestion(
+            image: "The Godfather",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "The Dark Knight",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "Kill Bill",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "The Avengers",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "Deadpool",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "The Green Knight",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "Old",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false),
+        QuizQuestion(
+            image: "The Ice Age Adventures of Buck Wild",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false),
+        QuizQuestion(
+            image: "Tesla",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false),
+        QuizQuestion(
+            image: "Vivarium",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false)
+    ]
     override func viewDidLoad() {
+        
+        
         super.viewDidLoad()
     }
+    
+    @IBAction func yesButtonClicked(_ sender: UIButton) {
+        let currentQuestion = questions[currentQuestionIndex]
+        let givenAnswer = true
+        
+        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
+    }
+    
+    
+    
+    
+    
+    @IBAction func noButtonClicked(_ sender: UIButton) {
+        let currentQuestion = questions[currentQuestionIndex]
+        let givenAnswer = false
+        
+        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
+    }
+    
+    
+    
+    
+    
+    
+    private struct QuizQuestion {
+        let image: String
+        let text: String
+        let correctAnswer: Bool
+        
+    }
+    private struct QuizStepViewModel {
+        let image: UIImage
+        let question: String
+        let questionNumber:  String
+    }
+    private func convert(model: QuizQuestion) -> QuizStepViewModel {
+        let questionStep = QuizStepViewModel(
+            image:UIImage(named: model.image) ?? UIImage(), // 2
+            question: model.text,
+            questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)")
+        return questionStep
+    }
+    
+    private func show(quiz step: QuizStepViewModel) {
+        imageView.image = step.image
+        textLabel.text = step.question
+        counterLabel.text = step.questionNumber
+        imageView.layer.borderColor =
+        UIColor.ypBlack.cgColor
+    }
+    private func showAnswerResult(isCorrect: Bool) {
+        if isCorrect {
+            correctAnswers += 1
+        }
+        imageView.layer.masksToBounds = true
+        imageView.layer.borderWidth = 8
+        imageView.layer.borderColor = isCorrect ?
+        UIColor.ypGreen.cgColor:
+        UIColor.ypRed.cgColor
+        imageView.layer.cornerRadius = 20
+        
+    }
+    private func showNextQuestionOrResults() {
+        if currentQuestionIndex == questions.count - 1 { // 1
+            
+            
+            
+            
+        } else { // 2
+            currentQuestionIndex += 1
+            
+            
+            
+            
+        }
+        
+        /*
+         Mock-данные
+         
+         
+         Картинка: The Godfather
+         Настоящий рейтинг: 9,2
+         Вопрос: Рейтинг этого фильма больше чем 6?
+         Ответ: ДА
+         
+         
+         Картинка: The Dark Knight
+         Настоящий рейтинг: 9
+         Вопрос: Рейтинг этого фильма больше чем 6?
+         Ответ: ДА
+         
+         
+         Картинка: Kill Bill
+         Настоящий рейтинг: 8,1
+         Вопрос: Рейтинг этого фильма больше чем 6?
+         Ответ: ДА
+         
+         
+         Картинка: The Avengers
+         Настоящий рейтинг: 8
+         Вопрос: Рейтинг этого фильма больше чем 6?
+         Ответ: ДА
+         
+         
+         Картинка: Deadpool
+         Настоящий рейтинг: 8
+         Вопрос: Рейтинг этого фильма больше чем 6?
+         Ответ: ДА
+         
+         
+         Картинка: The Green Knight
+         Настоящий рейтинг: 6,6
+         Вопрос: Рейтинг этого фильма больше чем 6?
+         Ответ: ДА
+         
+         
+         Картинка: Old
+         Настоящий рейтинг: 5,8
+         Вопрос: Рейтинг этого фильма больше чем 6?
+         Ответ: НЕТ
+         
+         
+         Картинка: The Ice Age Adventures of Buck Wild
+         Настоящий рейтинг: 4,3
+         Вопрос: Рейтинг этого фильма больше чем 6?
+         Ответ: НЕТ
+         
+         
+         Картинка: Tesla
+         Настоящий рейтинг: 5,1
+         Вопрос: Рейтинг этого фильма больше чем 6?
+         Ответ: НЕТ
+         
+         
+         Картинка: Vivarium
+         Настоящий рейтинг: 5,8
+         Вопрос: Рейтинг этого фильма больше чем 6?
+         Ответ: НЕТ
+         */
+    }
 }
-
-/*
- Mock-данные
- 
- 
- Картинка: The Godfather
- Настоящий рейтинг: 9,2
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: The Dark Knight
- Настоящий рейтинг: 9
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: Kill Bill
- Настоящий рейтинг: 8,1
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: The Avengers
- Настоящий рейтинг: 8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: Deadpool
- Настоящий рейтинг: 8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: The Green Knight
- Настоящий рейтинг: 6,6
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: Old
- Настоящий рейтинг: 5,8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
-
-
- Картинка: The Ice Age Adventures of Buck Wild
- Настоящий рейтинг: 4,3
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
-
-
- Картинка: Tesla
- Настоящий рейтинг: 5,1
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
-
-
- Картинка: Vivarium
- Настоящий рейтинг: 5,8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
- */
