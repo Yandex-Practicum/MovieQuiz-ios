16d4ed16832bee10f0f226d8d8279fbd902f2373
averpovskii <averpovskii@phoenixit.ru>
2024-02-01 20:33:08 +0300

    Task of the 4 sprint

---
+++ MovieQuiz/Helpers/CustomLabel.swift
@@ -0,0 +1,23 @@
+//
+//  CustomLabel.swift
+//  MovieQuiz
+//
+//  Created by Александр Верповский on 29.01.2024.
+//
+
+import UIKit
+
+final class CustomLabel: UILabel {
+    
+    override init(frame: CGRect){
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+}
