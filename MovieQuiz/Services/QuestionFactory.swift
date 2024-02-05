054ffeba2007dcccc1090d9ac57cd27f8227176f
Kamila <ksalimzh@gmail.com>
2024-02-05 19:55:48 +0600

    modified questionFactory

--- MovieQuiz/Services/QuestionFactory.swift
+++ MovieQuiz/Services/QuestionFactory.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-class QuestionFactory: QuestionFactoryProtocol {
+final class QuestionFactory: QuestionFactoryProtocol {
     weak var delegate: QuestionFactoryDelegate? 
     private let questions: [QuizQuestion] = [
         QuizQuestion(
