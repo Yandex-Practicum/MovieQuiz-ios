31f436ffeb9dcc90fe7b2c795192ce5a5de690cc
Daudben <daudcecoev01@gmail.com>
2024-02-25 13:47:03 +0600

    My home work

--- MovieQuiz/AppDelegate.swift
+++ MovieQuiz/AppDelegate.swift
@@ -6,6 +6,8 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
+        sleep(1)
+        
         return true
     }
 
